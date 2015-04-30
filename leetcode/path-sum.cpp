ce338c6af2bec6fb8a9d4596286095d636ceb465
smilingpoplar <smilingpoplar@gmail.com>
2015-04-30 21:40:08 +0800

    path sum

---
+++ leetcode/path-sum.cpp
@@ -0,0 +1,41 @@
+//
+//  path-sum.cpp
+//  https://leetcode.com/problems/path-sum/
+//
+//  Created by smilingpoplar on 15/4/30.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if (!root) return false;
+        if (!root->left && !root->right) return root->val == sum;
+        
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    TreeNode t0(5);
+    TreeNode t1l(2);
+    t0.left = &t1l;
+    TreeNode t1r(8);
+    t0.right = &t1r;
+    
+    Solution solution;
+    cout << solution.hasPathSum(&t0, 13);
+    
+    return 0;
+}
