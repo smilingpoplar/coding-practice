500491fa0aef245bcce9fa36f9bf0aac09968c1f
smilingpoplar <smilingpoplar@gmail.com>
2015-05-02 19:09:25 +0800

    word break ii

--- leetcode/word-break-ii.cpp
+++ leetcode/word-break-ii.cpp
@@ -16,34 +16,34 @@ using namespace std;
 class Solution {
 public:
     vector<string> wordBreak(string s, unordered_set<string>& dict) {
-        // 动态规划，设possible[i]表示s[0,i)可break，split[i,j]表示s[0,i)可在j处break成s[0,j)和s[j,i) 
+        // 动态规划，设ok[i]表示s[0,i)可break，breakAt[i,j]表示s[0,i)可在j处break成s[0,j)和s[j,i)
         // (s[0,j)是子问题，s[j,i)是单词，0<=i<=N, 0<=j<i)
         const size_t N = s.size();
-        vector<bool> possible(N + 1, false);
-        vector<vector<bool>> split(N + 1, vector<bool>(N, false));
+        vector<bool> ok(N + 1, false);
+        vector<vector<bool>> breakAt(N + 1, vector<bool>(N, false));
         
-        possible[0] = true;
+        ok[0] = true;
         for (size_t i = 1; i <= N; i++) {
             for (size_t j = 0; j < i; j++) {
-                if (possible[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
-                    possible[i] = true;
-                    split[i][j] = true;
+                if (ok[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
+                    ok[i] = true;
+                    breakAt[i][j] = true;
                 }
             }
         }
         
-        return generateSentences(s, split);
+        return generateSentences(s, breakAt);
     }
 private:
-    vector<string> generateSentences(const string &s, const vector<vector<bool>> &split) {
+    vector<string> generateSentences(const string &s, const vector<vector<bool>> &breakAt) {
         vector<string> result;
         vector<string> path;
-        generateSentences(s.size(), s, split, path, result);
+        generateSentences(s.size(), s, breakAt, path, result);
         return result;
     }
     
     // 为s[0,i)生成句子
-    void generateSentences(size_t i, const string &s, const vector<vector<bool>> &split, vector<string> &path, vector<string> &result) {
+    void generateSentences(size_t i, const string &s, const vector<vector<bool>> &breakAt, vector<string> &path, vector<string> &result) {
         if (i == 0) { // 终止条件
             auto sentence = generateSentence(path);
             result.push_back(sentence);
@@ -51,9 +51,9 @@ private:
         }
         
         for (size_t j = 0; j < i; j++) {
-            if (split[i][j]) {
+            if (breakAt[i][j]) {
                 path.push_back(s.substr(j, i - j));
-                generateSentences(j, s, split, path, result);
+                generateSentences(j, s, breakAt, path, result);
                 path.pop_back();
             }
         }
