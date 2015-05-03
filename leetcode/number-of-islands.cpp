3302915102a9f601afda6e7437e9c147bb93f02f
smilingpoplar <smilingpoplar@gmail.com>
2015-05-04 00:38:13 +0800

    number of islands

---
+++ leetcode/number-of-islands.cpp
@@ -0,0 +1,63 @@
+//
+//  number-of-islands.cpp
+//  https://leetcode.com/problems/number-of-islands/
+//
+//  Created by smilingpoplar on 15/5/2.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty()) return 0;
+        const size_t M = grid.size();
+        const size_t N = grid[0].size();
+        vector<vector<bool>> visited(M, vector<bool>(N, false));
+        
+        int count = 0;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    count++;
+                    dfs(i, j, grid, visited);
+                }
+            }
+        }
+        
+        return count;
+    }
+private:
+    void dfs(int i, int j, const vector<vector<char>> &grid, vector<vector<bool>> &visited) {
+        if (grid.empty()) return;
+        const size_t M = grid.size();
+        const size_t N = grid[0].size();
+        if (i < 0 || i >= M) return;
+        if (j < 0 || j >= N) return;
+        if (visited[i][j]) return;
+
+        visited[i][j] = true;
+        if (grid[i][j] == '0') return;
+        dfs(i - 1, j, grid, visited);
+        dfs(i + 1, j, grid, visited);
+        dfs(i, j - 1, grid, visited);
+        dfs(i, j + 1, grid, visited);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    vector<vector<char>> grid = {
+        {'1','1','0','0','0'},
+        {'1','1','0','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','0','1','1'},
+    };
+    Solution solution;
+    cout << solution.numIslands(grid);
+    
+    return 0;
+}
