d254323c1f170203126a9337089bc60c9cb8594a
smilingpoplar <smilingpoplar@gmail.com>
2015-05-07 21:17:35 +0800

    longest-substring-without-repeating-characters

--- leetcode/longest-substring-without-repeating-characters.cpp
+++ leetcode/longest-substring-without-repeating-characters.cpp
@@ -14,18 +14,18 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // 若子串含重复字符，则父串一定也含重复字符，单个子问题决定父问题，用贪心法
-        // 这跟dp不同，dp是单个子问题影响父问题，不足以决定父问题
+        // 若子串含重复字符，则父串也含重复字符，设s[start,i)是无重复子串
+        // 1. 若s[i]是重复字符，则新的无重复子串：s[lastIndex[s[i]]+1,i+1)
+        // 2. 若s[i]不是重复字符，则新的无重复子串：s[start,i+1)
         const size_t ASCII_SIZE = 256;
         vector<int> lastIndex(ASCII_SIZE, -1); // 每个字符最后出现的位置
         int longest = 0;
-        // s[start,i]是无重复字符子串
         for (int i = 0, start = 0; i < s.size(); i++) {
             if (lastIndex[s[i]] >= start) {
                 start = lastIndex[s[i]] + 1;
+            } else {
+                longest = max(longest, i + 1 - start);
             }
-            longest = max(longest, i - start + 1);
-
             lastIndex[s[i]] = i;
         }
         
