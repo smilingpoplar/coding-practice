8b2ddf4463c2c1278c30d5411bc290dfaa3da83d
smilingpoplar <smilingpoplar@gmail.com>
2015-05-20 00:55:39 +0800

    course schedule

--- leetcode/course-schedule.cpp
+++ leetcode/course-schedule.cpp
@@ -8,48 +8,48 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
     // 若能finish则图是无环有向图，即dfs时不存在回边，见图算法3.2章（回边：访问有较大后序编号的节点）
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
         // 构造图
-        vector<vector<int>> graph(numCourses, vector<int>());
+        vector<unordered_set<int>> graph(numCourses);
         for (const auto &edge : prerequisites) {
-            graph[edge[0]].push_back(edge[1]);
+            graph[edge.second].insert(edge.first);
         }
         // 遍历图
         vector<bool> visited(numCourses, false);
-        vector<int> postOrder(numCourses, -1);
+        vector<int> post(numCourses, -1);
         int order = 0;
         for (int i = 0; i < numCourses; i++) {
             if (!visited[i]) {
-                if (hasCycle(i, i, graph, visited, postOrder, order)) return false;
+                if (hasCycle(i, graph, visited, post, order)) return false;
             }
         }
         return true;
     }
 private:
-    bool hasCycle(int from, int to, const vector<vector<int>> &graph, vector<bool> &visited, vector<int> &postOrder, int &order) {
-        visited[to] = true;
-        for (int i = 0; i < graph[to].size(); i++) {
-            int next = graph[to][i];
+    bool hasCycle(int v, const vector<unordered_set<int>> &graph, vector<bool> &visited, vector<int> &post, int &order) {
+        visited[v] = true;
+        for (int next : graph[v]) {
             if (!visited[next]) {
-                if (hasCycle(to, next, graph, visited, postOrder, order)) return true;
-            } else if (postOrder[next] == -1) {
+                if (hasCycle(next, graph, visited, post, order)) return true;
+            } else if (post[next] == -1) {
                 return true; // 回边
             }
         }
-        postOrder[to] = order++;
+        post[v] = order++;
         return false;
     }
 };
 
 int main(int argc, const char * argv[]) {
     Solution solution;
-    vector<vector<int>> prerequisites{{1,0}, {0,1}};
+    vector<pair<int, int>> prerequisites{{1,0}, {0,1}};
     cout << solution.canFinish(2, prerequisites);
     
     return 0;
