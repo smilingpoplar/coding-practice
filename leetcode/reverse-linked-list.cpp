fbe2f9a7da336199d1985eb9bd49c0ce33b0cbb6
smilingpoplar <smilingpoplar@gmail.com>
2015-05-20 12:08:00 +0800

    reverse-linked-list

---
+++ leetcode/reverse-linked-list.cpp
@@ -0,0 +1,48 @@
+//
+//  reverse-linked-list
+//  https://leetcode.com/problems/reverse-linked-list/
+//
+//  Created by smilingpoplar on 15/5/20.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode dummy(-1);
+        while (head) {
+            ListNode *next = head->next;
+            head->next = dummy.next;
+            dummy.next = head;
+            head = next;
+        }
+        return dummy.next;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    ListNode l1(1);
+    ListNode l2(2);
+    ListNode l3(3);
+    l1.next = &l2;
+    l2.next = &l3;
+    
+    Solution solution;
+    auto head = solution.reverseList(&l1);
+    while (head) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    
+    return 0;
+}
