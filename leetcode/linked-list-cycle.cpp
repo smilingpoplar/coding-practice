be35af8d1f2bf908217fd38e37486e7a78ab6083
smilingpoplar <smilingpoplar@gmail.com>
2015-05-23 18:41:31 +0800

    linked-list-cycle

---
+++ leetcode/linked-list-cycle.cpp
@@ -0,0 +1,50 @@
+//
+//  linked-list-cycle
+//  https://leetcode.com/problems/linked-list-cycle/
+//
+//  Created by smilingpoplar on 15/5/23.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // 快慢指针，若快指针赶上慢指针则有环
+        auto fast = head;
+        auto slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    ListNode l1(1);
+    ListNode l2(2);
+    ListNode l3(3);
+    ListNode l4(4);
+    ListNode l5(5);
+    l1.next = &l2;
+    l2.next = &l3;
+    l3.next = &l4;
+    l4.next = &l5;
+    l5.next = &l3;
+    
+    Solution solution;
+    cout << solution.hasCycle(&l1);
+    
+    return 0;
+}
