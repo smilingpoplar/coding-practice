fb85b8a9990f1f64d1c9d93f74a7cbf1ac35d958
smilingpoplar <smilingpoplar@gmail.com>
2015-05-24 22:57:46 +0800

    unique-paths

--- leetcode/unique-paths.cpp
+++ leetcode/unique-paths.cpp
@@ -14,14 +14,14 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // 设f(i-1,j-1)表示ixj网格的路径数，1<=i<=m，1<=j<=n，
-        // 可以往右往下走，f(i-1,j-1) = f(i-2,j-1) + f(i-1,j-2)
+        // 设f(i,j)表示从[0,0]到[m-1,n-1]的路径数，0<=i<=m-1，0<=j<=n-1，
+        // 可以往右往下走，f(i,j) = f(i-1,j) + f(i,j-1)
         vector<vector<int>> f(m, vector<int>(n, 0));
-        for (int i = 1; i <= m; ++i) f[i - 1][0] = 1;
-        for (int j = 1; j <=n; ++j) f[0][j - 1] = 1;
-        for (int i = 2; i <= m; ++i) {
-            for (int j = 2; j <=n; ++j) {
-                f[i - 1][j - 1] = f[i - 2][j - 1] + f[i - 1][j - 2];
+        for (int i = 0; i <= m - 1; ++i) f[i][0] = 1;
+        for (int j = 0; j <= n - 1; ++j) f[0][j] = 1;
+        for (int i = 1; i <= m - 1; ++i) {
+            for (int j = 1; j <= n - 1; ++j) {
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
             }
         }
         return f[m - 1][n - 1];
