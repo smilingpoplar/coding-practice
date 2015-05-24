26621f03edac68e4217cff0ff285ed66d3d41e55
smilingpoplar <smilingpoplar@gmail.com>
2015-05-24 16:19:37 +0800

    reorder-list

--- leetcode/reorder-list.cpp
+++ leetcode/reorder-list.cpp
@@ -47,18 +47,17 @@ public:
             auto list1 = head;
             ListNode dummy(-1);
             ListNode *current = &dummy;
-            while (list1 || list2) {
-                if (list1) {
-                    current->next = list1;
-                    current = current->next;
-                    list1 = list1->next;
-                }
-                if (list2) {
-                    current->next = list2;
-                    current = current->next;
-                    list2 = list2->next;
-                }
+            while (list1 && list2) {
+                current->next = list1;
+                current = current->next;
+                list1 = list1->next;
+
+                current->next = list2;
+                current = current->next;
+                list2 = list2->next;
             }
+            current->next = list1 ? list1 : list2;
+
             head = dummy.next;
         }
     }
