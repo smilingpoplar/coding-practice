ce0ae692fdaba29c210d0750ed87ec734beb48b5
smilingpoplar <smilingpoplar@gmail.com>
2015-05-26 00:21:27 +0800

    contains-duplicate

---
+++ leetcode/contains-duplicate.cpp
@@ -0,0 +1,33 @@
+//
+//  contains-duplicate
+//  https://leetcode.com/problems/contains-duplicate/
+//
+//  Created by smilingpoplar on 15/5/25.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> set;
+        for (int num : nums) {
+            if (set.find(num) != set.end()) return true;
+            set.insert(num);
+        }
+        return false;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    vector<int> nums = {2,3,1,2,4};
+    Solution solution;
+    cout << solution.containsDuplicate(nums);
+    
+    return 0;
+}
