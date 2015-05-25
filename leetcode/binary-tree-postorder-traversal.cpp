f93eb3071340e94d6f549d5125c37f738cbfa7e2
smilingpoplar <smilingpoplar@gmail.com>
2015-05-25 13:27:05 +0800

    binary-tree-postorder-traversal

--- leetcode/binary-tree-postorder-traversal.cpp
+++ leetcode/binary-tree-postorder-traversal.cpp
@@ -18,10 +18,11 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+/*
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        // 后序遍历迭代版，用栈模拟，通过比较prev和curr来判断遍历方向
+        // 后序遍历，用栈模拟，通过比较prev和curr来判断遍历方向
         vector<int> result;
         vector<TreeNode *> stack;
         if (root) stack.push_back(root);
@@ -49,6 +50,50 @@ public:
         }
         
         return result;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        // morris后序遍历，对morris中序遍历作两处改动：
+        // 1.添加dummy节点，dummy.left = root;
+        // 2.访问完左子树后，倒序输出从左子树根节点current->left往右直到前驱节点prev这路径上的各个节点
+        // 遍历过程同morris中序遍历，用中序遍历前驱节点的右指针prev->right表示左子树是否访问过
+        // prev->right为空 => 左子树未访问过，记住要返回到当前节点，进入左子树
+        // prev->right非空 => 左子树已访问过，清空prev->right，倒序输出从左子树根节点current->left往右直到前驱节点prev这路径上的各个节点，进入右子树
+        TreeNode dummy(-1);
+        dummy.left = root;
+        auto current = &dummy;
+        TreeNode *prev = NULL;
+        vector<int> result;
+        while (current) {
+            if (!current->left) {
+                current = current->right;
+            } else {
+                // 找到中序遍历前驱结点（左子树的最右节点）
+                prev = current->left;
+                while (prev->right && prev->right != current) {
+                    prev = prev->right;
+                }
+                if (!prev->right) { // 左子树未访问过
+                    prev->right = current;
+                    current = current->left;
+                } else { // 左子树已访问过
+                    prev->right = NULL;
+                    reverseVisit(current->left, prev, result);
+                    current = current->right;
+                }
+            }
+        }
+        return result;
+    }
+private:
+    void reverseVisit(TreeNode *from, TreeNode *to, vector<int> &result) {
+        if (!from) return;
+        reverseVisit(from->right, to, result);
+        result.push_back(from->val);
     }
 };
 
