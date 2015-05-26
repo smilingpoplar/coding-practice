5e18199752e9afd3c8f1e9c0e1c247373c441013
smilingpoplar <smilingpoplar@gmail.com>
2015-05-26 22:09:41 +0800

    combinations

---
+++ leetcode/combinations.cpp
@@ -0,0 +1,48 @@
+//
+//  combinations
+//  https://leetcode.com/problems/combinations/
+//
+//  Created by smilingpoplar on 15/5/26.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> combination;
+        dfs(1, n, k, combination, result);
+        return result;
+    }
+private:
+    void dfs(int start, int n, int k,
+             vector<int> &combination, vector<vector<int>> &result) {
+        if (k == 0) {
+            result.push_back(combination);
+            return;
+        }
+        for (int i = start; i <= n; ++i) {
+            combination.push_back(i);
+            dfs(i + 1, n, k - 1, combination, result);
+            combination.pop_back();
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution solution;
+    auto result = solution.combine(4, 2);
+    for (const auto &combination : result) {
+        for (int num : combination) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
