3a9303f3773b0dc8c6786fbb0f06e8c304a1295c
smilingpoplar <smilingpoplar@gmail.com>
2015-06-03 21:08:29 +0800

    implement-strstr

---
+++ leetcode/implement-strstr.cpp
@@ -0,0 +1,32 @@
+//
+//  implement-strstr
+//  https://leetcode.com/problems/implement-strstr/
+//
+//  Created by smilingpoplar on 15/6/3.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        const int M = (int)haystack.size();
+        const int N = (int)needle.size();
+        for (int i = 0; i + N <= M; ++i) {
+            int j = 0;
+            while (j < N && haystack[i + j] == needle[j]) ++j;
+            if (j == N) return i;
+        }
+        return -1;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution solution;
+    cout << solution.strStr("good luck", "luck");
+    
+    return 0;
+}
