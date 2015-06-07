5ce32ea8958604b9713c69b3ab41da8eae4d3560
smilingpoplar <smilingpoplar@gmail.com>
2015-06-08 00:44:38 +0800

    pascals-triangle-ii

--- leetcode/pascals-triangle-ii.cpp
+++ leetcode/pascals-triangle-ii.cpp
@@ -13,17 +13,16 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> getRow(int k) {
-        // pascal三角的通项：p(i,j) = p(i-1,j-1) + p(i-1,j)，1<=i<=k && 1<=j<k
-        // 在i的递增循环j的递减循环中降维，p(j) = p(j-1) + p(j)
-        if (k < 0) return {};
-        vector<int> p(k + 1, 0);
-        p[0] = 1;
-        for (int i = 1; i <= k; ++i) {
-            for (int j = k - 1; j >= 1; --j) {
+    vector<int> getRow(int rowIndex) {
+        // pascal三角的通项：p(i,j) = p(i-1,j-1) + p(i-1,j)，1<=j<i<=rowIndex
+        // 在i的递增循环j的递减循环中降维：p(j) = p(j-1) + p(j)，1<=j<i<=rowIndex
+        if (rowIndex < 0) return {};
+        vector<int> p;
+        for (int i = 0; i <= rowIndex; ++i) {
+            for (int j = i - 1; j >= 1; --j) { // i>=2进循环
                 p[j] += p[j - 1];
             }
-            p[k] = 1; // 行末的1
+            p.push_back(1); // 行末的1
         }
         return p;
     }
@@ -31,7 +30,7 @@ public:
 
 int main(int argc, const char * argv[]) {
     Solution solution;
-    auto result = solution.getRow(20);
+    auto result = solution.getRow(5);
     for (int num : result) {
         cout << num << " ";
     }
