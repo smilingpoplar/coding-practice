1946ecd76aefd7ee71609dc6645ab68f26508417
smilingpoplar <smilingpoplar@gmail.com>
2015-06-16 03:24:33 +0800

    longest-common-prefix

---
+++ leetcode/longest-common-prefix.cpp
@@ -0,0 +1,38 @@
+//
+//  longest-common-prefix
+//  https://leetcode.com/problems/longest-common-prefix/
+//
+//  Created by smilingpoplar on 15/6/16.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        int longest = (int)strs[0].size();
+        for (int i = 1; i < strs.size(); ++i) {
+            longest = min(longest, (int)strs[i].size());
+            for (int j = 0; j < longest; ++j) {
+                if (strs[0][j] != strs[i][j]) {
+                    longest = j;
+                    break;
+                }
+            }
+        }
+        return strs[0].substr(0, longest);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    vector<string> strs = {"abc", "abd", "abe"};
+    Solution solution;
+    cout << solution.longestCommonPrefix(strs);
+    
+    return 0;
+}
