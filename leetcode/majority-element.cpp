d0dc1ddbc78cf3f11107974c2202e57b64668cd4
smilingpoplar <smilingpoplar@gmail.com>
2015-06-18 00:43:27 +0800

    majority-element

---
+++ leetcode/majority-element.cpp
@@ -0,0 +1,39 @@
+//
+//  majority-element
+//  https://leetcode.com/problems/majority-element/
+//
+//  Created by smilingpoplar on 15/6/17.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate = INT_MIN;
+        int count = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+                ++count;
+            } else if (num == candidate) {
+                ++count;
+            } else {
+                --count;
+            }
+        }
+        return candidate;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    vector<int> nums = {6,5,5};
+    Solution solution;
+    cout << solution.majorityElement(nums);
+ 
+    return 0;
+}
