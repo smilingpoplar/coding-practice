bc4931805f8165702542da66cc256290d9cac26e
smilingpoplar <smilingpoplar@gmail.com>
2015-06-23 01:05:04 +0800

    isomorphic-strings

---
+++ leetcode/isomorphic-strings.cpp
@@ -0,0 +1,36 @@
+//
+//  isomorphic-strings
+//  https://leetcode.com/problems/isomorphic-strings/
+//
+//  Created by smilingpoplar on 15/6/23.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if (s.size() != t.size()) return false;
+        unordered_map<char, char> mapping;
+        unordered_map<char, char> rMapping;
+        for (int i = 0; i < s.size(); ++i) {
+            if (mapping.find(s[i]) != mapping.end() && mapping[s[i]] != t[i]) return false; // 一对多
+            if (rMapping.find(t[i]) != rMapping.end() && rMapping[t[i]] != s[i]) return false; // 多对一
+            mapping[s[i]] = t[i];
+            rMapping[t[i]] = s[i];
+        }
+        return true;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution solution;
+    cout << solution.isIsomorphic("bar", "foo") << endl; // 0
+    cout << solution.isIsomorphic("paper", "title") << endl; // 1
+    
+    return 0;
+}
