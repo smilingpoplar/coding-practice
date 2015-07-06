de60a35dad2bac24d968a47db9eed7d83a2e5abc
smilingpoplar <smilingpoplar@gmail.com>
2015-07-06 21:10:22 +0800

    power-of-two

---
+++ leetcode/power-of-two.cpp
@@ -0,0 +1,25 @@
+//
+//  power-of-two
+//  https://leetcode.com/problems/power-of-two/
+//
+//  Created by smilingpoplar on 15/7/6.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution solution;
+    cout << solution.isPowerOfTwo(16);
+    
+    return 0;
+}
