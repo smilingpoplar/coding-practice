c20f102bead3565b2c9a8f0709c171a3b3f0f22f
smilingpoplar <smilingpoplar@gmail.com>
2015-08-21 15:06:11 +0800

    ugly-number

---
+++ leetcode/ugly-number.cpp
@@ -0,0 +1,33 @@
+//
+//  ugly-number.cpp
+//  https://leetcode.com/problems/ugly-number/
+//
+//  Created by smilingpoplar on 15/5/21.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) return false;
+        const vector<int> factors = {2, 3, 5};
+        for (auto factor : factors) {
+            while (num % factor == 0) {
+                num /= factor;
+            }
+        }
+        return num == 1;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    Solution solution;
+    cout << solution.isUgly(30);
+    
+    return 0;
+}
