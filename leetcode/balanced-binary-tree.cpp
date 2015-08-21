a5e10c06b8c796a8b7733712b44ee057ef315ab3
smilingpoplar <smilingpoplar@gmail.com>
2015-08-21 19:29:21 +0800

    balanced-binary-tree

---
+++ leetcode/balanced-binary-tree.cpp
@@ -0,0 +1,49 @@
+//
+//  balanced-binary-tree
+//  https://leetcode.com/problems/balanced-binary-tree/
+//
+//  Created by smilingpoplar on 15/8/21.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        return balancedHeight(root) >= 0;
+    }
+private:
+    int balancedHeight(TreeNode *root) {
+        // 返回树的高度（balanced）或返回-1（non-balanced）
+        if (!root) return 0;
+        int left = balancedHeight(root->left);
+        int right = balancedHeight(root->right);
+        if (left < 0 || right < 0 || abs(left - right) > 1) return -1;
+        return 1 + max(left, right);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    TreeNode t(1);
+    TreeNode t0(2);
+    t.left = &t0;
+    TreeNode t1(3);
+    t.right = &t1;
+    TreeNode t01(4);
+    t0.right = &t01;
+    
+    Solution solution;
+    cout << solution.isBalanced(&t);
+    
+    return 0;
+}
