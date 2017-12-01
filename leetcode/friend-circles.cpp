556200fda9a93c7c02c67e3dff0bd93476345188
smilingpoplar <smilingpoplar@gmail.com>
2017-12-01 13:02:06 +0800

    update

---
+++ leetcode/friend-circles.cpp
@@ -0,0 +1,42 @@
+//
+//  friend-circles
+//  https://leetcode.com/problems/friend-circles/
+//
+//  Created by smilingpoplar on 17/12/01.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // 求连通图的个数，dfs
+    int findCircleNum(vector<vector<int>>& M) {
+        const int n = (int)M.size();
+        vector<bool> visited(n, false);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(M, visited, i);
+                count++;
+            }
+        }
+        return count;        
+    }
+
+    void dfs(vector<vector<int>>& M, vector<bool>& visited, int i) {
+        visited[i] = true;
+        for (int j = 0; j < M[i].size(); j++) {
+            if (M[i][j] == 1 && !visited[j]) {
+                dfs(M, visited, j);
+            }
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
