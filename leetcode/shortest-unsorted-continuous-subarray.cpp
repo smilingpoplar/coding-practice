1046e694b56aa961a3b315a2d110433b18ebb8e9
smilingpoplar <smilingpoplar@gmail.com>
2017-12-01 17:32:55 +0800

    update

---
+++ leetcode/shortest-unsorted-continuous-subarray.cpp
@@ -0,0 +1,42 @@
+//
+//  shortest-unsorted-continuous-subarray
+//  https://leetcode.com/problems/shortest-unsorted-continuous-subarray/
+//
+//  Created by smilingpoplar on 17/12/01.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        const int N = (int)nums.size();
+        // 先从两端向内找到第一个无序的位置
+        int i = 0, j = N - 1;
+        while (i < N - 1 && nums[i] <= nums[i + 1])
+            i++;
+        while (j > 0 && nums[j - 1] <= nums[j])
+            j--;
+        if (i >= j) return 0;
+        // 在nums[i..j]中找到minNum和maxNum
+        int minNum = INT_MAX, maxNum = INT_MIN;
+        for (int k = i; k <= j; k++) {
+            minNum = min(minNum, nums[k]);
+            maxNum = max(maxNum, nums[k]);
+        }
+        // 根据nums[i-1]<=minNum、nums[j+1]>=maxNum向外扩展
+        while (i >= 1 && nums[i - 1] > minNum)
+            i--;
+        while (j < N - 1 && nums[j + 1] < maxNum)
+            j++;
+        
+        return j - i + 1;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
