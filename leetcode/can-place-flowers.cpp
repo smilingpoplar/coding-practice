d56b1b8374e12a1b1c0b66a92e27446b2ad97c04
smilingpoplar <smilingpoplar@gmail.com>
2017-12-03 00:14:36 +0800

    update

---
+++ leetcode/can-place-flowers.cpp
@@ -0,0 +1,32 @@
+//
+//  can-place-flowers
+//  https://leetcode.com/problems/can-place-flowers/
+//
+//  Created by smilingpoplar on 17/12/02.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        // 贪心法，能种就种
+        const int len = flowerbed.size();
+        int count = 0;
+        for (int i = 0; i < len; i++) {
+            if (!flowerbed[i] && (i == 0 || !flowerbed[i-1]) && (i == len-1 || !flowerbed[i+1])) {
+                flowerbed[i] = 1;
+                ++count;
+            }
+            if (count >= n) return true;
+        }
+        return false;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
---
+++ leetcode/construct-string-from-binary-tree.cpp
@@ -0,0 +1,40 @@
+//
+//  construct-string-from-binary-tree
+//  https://leetcode.com/problems/construct-string-from-binary-tree/
+//
+//  Created by smilingpoplar on 17/12/02.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* t) {
+        if (!t) return "";
+        ostringstream oss;
+        oss << t->val;
+        // 右非空时左都得输出括号
+        if (t->right) {
+            oss << "(" << tree2str(t->left) << ")" << "(" << tree2str(t->right) << ")";
+        } else if (t->left) {
+            oss << "(" << tree2str(t->left) << ")";
+        }
+        return oss.str();
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
