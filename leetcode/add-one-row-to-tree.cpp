05fec1fe2aee5b84d88a808a754d86c7ea8094d8
smilingpoplar <smilingpoplar@gmail.com>
2017-12-07 14:14:16 +0800

    update

---
+++ leetcode/add-one-row-to-tree.cpp
@@ -0,0 +1,54 @@
+//
+//  add-one-row-to-tree
+//  https://leetcode.com/problems/add-one-row-to-tree/
+//
+//  Created by smilingpoplar on 17/12/03.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int v, int d) {
+        if (d == 1) {
+            auto node = new TreeNode(v);
+            node->left= root;
+            return node;
+        }
+        insert(root, 1, v, d);
+        return root;
+    }
+    
+    // root是当前节点，depth是当前节点深度
+    void insert(TreeNode* root, int depth, int v, int d) {
+        if (!root) return;
+        if (depth == d - 1) { // 要在当前节点的子节点插入
+            auto ln = new TreeNode(v);
+            ln->left = root->left;
+            root->left = ln;
+            
+            auto rn = new TreeNode(v);
+            rn->right = root->right;
+            root->right = rn;                
+        } else {
+            insert(root->left, depth + 1, v, d);
+            insert(root->right, depth + 1, v, d);
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
---
+++ leetcode/sum-of-square-numbers.cpp
@@ -0,0 +1,34 @@
+//
+//  sum-of-square-numbers
+//  https://leetcode.com/problems/sum-of-square-numbers/
+//
+//  Created by smilingpoplar on 17/12/07.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        int i = 0;
+        int j = sqrt(c);
+        while (i <= j) {
+            int sum = i * i + j * j;
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                ++i;
+            } else {
+                --j;
+            }
+        }
+        return false;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
