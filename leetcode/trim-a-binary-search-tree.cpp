2299513f6b6fe3a93164d75c692dc3daec39ae5a
smilingpoplar <smilingpoplar@gmail.com>
2018-01-18 17:03:40 +0800

    update

---
+++ leetcode/trim-a-binary-search-tree.cpp
@@ -0,0 +1,37 @@
+//
+//  trim-a-binary-search-tree
+//  https://leetcode.com/problems/trim-a-binary-search-tree/
+//
+//  Created by smilingpoplar on 18/01/18.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        if (!root) return root;
+        if (root->val > R) return trimBST(root->left, L, R);
+        if (root->val < L) return trimBST(root->right, L, R);
+            
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        return root;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
