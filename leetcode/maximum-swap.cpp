2ef98ee180a3cf9dca039bea487c438b8de2db4e
smilingpoplar <smilingpoplar@gmail.com>
2018-01-18 17:30:27 +0800

    update

---
+++ leetcode/maximum-swap.cpp
@@ -0,0 +1,39 @@
+//
+//  maximum-swap
+//  https://leetcode.com/problems/maximum-swap/
+//
+//  Created by smilingpoplar on 18/01/18.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximumSwap(int num) {
+        // 从左往右看各位置，看有没有尽量大的比当前数字大的数字在后面，交换
+        string s = to_string(num);
+        vector<int> pos(10, -1);
+        for (int i = 0; i < s.size(); ++i) {
+            pos[s[i] - '0'] = i;
+        }
+        
+        for (int i = 0; i < s.size(); ++i) {
+            for (int x = 9; x > s[i] - '0'; --x) {
+                if (pos[x] > i) {
+                    char c = s[pos[x]];
+                    s[pos[x]] = s[i];
+                    s[i] = c;
+                    return stoi(s);
+                }
+            }
+        }
+        return num;        
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
