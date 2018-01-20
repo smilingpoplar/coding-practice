e8d638a4307adc443480a6de64786906a6a80940
smilingpoplar <smilingpoplar@gmail.com>
2018-01-21 00:50:30 +0800

    update

--- leetcode/best-time-to-buy-and-sell-stock-ii.cpp
+++ leetcode/best-time-to-buy-and-sell-stock-ii.cpp
@@ -7,14 +7,34 @@
 //
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
+
+/*
+class Solution {
+public:    
+    int maxProfit(vector<int>& prices) {
+        // 设dp[i][s]表示第i天、手上有s股股票时的最大利润，s=0或1
+        // dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i] /*卖股票*/)
+        // dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i] /*买股票*/)
+        // 初始dp[-1][0]=0，dp[-1][1]=INT_MIN
+        // dp[i][]只依赖dp[i-1][]，降维
+        int share0 = 0;
+        int share1 = INT_MIN;
+        for (int price : prices) {
+            int oldShare0 = share0;
+            share0 = max(share0, share1 + price);
+            share1 = max(share1, oldShare0 - price);
+        }
+        return share0;
+    }
+};
+*/
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // 可以买卖多次，那么在价格上升时买入后卖出
+        // 可以无限次购买，相隔两数有利可图就购买
         if (prices.size() < 2) return 0;
         int maxProfit = 0;
         for (auto i = 0; i < prices.size() - 1; i++) {
