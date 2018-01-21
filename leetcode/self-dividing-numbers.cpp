c4f9d6b5b3ecbfd7f9854f430ff2333b6d36f332
smilingpoplar <smilingpoplar@gmail.com>
2018-01-21 15:40:58 +0800

    update

---
+++ leetcode/self-dividing-numbers.cpp
@@ -0,0 +1,38 @@
+//
+//  self-dividing-numbers/
+//  https://leetcode.com/problems/self-dividing-numbers//
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for (int i = left; i <= right; ++i) {
+            if (isSelfDividingNumber(i)) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+    
+    bool isSelfDividingNumber(int num) {
+        int n = num;
+        while (n) {
+            int d = n % 10;
+            if (d == 0 || num % d != 0) return false;
+            n /= 10;                
+        }
+        return true;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
