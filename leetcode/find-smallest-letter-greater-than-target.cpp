a33cce62bb7f131ac0d32ca689c956f66a1391f3
smilingpoplar <smilingpoplar@gmail.com>
2018-01-21 23:07:03 +0800

    update

---
+++ leetcode/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,32 @@
+//
+//  find-smallest-letter-greater-than-target
+//  https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l = -1, u = letters.size();
+        while (l + 1 < u) {
+            int mid = l + (u - l) / 2;
+            if (letters[mid] > target) {
+                u = mid;
+            } else {
+                l = mid;
+            }
+        }
+        if (u == letters.size()) return letters[0];
+        return letters[u];
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
