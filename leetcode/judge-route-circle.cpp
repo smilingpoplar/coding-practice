03025463f4cbf433f45261842966b58394d04f19
smilingpoplar <smilingpoplar@gmail.com>
2018-01-24 01:28:04 +0800

    update

---
+++ leetcode/judge-route-circle.cpp
@@ -0,0 +1,29 @@
+//
+//  judge-route-circle
+//  https://leetcode.com/problems/judge-route-circle/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int up = 0, left = 0;
+        for (char m : moves) {
+            if (m == 'U') ++up;
+            else if (m == 'D') --up;
+            else if (m == 'L') ++left;
+            else if (m == 'R') --left;
+        }
+        return up == 0 && left == 0;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
