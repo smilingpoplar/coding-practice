9f952a4e676d800ebf03af08708df065bab10ff7
smilingpoplar <smilingpoplar@gmail.com>
2018-01-24 02:03:01 +0800

    update

---
+++ leetcode/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,41 @@
+//
+//  reverse-words-in-a-string-iii
+//  https://leetcode.com/problems/reverse-words-in-a-string-iii/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left = 0;
+        int i = 0;
+        while (i < s.size()) {
+            while (s[i] == ' ' && i < s.size()) ++i;
+            left = i;
+            while (s[i] != ' ' && i < s.size()) ++i;
+            reverse(s, left, i - 1);
+        }
+        return s;
+    }
+    
+    void reverse(string &s, int i, int j) {
+        while (i < j) {
+            char c = s[i];
+            s[i] = s[j];
+            s[j] = c;
+            ++i;
+            --j;
+        }
+    }
+        
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
