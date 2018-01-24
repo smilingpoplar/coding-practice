8b777bcf33b3ef25c2152f4fbe0f46bbdca0f2e5
smilingpoplar <smilingpoplar@gmail.com>
2018-01-24 13:41:00 +0800

    update

---
+++ leetcode/dota2-senate.cpp
@@ -0,0 +1,38 @@
+//
+//  dota2-senate
+//  https://leetcode.com/problems/dota2-senate/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        const int N = senate.size();
+        queue<int> R, D;
+        for (int i = 0; i < N; ++i) {
+            if (senate[i] == 'R') R.push(i);
+            else D.push(i);
+        }
+        while (!R.empty() && !D.empty()) {
+            int r = R.front(); R.pop();
+            int d = D.front(); D.pop();
+            if (r < d) {
+                R.push(r + N);
+            } else {
+                D.push(d + N);
+            }
+        }
+        if (!R.empty()) return "Radiant";
+        return "Dire";
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
