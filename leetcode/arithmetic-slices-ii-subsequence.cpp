59167afc9e5c9038bc958628634c3ac3a816f0d1
smilingpoplar <smilingpoplar@gmail.com>
2018-01-25 18:54:51 +0800

    update

--- leetcode/arithmetic-slices-ii-subsequence.cpp
+++ leetcode/arithmetic-slices-ii-subsequence.cpp
@@ -13,18 +13,21 @@ using namespace std;
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
-        int sum = 0;
-        int n = A.size();
-        vector<map<int,int>> dp(n);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
+        // 设dp[i][d]表示A[0..i]以A[i]结尾、等差为d、长度>=2的等差序列数
+        // 对所有j<i，d=A[i]-A[j]，dp[i][d] += dp[j][d]+1，dp[j][d]来自变长的旧序列，1来自新出现的长度==2序列
+        // 初始dp[0][]=0
+        const int N = A.size();
+        vector<map<int,int>> dp(N);
+        int ans = 0;
+        for (int i = 1; i < N; ++i) {
+            for (int j = 0; j < i; ++j) {
                 long d = (long)A[i] - A[j];
                 if (d < INT_MIN || d > INT_MAX) continue; // pass OL
-                dp[i][d] += dp[j][d] + 1; // 两元素也算序列的“扩展子序列”
-                sum += dp[j][d]; // 至少三个元素的子序列                   
+                dp[i][d] += dp[j][d] + 1; // dp[j][d]来自变长的旧序列，长度>=3
+                ans += dp[j][d];
             }
         }
-        return sum;
+        return ans;
     }
 };
 
