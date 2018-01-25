b7ab43c649e191dd8ae84ee338764b388f7779ca
smilingpoplar <smilingpoplar@gmail.com>
2018-01-26 00:40:13 +0800

    update

--- leetcode/k-inverse-pairs-array.cpp
+++ leetcode/k-inverse-pairs-array.cpp
@@ -14,16 +14,18 @@ class Solution {
 public:
     int kInversePairs(int n, int k) {
         if (k > n * (n - 1) / 2) return 0;
-        const int M = 1000000007;
+        const int MOD = 1000000007;
+        // 初始dp[0][]=0，dp[i>=1][0]=1，dp[1][j>0]=0，最上两行和最左列都初始化了
         vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
         for (int i = 1; i <= n; i++) {
             dp[i][0] = 1;
         }
         
-        for (int i = 2; i <= n; i++) { // i>=2个元素才有逆序对
+        for (int i = 2; i <= n; i++) {
             for (int j = 1; j <= k; j++) {
-                int val = (dp[i][j - 1] + dp[i-1][j]) % M;
-                dp[i][j] = (val + M - (j >= i ? dp[i - 1][j - i] : 0)) % M;
+                int val = (dp[i][j - 1] + dp[i-1][j]) % MOD;
+                if (j - i >= 0) val = (val - dp[i-1][j-i] + MOD) % MOD;
+                dp[i][j] = val;
             }
         }
         return dp[n][k];
