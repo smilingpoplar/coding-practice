f20a43ca9c34d728c0cc568c9865facc4f2eae55
smilingpoplar <smilingpoplar@gmail.com>
2018-01-26 22:25:35 +0800

    update

--- leetcode/burst-balloons.cpp
+++ leetcode/burst-balloons.cpp
@@ -13,7 +13,7 @@ using namespace std;
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
-        // nums扩展成a
+        // nums前后添加1扩展成a
         const int N = (int)nums.size() + 2;
         vector<int> a(N);
         a[0] = a[N - 1] = 1;
@@ -21,8 +21,10 @@ public:
             a[i] = nums[i - 1];
         }
 
+        // 设dp[i][j]表示a[i..j]戳气球的最大得分
+        // 考虑最后一戳i<=k<=j，得分a[i-1]*a[k]*a[j+1]，还把数组分成两段dp[i][k-1]和dp[k+1][j]
         vector<vector<int>> dp(N, vector<int>(N, 0));
-        for (int i = N - 1; i >= 1; i--) { // dp[i,..]依赖于它右边的值，逆序遍历
+        for (int i = N - 2; i >= 1; i--) { // dp[i,..]依赖于它右边的值，逆序遍历
             for (int j = i; j < N - 1; j++) { // dp[..,j]依赖于它左边的值，正序遍历
                 for (int k = i; k <= j; k++) {
                     dp[i][j] = max(dp[i][j], a[i-1] * a[k] * a[j+1] + dp[i][k-1] + dp[k+1][j]);
@@ -32,7 +34,6 @@ public:
         return dp[1][N-2];
     }
 };
-
 
 int main(int argc, const char * argv[]) {    
     return 0;
--- leetcode/remove-boxes.cpp
+++ leetcode/remove-boxes.cpp
@@ -14,6 +14,8 @@ class Solution {
 public:
     int removeBoxes(vector<int>& boxes) {
         const int N = boxes.size();
+        // 设dp[i][j][k]表示boxes[i..j]且前面有k个与boxes[i]同色且待拿的盒子时的最大得分
+        // 考虑现在拿boxes[i]还是留着以后和后面同色的一起拿
         vector<vector<vector<int>>> dp(N, vector<vector<int>>(N, vector<int>(N, 0)));
         // i == j
         for (int i = 0; i < N; i++) {
@@ -28,7 +30,7 @@ public:
                     dp[i][j][k] = (k + 1) * (k + 1) + dp[i + 1][j][0]; // 现在拿boxes[i]
                     for (int m = i + 1; m <= j; m++) {
                         if (boxes[m] == boxes[i]) { // 以后和后面同色的boxes[m]一起拿
-                            dp[i][j][k] = max(dp[i][j][k], dp[i + 1][m - 1][0] + dp[m][j][k + 1]);
+                            dp[i][j][k] = max(dp[i][j][k], dp[m][j][k + 1] + dp[i + 1][m - 1][0]);
                         }
                     }
                 }
@@ -37,6 +39,7 @@ public:
         return dp[0][N-1][0];
     }
 };
+
 
 int main(int argc, const char * argv[]) {    
     return 0;
