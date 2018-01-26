d47da372ff001c40fcb87a2ef360c82ec06f6a5f
smilingpoplar <smilingpoplar@gmail.com>
2018-01-27 00:00:54 +0800

    update

--- leetcode/contains-duplicate-ii.cpp
+++ leetcode/contains-duplicate-ii.cpp
@@ -15,12 +15,12 @@ using namespace std;
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> map; // num=>index
+        unordered_map<int, int> mp; // num=>pos
         for (int i = 0; i < nums.size(); i++) {
-            if (map.find(nums[i]) != map.end()) {
-                if (i - map[nums[i]] <= k) return true;
+            if (mp.find(nums[i]) != mp.end()) {
+                if (i - mp[nums[i]] <= k) return true;
             }
-            map[nums[i]] = i;
+            mp[nums[i]] = i;
         }
         return false;
     }
