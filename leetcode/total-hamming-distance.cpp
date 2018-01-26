5289f83be7698d6c12caa2fd90500da5a9015b1f
smilingpoplar <smilingpoplar@gmail.com>
2018-01-26 13:58:35 +0800

    update

--- leetcode/total-hamming-distance.cpp
+++ leetcode/total-hamming-distance.cpp
@@ -13,7 +13,7 @@ using namespace std;
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-        int total = 0;
+        int ans = 0;
         const int numBits = sizeof(int) * 8;
         for (int i = 0; i < numBits; i++) {
             // 考查整组数的第i位
@@ -21,9 +21,9 @@ public:
             for (int num : nums) {
                 if ((num >> i) & 1) ones++;
             }
-            total += ones * (nums.size() - ones);
+            ans += ones * (nums.size() - ones);
         }
-        return total;
+        return ans;
     }
 };
 
