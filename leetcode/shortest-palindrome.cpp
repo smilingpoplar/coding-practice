ee768dec7d2170441ad9ddff33d3203bc3c075f5
smilingpoplar <smilingpoplar@gmail.com>
2018-01-27 23:13:36 +0800

    update

--- leetcode/shortest-palindrome.cpp
+++ leetcode/shortest-palindrome.cpp
@@ -2,47 +2,41 @@
 //  shortest-palindrome
 //  https://leetcode.com/problems/shortest-palindrome/
 //
-//  Created by smilingpoplar on 15/6/15.
+//  Created by smilingpoplar on 18/01/19.
 //  Copyright (c) 2015年 YangLe. All rights reserved.
 //
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        // 在s前面添加最少字符构成回文串，得看s的最长回文前缀有多长，
-        // 也就是看s#s'的最长边界有多长，边界指同时是前缀和后缀的子串，#是某分隔符，s’是s的反转串，
-        // 用KMP的预处理算法，设b[i]表示长为i的前缀的边界长，
-        // 不变式：i为前缀长，j为前缀的边界长，
-        // 1. 若s[i]==s[j]，当前边界可向右扩展1个字符
-        // 2. 若s[i]!=s[j]，当前边界不可扩展，下一待尝试的边界长j=b[j]，因为较短边界是较长边界的边界
-        string sReverse(s.rbegin(), s.rend());
-        string ss = s + '\0' + sReverse;
-        
-        const int N = (int)ss.size();
-        vector<int> b(N + 1, 0);
-        int i = 0;
-        int j = -1;
+        // 求s串首开始的最长回文长
+        string r(s.rbegin(), s.rend());
+        string S = s + "#" + r;
+        // 变成求S前缀等于后缀的最大边界长
+        // 用KMP算法的预处理，设b[i]表示长为i的前缀S[0..i-1]的最大边界长
+        // 不变式：i是前缀长、j是边界长
+        const int N = S.size();
+        vector<int> b(N + 1);
+        int i = 0, j = -1;
         b[i] = j;
         while (i < N) {
-            while (j >= 0 && ss[i] != ss[j]) j = b[j];
-            ++i;
-            ++j;
+            // 尝试扩展到S[i]
+            while (j >= 0 && S[i] != S[j]) // 无法扩展当前边界
+                j = b[j]; // 尝试下一小的边界
+            // 可以扩展了
+            ++i; ++j;
             b[i] = j;
         }
         
-        auto prepend = sReverse.substr(0, s.size() - b[N]);
+        string prepend = r.substr(0, s.size() - b[N]);
         return prepend + s;
     }
 };
 
-int main(int argc, const char * argv[]) {
-    Solution solution;
-    cout << solution.shortestPalindrome("ba");
-    
+int main(int argc, const char * argv[]) {    
     return 0;
 }
