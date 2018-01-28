f41651df27e014b6b0b4a03a42b0d5afe167567d
smilingpoplar <smilingpoplar@gmail.com>
2018-01-28 13:32:26 +0800

    update

--- leetcode/number-of-digit-one.cpp
+++ leetcode/number-of-digit-one.cpp
@@ -13,10 +13,10 @@ using namespace std;
 class Solution {
 public:
     int countDigitOne(int n) {
-        // 看[0,n]的数中有多少个在某一位为1，要看这一位是 >1、=1、=0：
-        //   当前位>1 => (高位数字+1)*当前位基数，如2225的百位（100、...、199、1100、...、1199、2100、...、2199）=> (2+1)*100
-        //   当前位=1 => 高位数字*当前位基数+低位数字+1，如2125的百位（100、...、199、1100、...、1199、2100、...、2125）=> 2*100+26
-        //   当前位=0 => 高位数字*当前位基数，如2025的百位（100、...、199、1100、...、1199）=> 2*100
+        // 看[0,n]的数中有多少个在某一位为1，要看这一位是 <1、>1、=1：
+        //   当前位<1 => 左边数字*当前位基数，如2025的百位（100、...、199、1100、...、1199）=> 2*100
+        //   当前位>1 => (左边数字+1)*当前位基数，如2225的百位（100、...、199、1100、...、1199、2100、...、2199）=> (2+1)*100
+        //   当前位=1 => 左边数字*当前位基数+低位数字+1，如2125的百位（100、...、199、1100、...、1199、2100、...、2125）=> 2*100+26
         if (n <= 0) return 0;
         int count = 0;
         int base = 1;
@@ -24,14 +24,14 @@ public:
             int lower = n % base;
             int current = (n / base) % 10;
             int higher = n / base / 10;
-            if (current > 1) {
-                count += (higher + 1) * base;
-            } else if (current == 1) {
-                count += higher * base + lower + 1;
-            } else {
+            if (current < 1) {
                 count += higher * base;
+            } else if (current > 1) {
+                count += (higher + 1) * base;
+            } else {
+                count += higher * base + lower + 1;
             }
-            if (base > n / 10) break; // 防止溢出，base*10>n
+            if (base > n / 10) break; // 防止溢出，从while(base<=n)改到这儿
             base *= 10;
         };
         return count;
