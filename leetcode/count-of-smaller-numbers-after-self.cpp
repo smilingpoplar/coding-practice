c10ba2c881417030f8db80eb73dd38b08334c3b9
smilingpoplar <smilingpoplar@gmail.com>
2018-01-29 21:47:48 +0800

    update

---
+++ leetcode/count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,58 @@
+//
+//  count-of-smaller-numbers-after-self
+//  https://leetcode.com/problems/count-of-smaller-numbers-after-self/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        // 在idx数组上作归并排序
+        const int N = nums.size();
+        vector<int> idx;
+        for (int i = 0; i < N; i++) {
+            idx.push_back(i);
+        }
+        vector<int> ans(N, 0);
+        mergeSort(nums, idx, 0, N - 1, ans);
+        return ans;
+    }
+    
+    void mergeSort(vector<int> &nums, vector<int> &idx, int l, int r, vector<int> &ans) {
+        if (l >= r) return;
+        int mid = l + (r - l) / 2;
+        mergeSort(nums, idx, l, mid, ans);
+        mergeSort(nums, idx, mid + 1, r, ans);
+        
+        vector<int> merged(r - l + 1);
+        int i = mid, j = r, k = r - l;
+        while (i >= l && j > mid) {
+            // 从两数组末端开始比较
+            if (nums[idx[i]] > nums[idx[j]]) {
+                ans[idx[i]] += j - mid;
+                merged[k--] = idx[i--];
+            } else {
+                merged[k--] = idx[j--];
+            }
+        }
+        while (i >= l) {
+            merged[k--] = idx[i--];
+        }
+        while (j > mid) {
+            merged[k--] = idx[j--];
+        }
+        for (int k = 0; k < merged.size(); k++) {
+            idx[l + k] = merged[k];
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
