551bded2a184597abcc705d9ea042d57639c9c0c
smilingpoplar <smilingpoplar@gmail.com>
2018-02-03 18:38:38 +0800

    update

---
+++ leetcode/4sum-ii.cpp
@@ -0,0 +1,37 @@
+//
+//  4sum-ii
+//  https://leetcode.com/problems/4sum-ii/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        map<int, int> count; // sum=>count
+        for (int a : A) {
+            for (int b : B) {
+                count[a + b]++;
+            }
+        }
+        int ans = 0;
+        for (int c : C) {
+            for (int d : D) {
+                int sum = c + d;
+                if (count.find(-sum) != count.end()) {
+                    ans += count[-sum];
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
---
+++ leetcode/guess-number-higher-or-lower.cpp
@@ -0,0 +1,36 @@
+//
+//  guess-number-higher-or-lower
+//  https://leetcode.com/problems/guess-number-higher-or-lower/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, u = n;
+        while (l <= u) {
+            int mid = l + (u - l) / 2;
+            if (guess(mid) <= 0) { // guess(): 1 1 0 -1 -1 变成返回 0 0 1 1 1
+                u = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
