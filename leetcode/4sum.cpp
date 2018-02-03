6142d1efa15446a3fd395cb93a1c28c8f3cbd985
smilingpoplar <smilingpoplar@gmail.com>
2018-02-03 18:05:23 +0800

    update

--- leetcode/4sum.cpp
+++ leetcode/4sum.cpp
@@ -14,36 +14,35 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> result;
+        vector<vector<int>> ans;
         const int N = (int)nums.size();
-        if (N < 4) return result;
+        if (N < 4) return ans;
         sort(nums.begin(), nums.end());
         
-        for (int first = 0; first < N - 3; ++first) {
-            while (0 < first && first < N - 3 && nums[first] == nums[first - 1]) ++first;
-            for (int second = first + 1; second < N - 2; ++second) {
-                while (first + 1 < second && second < N - 2 && nums[second] == nums[second - 1]) ++second;
-                int third = second + 1;
-                int fourth = N - 1;
-                while (third < fourth) {
-                    int sum = nums[first] + nums[second] + nums[third] + nums[fourth];
+        for (int ia = 0; ia < N - 3; ++ia) {
+            while (0 < ia && ia < N - 3 && nums[ia] == nums[ia - 1]) ++ia;
+            for (int ib = ia + 1; ib < N - 2; ++ib) {
+                while (ia + 1 < ib && ib < N - 2 && nums[ib] == nums[ib - 1]) ++ib;
+                int ic = ib + 1, id = N - 1;
+                while (ic < id) {
+                    int sum = nums[ia] + nums[ib] + nums[ic] + nums[id];
                     if (sum == target) {
-                        result.push_back({nums[first], nums[second], nums[third], nums[fourth]});
-                        ++third;
-                        while (third < fourth && nums[third] == nums[third - 1]) ++third;
-                        --fourth;
-                        while (third < fourth && nums[fourth] == nums[fourth + 1]) --fourth;
+                        ans.push_back({nums[ia], nums[ib], nums[ic], nums[id]});
+                        ++ic;
+                        while (ic < id && nums[ic] == nums[ic - 1]) ++ic;
+                        --id;
+                        while (ic < id && nums[id] == nums[id + 1]) --id;
                     } else if (sum < target) {
-                        ++third;
-                        while (third < fourth && nums[third] == nums[third - 1]) ++third;
+                        ++ic;
+                        while (ic < id && nums[ic] == nums[ic - 1]) ++ic;
                     } else {
-                        --fourth;
-                        while (third < fourth && nums[fourth] == nums[fourth + 1]) --fourth;
+                        --id;
+                        while (ic < id && nums[id] == nums[id + 1]) --id;
                     }
                 }
             }
         }
-        return result;
+        return ans;
     }
 };
 
@@ -51,8 +50,8 @@ int main(int argc, const char * argv[]) {
     vector<int> nums = {1, 0, -1, 0, -2, 2};
     int target = 0;
     Solution solution;
-    auto result = solution.fourSum(nums, target);
-    for (const auto &quad : result) {
+    auto ans = solution.fourSum(nums, target);
+    for (const auto &quad : ans) {
         for (auto num : quad) {
             cout << num << " ";
         }
