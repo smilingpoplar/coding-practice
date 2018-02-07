4039af848d199892080024719cee7a40cbc577f8
smilingpoplar <smilingpoplar@gmail.com>
2018-02-07 23:21:24 +0800

    update

--- leetcode/interleaving-string.cpp
+++ leetcode/interleaving-string.cpp
@@ -14,34 +14,28 @@ using namespace std;
 class Solution {
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        // 动态规划，设f(i,j)表示s1[0,i)和s2[0,j)能interleaving成s3[0,i+j) (0<=i<=M, 0<=j<=N)
-        // 若 s1[i-1] == s3[i+j-1]，则 f(i,j) = f(i-1,j)
-        // 若 s2[j-1] == s3[i+j-1]，则 f(i,j) = f(i,j-1)
-        // 故 f(i,j) = (s1[i-1] == s3[i+j-1] && f(i-1,j) || (s2[j-1] == s3[i+j-1] && f(i,j-1))
-        
-        const size_t M = s1.size();
-        const size_t N = s2.size();
+        // 设dp[i][j]表示s1[0..i-1]和s2[0..j-1]能否编织出s3[0..i+j-1]
+        // 若s1[i-1]==s3[i+j-1]，dp[i][j]=dp[i-1][j]；若s2[j-1]==s3[i+j-1]，dp[i][j]=dp[i][j-1]；
+        // 否则dp[i][j]=false。初始dp[0][0]=true
+        const int M = s1.size();
+        const int N = s2.size();
         if (s3.size() != M + N) return false;
         
-        vector<vector<bool>> f(M + 1, vector<bool>(N + 1, false));
-        f[0][0] = true;
-        for (size_t i = 1; i <= M; i++) {
-            if (s1[i - 1] == s3[i - 1]) {
-                f[i][0] = f[i - 1][0];
+        vector<vector<bool>> dp(M + 1, vector<bool>(N + 1, false));
+        dp[0][0] = true;
+        for (int i = 0; i <= M; i++) {
+            for (int j = 0; j <= N; j++) {
+                if (i > 0 && s1[i-1] == s3[i+j-1]) {
+                    dp[i][j] = dp[i-1][j];
+                    if (dp[i][j]) continue;
+                }
+                if (j > 0 && s2[j-1] == s3[i+j-1]) {
+                    dp[i][j] = dp[i][j-1];
+                    if (dp[i][j]) continue;
+                }
             }
         }
-        for (size_t j = 1; j <= N; j++) {
-            if (s2[j - 1] == s3[j - 1]) {
-                f[0][j] = f[0][j - 1];
-            }
-        }
-        for (size_t i = 1; i <= M; i++) {
-            for (size_t j = 1; j <= N; j++) {
-                f[i][j] = (s1[i - 1] == s3[i + j - 1] && f[i - 1][j]) || (s2[j - 1] == s3[i + j - 1] && f[i][j - 1]);
-            }
-        }
-        
-        return f[M][N];
+        return dp[M][N];
     }
 };
 
