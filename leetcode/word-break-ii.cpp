b3cfdb91c8fe987cf59bd09ae067a29f91953c9c
smilingpoplar <smilingpoplar@gmail.com>
2018-02-09 23:26:36 +0800

    update

--- leetcode/word-break-ii.cpp
+++ leetcode/word-break-ii.cpp
@@ -15,59 +15,34 @@ using namespace std;
 
 class Solution {
 public:
-    vector<string> wordBreak(string s, unordered_set<string>& dict) {
-        // 动态规划，设ok(i)表示s[0,i)可break，breakAt(i,j)表示s[0,i)可在j处break成s[0,j)和s[j,i)
-        // (s[0,j)是子问题，s[j,i)是单词，0<=i<=N, 0<=j<i)
-        const size_t N = s.size();
-        vector<bool> ok(N + 1, false);
-        vector<vector<bool>> breakAt(N + 1, vector<bool>(N, false));
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict;
+        dict.insert(wordDict.begin(), wordDict.end());
+        unordered_map<int, vector<string>> memo; 
+        return search(s, 0, dict, memo);
+    }
+    
+    // 为s[idx..]生成句子
+    vector<string> search(const string &s, int idx, unordered_set<string>& dict,
+                          unordered_map<int, vector<string>> &memo) {
+        if (memo.find(idx) != memo.end()) return memo[idx];
         
-        ok[0] = true;
-        for (size_t i = 1; i <= N; i++) {
-            for (size_t j = 0; j < i; j++) {
-                if (ok[j] && dict.find(s.substr(j, i - j)) != dict.end()) {
-                    ok[i] = true;
-                    breakAt[i][j] = true;
-                }
+        vector<string> ans;
+        for (int i = idx; i < s.size(); i++) {
+            auto word = s.substr(idx, i - idx + 1);
+            if (dict.find(word) == dict.end()) continue;
+            if (i == s.size() - 1) {
+                ans.push_back(word);
+            } else {
+                auto suffixes = search(s, i + 1, dict, memo);
+                for (auto &suffix : suffixes) {
+                    ans.push_back(word + " " + suffix);
+                }                
             }
         }
         
-        return generateSentences(s, breakAt);
-    }
-private:
-    vector<string> generateSentences(const string &s, const vector<vector<bool>> &breakAt) {
-        vector<string> result;
-        vector<string> path;
-        generateSentences(s.size(), s, breakAt, path, result);
-        return result;
-    }
-    
-    // 为s[0,i)生成句子
-    void generateSentences(size_t i, const string &s, const vector<vector<bool>> &breakAt, vector<string> &path, vector<string> &result) {
-        if (i == 0) { // 终止条件
-            auto sentence = generateSentence(path);
-            result.push_back(sentence);
-            return;
-        }
-        
-        for (size_t j = 0; j < i; j++) {
-            if (breakAt[i][j]) {
-                path.push_back(s.substr(j, i - j));
-                generateSentences(j, s, breakAt, path, result);
-                path.pop_back();
-            }
-        }
-    }
-    
-    string generateSentence(const vector<string> &path) {
-        if (path.empty()) return "";
-        const int N = (int)path.size();
-        ostringstream oss;
-        oss << path[N - 1];
-        for (int i = N - 2; i >= 0; i--) {
-            oss << " " << path[i];
-        }
-        return oss.str();
+        memo[idx] = ans;
+        return ans;
     }
 };
 
