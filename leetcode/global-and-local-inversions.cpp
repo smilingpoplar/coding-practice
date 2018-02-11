48b0bde2d912ff159c7269bacf5c28e419c7e73c
smilingpoplar <smilingpoplar@gmail.com>
2018-02-11 23:27:22 +0800

    update

---
+++ leetcode/global-and-local-inversions.cpp
@@ -0,0 +1,29 @@
+//
+//  global-and-local-inversions
+//  https://leetcode.com/problems/global-and-local-inversions/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isIdealPermutation(vector<int>& A) {
+        // 全局逆序数==局部逆序数，即对所有j、0<=i<=j-2，应有A[i]<=A[j]，max(A[0..j-2])<=A[j]
+        int theMax = -1;
+        for (int j = 2; j < A.size(); j++) {
+            theMax = max(theMax, A[j-2]);
+            if (theMax > A[j]) return false;
+        }
+        return true;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
