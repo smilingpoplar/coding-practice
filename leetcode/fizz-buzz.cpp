1a7b9b36a9e03c4feff08c85a44a35cd6772255a
smilingpoplar <smilingpoplar@gmail.com>
2018-02-11 16:50:58 +0800

    update

---
+++ leetcode/fizz-buzz.cpp
@@ -0,0 +1,30 @@
+//
+//  fizz-buzz
+//  https://leetcode.com/problems/fizz-buzz/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for (int i = 1; i <= n; i++) {
+            if (i % 15 == 0) ans.push_back("FizzBuzz");
+            else if (i % 3 == 0) ans.push_back("Fizz");
+            else if (i % 5 == 0) ans.push_back("Buzz");
+            else ans.push_back(to_string(i));
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ leetcode/shuffle-an-array.cpp
@@ -0,0 +1,48 @@
+//
+//  shuffle-an-array
+//  https://leetcode.com/problems/shuffle-an-array/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    vector<int> origin;
+public:
+    Solution(vector<int> nums) {
+        origin = nums;
+        srand(time(NULL));
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return origin;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        vector<int> ans(origin);
+        const int N = ans.size();
+        for (int i = 0; i < N; i++) {
+            int j = i + rand() % (N - i);
+            swap(ans[i], ans[j]);
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * vector<int> param_1 = obj.reset();
+ * vector<int> param_2 = obj.shuffle();
+ */
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
