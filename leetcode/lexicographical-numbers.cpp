53c5d976e431bf9e6cb749736becf994da964a96
smilingpoplar <smilingpoplar@gmail.com>
2018-02-17 12:29:38 +0800

    update

---
+++ leetcode/lexicographical-numbers.cpp
@@ -0,0 +1,36 @@
+//
+//  lexicographical-numbers
+//  https://leetcode.com/problems/lexicographical-numbers/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        // 十叉树的先序遍历
+        vector<int> ans;
+        for (int i = 1; i <= 9; i++) {
+            preorder(i, n, ans);
+        }
+        return ans;
+    }
+    
+    void preorder(int x, int n, vector<int> &ans) {
+        if (x > n) return;
+        ans.push_back(x);
+        for (int i = 0; i <= 9; i++) {
+            preorder(x * 10 + i, n, ans);
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
