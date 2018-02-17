f253229784de25357b1073114f1e71817a91e653
smilingpoplar <smilingpoplar@gmail.com>
2018-02-17 12:42:26 +0800

    update

--- leetcode/lexicographical-numbers.cpp
+++ leetcode/lexicographical-numbers.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+/*
 class Solution {
 public:
     vector<int> lexicalOrder(int n) {
@@ -28,6 +29,28 @@ public:
         for (int i = 0; i <= 9; i++) {
             preorder(x * 10 + i, n, ans);
         }
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        // 十叉树的先序遍历
+        vector<int> ans;
+        stack<int> stk;
+        for (int i = 9; i >= 1; i--) {
+            if (i <= n) stk.push(i);
+        }
+        while (!stk.empty()) {
+            int x = stk.top(); stk.pop();
+            ans.push_back(x);
+            for (int i = 9; i >= 0; i--) {
+                int next = x * 10 + i;
+                if (next <= n) stk.push(next);
+            }
+        }
+        return ans;
     }
 };
 
