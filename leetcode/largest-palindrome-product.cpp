09e98703b5bea9942b1f94030219850328a642a4
smilingpoplar <smilingpoplar@gmail.com>
2018-02-20 21:47:52 +0800

    update

---
+++ leetcode/largest-palindrome-product.cpp
@@ -0,0 +1,41 @@
+//
+//  largest-palindrome-product
+//  https://leetcode.com/problems/largest-palindrome-product/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestPalindrome(int n) {
+        if (n == 1) return 9;
+        // 穷举两个n位数构成的回文，看它能否分解成两个n位数相乘
+        int minNum = pow(10, n-1), maxNum = minNum * 10 - 1;
+        for (int i = maxNum; i >= minNum; i--) {
+            long pali = getPalindrome(i);
+            // 因子j和pali/j：maxNum >= j >= pali/j >= minNum
+            for (long j = maxNum; j * j >= pali; j--) {
+                if (pali % j == 0 && j * minNum <= pali) {
+                    return pali % 1337;
+                }
+            }
+        }
+        return -1;
+    }
+    
+    long getPalindrome(int x) {
+        string s = to_string(x);
+        string pali = s + string(s.rbegin(), s.rend());
+        return stol(pali);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
