5bc11b07c99e0c6cce768edbd86d2f28877e5729
smilingpoplar <smilingpoplar@gmail.com>
2018-02-20 22:48:37 +0800

    update

---
+++ leetcode/relative-ranks.cpp
@@ -0,0 +1,39 @@
+//
+//  relative-ranks
+//  https://leetcode.com/problems/relative-ranks/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& nums) {
+        const int N = nums.size();
+        vector<int> idx;
+        for (int i = 0; i < N; i++)
+            idx.push_back(i);
+        sort(idx.begin(), idx.end(), [&](int i, int j) { return nums[i] > nums[j]; });
+
+        vector<string> ans(N);
+        for (int i = 0; i < N; i++)
+            ans[idx[i]] = rankStr(i);
+        return ans;
+    }
+    
+    string rankStr(int idx) {
+        if (idx == 0) return "Gold Medal";
+        if (idx == 1) return "Silver Medal";
+        if (idx == 2) return "Bronze Medal";
+        return to_string(idx + 1);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
