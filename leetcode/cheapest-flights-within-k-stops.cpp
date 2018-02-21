0427f296d6aeed23abeac3f4ac7a7da7f510e0cd
smilingpoplar <smilingpoplar@gmail.com>
2018-02-21 21:13:34 +0800

    update

--- leetcode/cheapest-flights-within-k-stops.cpp
+++ leetcode/cheapest-flights-within-k-stops.cpp
@@ -11,10 +11,11 @@
 
 using namespace std;
 
+/*
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
-        // dijkstra算法，抛掉steps>K的情况
+        // dijkstra算法，抛掉step>K的情况
         if (flights.empty()) return -1;
         const int M = flights.size();
         const int N = flights[0].size();
@@ -22,28 +23,47 @@ public:
         for (auto &e : flights) {
             adj[e[0]][e[1]] = e[2];
         }
-        vector<int> dists(n, INT_MAX);
-        dists[src] = 0;
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
         
         auto cmp = [](vector<int> &a, vector<int> &b) { // {node, dist, steps}
             return a[1] > b[0]; // 最小堆
         };
         priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pq(cmp);
-        pq.push({src, dists[src], 0});
+        pq.push({src, dist[src], 0});
         while (!pq.empty()) {
             auto info = pq.top(); pq.pop();
-            int u = info[0], dist = info[1], step = info[2];
+            int u = info[0], d = info[1], step = info[2];
             if (step > K) continue;
             for (auto &neighbor : adj[u]) {
                 int v = neighbor.first, cost = neighbor.second;
-                int newdist = dist + cost;
-                if (newdist < dists[v]) {
-                    dists[v] = newdist;
+                int newdist = d + cost;
+                if (newdist < dist[v]) {
+                    dist[v] = newdist;
                     pq.push({v, newdist, step + 1});
                 }
             }
         }
-        return (dists[dst] != INT_MAX) ? dists[dst] : -1;
+        return (dist[dst] != INT_MAX) ? dist[dst] : -1;
+    }
+};
+*/
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        // bellman ford算法，对所有边做K+1次松弛
+        const int INF = 1e9;
+        vector<int> dist(n, INF);
+        dist[src] = 0;
+        for (int i = 0; i <= K; i++) {
+            auto prev = dist;
+            for (auto &e : flights) {
+                int u = e[0], v = e[1], cost = e[2];
+                int newdist = prev[u] + cost;
+                if (newdist < dist[v]) dist[v] = newdist;
+            }
+        }
+        return (dist[dst] != INF) ? dist[dst] : -1;
     }
 };
 
--- leetcode/network-delay-time.cpp
+++ leetcode/network-delay-time.cpp
@@ -17,33 +17,33 @@ public:
         for (auto &e : times) {
             adj[e[0]][e[1]] = e[2];
         }
-        vector<int> dists(N + 1, INT_MAX);
-        dists[K] = 0;
+        vector<int> dist(N + 1, INT_MAX);
+        dist[K] = 0;
         
         // vector<int> visited(N + 1, false);
         auto cmp = [](vector<int> &a, vector<int> &b) { // {node, dist}
             return a[1] > b[1];
         };
         priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pq(cmp);
-        pq.push({K, dists[K]});
+        pq.push({K, dist[K]});
         while (!pq.empty()) {
             auto info = pq.top(); pq.pop();
-            int u = info[0], dist = info[1];
+            int u = info[0], d = info[1];
             // if (visited[u]) continue;
             // visited[u] = true;
             for (auto &e : adj[u]) { // 遍历u的所有邻接点
                 int v = e.first, cost = e.second;
-                int newdist = dist + cost;
-                if (newdist < dists[v]) {
-                    dists[v] = newdist;
+                int newdist = d + cost;
+                if (newdist < dist[v]) {
+                    dist[v] = newdist;
                     pq.push({v, newdist}); // pq中将可能有多个v值，但后续v值对结果没影响。可用visited[]来排除。
                 }
             }
         }
         
         int ans = INT_MIN;
-        for (int i = 1; i < dists.size(); i++) {
-            ans = max(ans, dists[i]);
+        for (int i = 1; i < dist.size(); i++) {
+            ans = max(ans, dist[i]);
         }
         return (ans != INT_MAX) ? ans : -1;
     }
