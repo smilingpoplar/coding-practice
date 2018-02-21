4e167a31bc54faee8e93992847e85869c7649945
smilingpoplar <smilingpoplar@gmail.com>
2018-02-21 22:14:46 +0800

    update

---
+++ leetcode/replace-words.cpp
@@ -0,0 +1,66 @@
+//
+//  replace-words
+//  https://leetcode.com/problems/replace-words/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TrieNode {
+    string word;
+    vector<TrieNode *> child;
+    TrieNode() : child(26, NULL) { }
+};
+
+class Trie {
+    TrieNode root;
+public:
+    void insert(const string &word) {
+        auto p = &root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!p->child[idx]) p->child[idx] = new TrieNode();
+            p = p->child[idx];
+        }
+        p->word = word;
+    }
+    
+    string shortestPrefix(const string &word) {
+        auto p = &root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!p->child[idx]) break;
+            p = p->child[idx];
+            if (!p->word.empty()) return p->word;
+        }
+        return word;
+    }
+};
+
+class Solution {
+public:
+    string replaceWords(vector<string>& dict, string sentence) {
+        Trie trie;
+        for (auto &word : dict) {
+            trie.insert(word);
+        }
+
+        istringstream iss(sentence);
+        ostringstream oss;
+        string word;
+        while (iss >> word) {
+            if (oss.tellp() > 0) oss << " ";
+            oss << trie.shortestPrefix(word);
+        }
+        return oss.str();
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
