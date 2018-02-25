69d0901fac5233f2554f5c0239794a76179e027a
smilingpoplar <smilingpoplar@gmail.com>
2018-02-25 20:46:04 +0800

    update

---
+++ leetcode/smallest-good-base.cpp
@@ -0,0 +1,38 @@
+//
+//  smallest-good-base
+//  https://leetcode.com/problems/smallest-good-base/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string smallestGoodBase(string n) {
+        // n = 111..11（x个1）= b^0+b^1+b^2+...+b^(x-1) = (b^x-1)/(b-1)
+        // 要b尽量小，即x尽量大。b=[2..n-1]，b=2时n=2^x-1，x=lg(n+1)；b=n-1时x=2，故x=[2..lg(n+1)]
+        // 由二项式定理(b+1)^(x-1) > b^0+b^1+b^2+...+b^(x-1)=n，又n > b^(x-1)，故 b+1 > pow(n,1/(x-1)) > b
+        // b = floor( pow(n,1/(x-1)) )
+        long long num = stoll(n);
+        for (int x = log2(num + 1); x >= 2; x--) {
+            int b = pow(num, 1.0 /(x - 1));
+            // 对特定x和b计算累加和n
+            long long sum = 1, curr = 1;
+            for (int i = 1; i <= x - 1; i++) {
+                curr *= b;
+                sum += curr;
+            }
+            if (sum == num) return to_string(b);
+        }
+        return to_string(num - 1);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
