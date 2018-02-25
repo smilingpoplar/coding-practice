4eae07e8b7625d86dcf4d16be125e9e05ae4fdd0
smilingpoplar <smilingpoplar@gmail.com>
2018-02-25 23:39:05 +0800

    update

---
+++ leetcode/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,30 @@
+//
+//  single-element-in-a-sorted-array
+//  https://leetcode.com/problems/single-element-in-a-sorted-array/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        // 找第一个 nums[mid] != nums[mid^1] 的数
+        int l = 0, u = nums.size() - 1;
+        while (l <= u) {
+            int mid = l + (u - l) / 2;
+            if (nums[mid] != nums[mid ^ 1]) u = mid - 1;
+            else l = mid + 1;
+        }
+        return nums[l];
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
