8721e10492c17e6c6a2d2276773042e0a648131d
smilingpoplar <smilingpoplar@gmail.com>
2018-02-26 23:01:56 +0800

    update

---
+++ leetcode/optimal-division.cpp
@@ -0,0 +1,36 @@
+//
+//  optimal-division
+//  https://leetcode.com/problems/optimal-division/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string optimalDivision(vector<int>& nums) {
+        // 答案一定是 a/(b/c/...)
+        if (nums.empty()) return "";
+        const int N = nums.size();
+        ostringstream oss;
+        if (N == 1) {
+            oss << nums[0];
+        } else if (N == 2) {
+            oss << nums[0] << "/" << nums[1];
+        } else {
+            oss << nums[0] << "/(" << nums[1];
+            for (int i = 2; i < N; i++) oss << "/" << nums[i];
+            oss << ")";   
+        }
+        return oss.str();
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
