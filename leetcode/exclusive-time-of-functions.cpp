e5293ee44086ff625aa9170e7c405bad313ad09a
smilingpoplar <smilingpoplar@gmail.com>
2018-02-28 17:06:02 +0800

    update

---
+++ leetcode/exclusive-time-of-functions.cpp
@@ -0,0 +1,53 @@
+//
+//  exclusive-time-of-functions
+//  https://leetcode.com/problems/exclusive-time-of-functions/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    struct Log {
+        int funcId;
+        bool isStart;
+        int timestamp;
+    };
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> ans(n, 0);
+        stack<Log> stk;
+        for (auto &str : logs) {
+            auto log = parse(str);
+            if (log.isStart) {
+                stk.push(log);
+            } else {
+                auto &curr = stk.top();  stk.pop();
+                int time = log.timestamp - curr.timestamp + 1;
+                ans[log.funcId] += time;
+                if (!stk.empty()) ans[stk.top().funcId] -= time;
+            }            
+        }
+        return ans;
+    }
+    
+    Log parse(string &str) {
+        istringstream iss(str);
+        string s;
+        getline(iss, s, ':');
+        int funcId = stoi(s);
+        getline(iss, s, ':');
+        bool isStart = s == "start";
+        getline(iss, s, ':');
+        int timestamp = stoi(s);
+        return { funcId, isStart, timestamp };
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
