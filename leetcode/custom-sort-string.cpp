4bbb5a26b16f57ec05dd6fa5fe9e7f2e9e8a6f76
smilingpoplar <smilingpoplar@gmail.com>
2018-02-28 22:10:55 +0800

    update

---
+++ leetcode/custom-sort-string.cpp
@@ -0,0 +1,39 @@
+//
+//  custom-sort-string
+//  https://leetcode.com/problems/custom-sort-string/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string customSortString(string S, string T) {
+        unordered_map<char, int> count;
+        for (char c : T) 
+            count[c]++;
+        
+        string ans;
+        // S中出现的
+        for (char c : S) {
+            for (int i = 0; i < count[c]; i++)
+                ans.push_back(c);
+            count[c] = 0;
+        }
+        // S中没出现的
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < count[c]; i++)
+                ans.push_back(c);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ leetcode/escape-the-ghosts.cpp
@@ -0,0 +1,32 @@
+//
+//  escape-the-ghosts
+//  https://leetcode.com/problems/escape-the-ghosts/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        vector<int> pacman = {0, 0};
+        int distPacman = dist(pacman, target);
+        for (auto &ghost : ghosts) {
+            if (dist(ghost, target) <= distPacman) return false;
+        }
+        return true;
+    }
+    
+    int dist(vector<int> &from, vector<int> &target) {
+        return abs(from[0] - target[0]) + abs(from[1] - target[1]);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
