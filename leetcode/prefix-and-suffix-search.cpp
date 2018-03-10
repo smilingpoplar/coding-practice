10b382b41e5029e2249169f625cd84a9567d4787
smilingpoplar <smilingpoplar@gmail.com>
2018-03-10 17:51:25 +0800

    update

---
+++ leetcode/prefix-and-suffix-search.cpp
@@ -0,0 +1,81 @@
+//
+//  prefix-and-suffix-search
+//  https://leetcode.com/problems/prefix-and-suffix-search/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TrieNode {
+    vector<int> wordIdx;
+    vector<TrieNode *> child;
+    TrieNode() : child(26, NULL) { }
+};
+
+class Trie {
+    TrieNode root;
+public:
+    void insert(const string &word, int index) {
+        auto p = &root;
+        p->wordIdx.push_back(index); // ""前缀可对应单词
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!p->child[idx]) p->child[idx] = new TrieNode();
+            p = p->child[idx];
+            p->wordIdx.push_back(index);
+        }
+    }
+    
+    vector<int> search(const string &prefix) {
+        auto p = &root;        
+        for (char c : prefix) {
+            int idx = c - 'a';
+            if (!p->child[idx]) return {};
+            p = p->child[idx];
+        }
+        return p->wordIdx;
+    }
+};
+
+class WordFilter {
+    Trie forward;
+    Trie backward;
+public:
+    WordFilter(vector<string> words) {
+        for (int i = 0; i < words.size(); i++) {
+            forward.insert(words[i], i);
+            reverse(words[i].begin(), words[i].end());
+            backward.insert(words[i], i);
+        }
+    }
+    
+    /*  */
+    int f(string prefix, string suffix) {
+        auto wordIdx1 = forward.search(prefix);
+        reverse(suffix.begin(), suffix.end());
+        auto wordIdx2 = backward.search(suffix);
+        // 找出这两个列表的交集中的最大值
+        int i = wordIdx1.size() - 1, j = wordIdx2.size() - 1;
+        while (i >= 0 && j >= 0) {
+            if (wordIdx1[i] == wordIdx2[j]) return wordIdx1[i];
+            else if (wordIdx1[i] > wordIdx2[j]) i--;
+            else j--;
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter obj = new WordFilter(words);
+ * int param_1 = obj.f(prefix,suffix);
+ */
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
