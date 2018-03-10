ccb72126d8290080728ce977c9ba75f25387fdb4
smilingpoplar <smilingpoplar@gmail.com>
2018-03-10 23:31:48 +0800

    update

---
+++ leetcode/read-n-characters-given-read4-ii-call-multiple-times.cpp
@@ -0,0 +1,44 @@
+//
+//  read-n-characters-given-read4-ii-call-multiple-times
+//  https://leetcode.com/problems/read-n-characters-given-read4-ii-call-multiple-times/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+    char buffer[4];
+    int bufferSize = 0;
+    int bufferPos = 0;
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+        // read()的下次调用能从上次结束位置继续读，read4()的调用结果要内部缓存起来
+        int size = 0;
+        while (size < n) {
+            if (bufferPos == bufferSize) { // buffer[]空了
+                bufferPos = 0;
+                bufferSize = read4(buffer);
+                if (bufferSize == 0) break;
+            }
+            buf[size++] = buffer[bufferPos++];
+        }
+        return size;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ leetcode/read-n-characters-given-read4.cpp
@@ -0,0 +1,41 @@
+//
+//  read-n-characters-given-read4
+//  https://leetcode.com/problems/read-n-characters-given-read4/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Forward declaration of the read4 API.
+int read4(char *buf);
+
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+        char tmp[4];
+        int size = 0;
+        while (size < n) {
+            int count = read4(tmp);
+            for (int i = 0; i < count; i++) {
+                buf[size + i] = tmp[i];
+            }
+            size += count;
+            if (count < 4) break;
+        }
+        return min(size, n);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
