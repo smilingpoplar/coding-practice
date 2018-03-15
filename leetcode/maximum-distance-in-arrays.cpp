5922bb62cc53f28c3bfa78f700edf672d87586d1
smilingpoplar <smilingpoplar@gmail.com>
2018-03-15 17:03:29 +0800

    update

---
+++ leetcode/maximum-distance-in-arrays.cpp
@@ -0,0 +1,31 @@
+//
+//  maximum-distance-in-arrays
+//  https://leetcode.com/problems/maximum-distance-in-arrays/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        // 最大距离来自：theMax-a[0]、a[n-1]-theMin
+        int theMin = arrays[0][0], theMax = arrays[0][arrays[0].size() - 1];
+        int ans = INT_MIN;
+        for (int i = 1; i < arrays.size(); i++) {
+            int newMin = arrays[i][0], newMax = arrays[i][arrays[i].size() - 1];
+            ans = max(ans, max(theMax - newMin, newMax - theMin));
+            theMin = min(theMin, newMin), theMax = max(theMax, newMax);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
