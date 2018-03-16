887090c691c3d476d3576a1f60b3905fb14f454a
smilingpoplar <smilingpoplar@gmail.com>
2018-03-17 00:17:45 +0800

    update

---
+++ leetcode/generalized-abbreviation.cpp
@@ -0,0 +1,38 @@
+//
+//  generalized-abbreviation
+//  https://leetcode.com/problems/generalized-abbreviation/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> generateAbbreviations(string word) {
+        vector<string> ans;
+        dfs(word, 0, 0, "", ans);
+        return ans;
+    }
+    
+    void dfs(const string &word, int pos, int count, string abbr, vector<string> &ans) {
+        if (pos == word.size()) {
+            if (count > 0) abbr += to_string(count);
+            ans.push_back(abbr);
+            return;
+        }
+        // 对每个字母，或者：省略它、并增加计数
+        dfs(word, pos + 1, count + 1, abbr, ans);
+        // 或者：计数>0时输出累加的数字、再输出字母
+        abbr += (count > 0 ? to_string(count) : "") + word[pos]; // 2
+        dfs(word, pos + 1, 0, abbr, ans);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ leetcode/unique-word-abbreviation.cpp
@@ -0,0 +1,46 @@
+//
+//  unique-word-abbreviation
+//  https://leetcode.com/problems/unique-word-abbreviation/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class ValidWordAbbr {
+    unordered_map<string, set<string>> mp; // abbr=>words
+public:
+    ValidWordAbbr(vector<string> dictionary) {
+        // 按abbr分桶
+        for (auto &word : dictionary) {
+            auto abbr = toAbbr(word);
+            mp[abbr].insert(word);
+        }
+    }
+    
+    bool isUnique(string word) {
+        auto abbr = toAbbr(word);
+        return (mp.find(abbr) == mp.end() || 
+                (mp[abbr].size() == 1 && mp[abbr].find(word) != mp[abbr].end()));
+    }
+    
+    string toAbbr(const string &word) {
+        const int N = word.size();
+        if (N <= 2) return word;
+        return word[0] + to_string(N - 2) + word[N-1];
+    }
+};
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
+ * bool param_1 = obj.isUnique(word);
+ */
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
