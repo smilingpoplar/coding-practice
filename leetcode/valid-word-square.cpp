5f4d76136e67ec2772cb2e8b4749b7ad44245fad
smilingpoplar <smilingpoplar@gmail.com>
2018-03-16 11:30:16 +0800

    update

---
+++ leetcode/valid-word-square.cpp
@@ -0,0 +1,29 @@
+//
+  //  valid-word-square
+  //  https://leetcode.com/problems/valid-word-square/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words[i].size(); j++) {
+                // words[j][i]的下标要合法
+                if (j >= words.size() || i >= words[j].size() || words[i][j] != words[j][i]) return false;
+            }
+        }
+        return true;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
