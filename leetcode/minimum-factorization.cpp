78fc9045031889ac60c2954dbcb9218f1ffcee5a
smilingpoplar <smilingpoplar@gmail.com>
2018-03-20 02:00:43 +0800

    update

---
+++ leetcode/minimum-factorization.cpp
@@ -0,0 +1,34 @@
+//
+//  minimum-factorization
+//  https://leetcode.com/problems/minimum-factorization/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int smallestFactorization(int a) {
+        if (a < 10) return a;
+        // a要分解成单数字因子，且个数越少越好，所以从9到2地尝试分解
+        long ans = 0, base = 1;
+        for (int i = 9; i >= 2; i--) {
+            while (a % i == 0) {
+                ans += i * base;
+                if (ans > INT_MAX) return 0;
+                a /= i;
+                base *= 10;
+            }
+        }
+        return (a == 1) ? ans : 0;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
