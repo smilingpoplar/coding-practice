f1492c2cb3b196df1c401190e1df4ae7bfb815fe
smilingpoplar <smilingpoplar@gmail.com>
2018-03-23 00:31:50 +0800

    update

---
+++ leetcode/binary-tree-vertical-order-traversal.cpp
@@ -0,0 +1,50 @@
+//
+//  binary-tree-vertical-order-traversal
+//  https://leetcode.com/problems/binary-tree-vertical-order-traversal/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        // 在同一列的按层排，所以bfs按层遍历
+        map<int, vector<int>> cols; // 用个哈希表按列分桶
+        queue<pair<int, TreeNode *>> q; // pair: <col,node>
+        if (root) q.push({0, root});
+
+        while (!q.empty()) {
+            auto top = q.front(); q.pop();
+            auto col = top.first;
+            auto node = top.second;
+            cols[col].push_back(node->val);
+            if (node->left) q.push({col-1, node->left});
+            if (node->right) q.push({col+1, node->right});
+        }
+        
+        vector<vector<int>> ans;
+        for (auto &e : cols) {
+            ans.push_back(e.second);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
