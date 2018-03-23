db473aa6c35b26b8d417f6e692e54d715baf3c9f
smilingpoplar <smilingpoplar@gmail.com>
2018-03-23 21:35:23 +0800

    update

---
+++ leetcode/equal-tree-partition.cpp
@@ -0,0 +1,42 @@
+//
+//  equal-tree-partition
+//  https://leetcode.com/problems/equal-tree-partition/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkEqualTree(TreeNode* root) {
+        unordered_map<int, int> mp; // sum=>count
+        int sum = getSum(root, mp);
+        if (sum == 0) return mp[0] > 1;
+        return sum % 2 == 0 && mp.count(sum / 2);
+    }
+    
+    int getSum(TreeNode *root, unordered_map<int, int> &mp) {
+        if (!root) return 0;
+        int sum = root->val + getSum(root->left, mp) + getSum(root->right, mp);
+        mp[sum]++;
+        return sum;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ leetcode/path-sum-iv.cpp
@@ -0,0 +1,46 @@
+//
+//  path-sum-iv
+//  https://leetcode.com/problems/path-sum-iv/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int pathSum(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        // 第一数字表示depth，第二数字表示在它那一层的pos，第三数字表示值val
+        // "xy?"前两个数字xy是节点位置信息，它的左子节点是(x+1)(2y-1)?，右子节点是(x+1)(2y)?，可模拟dfs遍历
+        unordered_map<int, int> mp; // 用哈希表记录映射：xy=>?
+        for (int num : nums)
+            mp[num / 10] = num % 10;
+
+        int ans = 0;
+        dfs(nums[0] / 10, mp, 0, ans);
+        return ans;
+    }
+    
+    void dfs(int xy, unordered_map<int, int> &mp, int sum, int &ans) {
+        if (!mp.count(xy)) return;
+        sum += mp[xy];
+        
+        int x = xy / 10, y = xy % 10;
+        int left = (x + 1) * 10 + (2 * y - 1), right = left + 1;
+        if (!mp.count(left) && !mp.count(right)) { // 叶节点
+            ans += sum;
+            return;
+        }
+        dfs(left, mp, sum, ans);
+        dfs(right, mp, sum, ans);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
