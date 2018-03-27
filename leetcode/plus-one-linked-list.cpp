496e3b4d287573eb12abf9a5012dc0fa82ff7356
smilingpoplar <smilingpoplar@gmail.com>
2018-03-27 22:29:43 +0800

    update

---
+++ leetcode/plus-one-linked-list.cpp
@@ -0,0 +1,44 @@
+//
+//  plus-one-linked-list
+//  https://leetcode.com/problems/plus-one-linked-list/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* plusOne(ListNode* head) {
+        auto dummy = new ListNode(0);
+        dummy->next = head;
+        // 找最后一个!=9（不进位）的位置
+        ListNode *found;
+        for (auto p = dummy; p != NULL; p = p->next) {
+            if (p->val != 9) found = p;
+        }
+        // found位置值加1，后面位置值全变0
+        found->val += 1;
+        for (auto p = found->next; p != NULL; p = p->next) {
+            p->val = 0;
+        }
+        if (dummy->val == 0) return dummy->next; // 没进位到dummy节点
+        return dummy;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
