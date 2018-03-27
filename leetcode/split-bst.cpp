35011c8b3d67ae335d11cac0a529407af13324f5
smilingpoplar <smilingpoplar@gmail.com>
2018-03-27 21:07:38 +0800

    update

---
+++ leetcode/split-bst.cpp
@@ -0,0 +1,43 @@
+//
+//  split-bst
+//  https://leetcode.com/problems/split-bst/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> splitBST(TreeNode* root, int V) {
+        if (!root) return {NULL, NULL};
+        if (root->val <= V) { // root分在左边
+            auto splitted = splitBST(root->right, V);
+            root->right = splitted[0];
+            splitted[0] = root;
+            return splitted;
+        } else {
+            auto splitted = splitBST(root->left, V);
+            root->left = splitted[1];
+            splitted[1] = root;
+            return splitted;
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
