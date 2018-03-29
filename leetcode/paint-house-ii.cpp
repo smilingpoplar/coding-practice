61905485cb5d0b08f95eafb68365758ff8345a0f
smilingpoplar <smilingpoplar@gmail.com>
2018-03-29 15:56:33 +0800

    update

--- leetcode/paint-house-ii.cpp
+++ leetcode/paint-house-ii.cpp
@@ -15,26 +15,29 @@ class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
         // dp的维度来自dfs中的递归参数
-        // 设dp[i][c]表示前[0..i]个房子、第i房子颜色c时的minCost
-        // dp[i][c] = costs[i][c] + min{ dp[i-1][not_c] }
-        // 初始dp[0][c] = costs[0][c]
-        // dp[i][]只依赖于dp[i-1][]，去掉i维度降维，设curr[]表示dp[i][]，prev[]表示dp[i-1][]
+        // 设dp[i][c]表示前[0..i]房子、第i房颜色为c时的minCost
+        // dp[i][c] = costs[i][c] + min{ dp[i-1][not_c] }，初始dp[0][c] = costs[0][c]
+        // 而求min{ dp[i-1][not_c] }，只需要维护dp[i-1][]中的最小min1（及其对应的颜色min1c）和第二小min2
+        // 这样，dp[i][c] = costs[i][c] + (min1c != c ? min1 : min2)
         if (costs.empty()) return 0;
         const int N = costs.size();
         const int K = costs[0].size();
-        vector<int> prev = costs[0];
-        for (int i = 1; i < N; i++) {
-            vector<int> curr(K, INT_MAX);
+        int min1 = 0, min1c = -1, min2 = 0;
+        for (int i = 0; i < N; i++) {
+            int currMin1 = INT_MAX, currMin1c = -1, currMin2 = INT_MAX;
             for (int c = 0; c < K; c++) {
-                for (int not_c = 0; not_c < K; not_c++) {
-                    if (not_c != c) {
-                        curr[c] = min(curr[c], costs[i][c] + prev[not_c]);                
-                    }
+                int cost = costs[i][c] + (min1c != c ? min1 : min2);
+                if (cost < currMin1) {
+                    currMin2 = currMin1;
+                    currMin1 = cost;
+                    currMin1c = c;
+                } else if (cost < currMin2) {
+                    currMin2 = cost;
                 }
             }
-            swap(curr, prev);
+            min1 = currMin1, min1c = currMin1c, min2 = currMin2;
         }
-        return *min_element(prev.begin(), prev.end());
+        return min1;
     }
 };
 
