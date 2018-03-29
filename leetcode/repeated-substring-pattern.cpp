464ce06eff3f06c39d3ec7aae797d4e2d7cb48e1
smilingpoplar <smilingpoplar@gmail.com>
2018-03-30 01:49:51 +0800

    update

--- leetcode/repeated-substring-pattern.cpp
+++ leetcode/repeated-substring-pattern.cpp
@@ -7,15 +7,13 @@
 //
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        // 如果s是由子串重复而得，那s[1:]（去掉开头重复子串）、s[:-1]（去掉末尾重复子串）拼合后，ss[1:-1]应包含s
-        return (s + s).substr(1, 2 * s.size() - 2).find(s) != string::npos;        
+        return (s + s).find(s, 1) < s.size();        
     }
 };
 
