bc8c8dce14128e0966d300069dcfbd62df8aea3d
smilingpoplar <smilingpoplar@gmail.com>
2018-04-06 03:19:25 +0800

    update

---
+++ leetcode/factor-combinations.cpp
@@ -0,0 +1,41 @@
+//
+//  factor-combinations
+//  https://leetcode.com/problems/factor-combinations/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> getFactors(int n) {
+        vector<vector<int>> ans;
+        vector<int> comb;
+        search(n, 2, comb, ans);
+        return ans;
+    }
+    
+    void search(int n, int start, vector<int> &comb, vector<vector<int>> &ans) {
+        if (n == 1) {
+            if (comb.size() > 1) ans.push_back(comb);
+            return;
+        }
+        
+        for (int i = start; i <= n; i++) {
+            if (n % i == 0) {
+                comb.push_back(i);
+                search(n / i, i, comb, ans);
+                comb.pop_back();
+            }
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
