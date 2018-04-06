77279fdaefb52a360d017ec4ccb54465be130727
smilingpoplar <smilingpoplar@gmail.com>
2018-04-06 16:54:38 +0800

    update

---
+++ leetcode/sparse-matrix-multiplication.cpp
@@ -0,0 +1,37 @@
+//
+//  sparse-matrix-multiplication
+//  https://leetcode.com/problems/sparse-matrix-multiplication/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> multiply(vector<vector<int>>& A, vector<vector<int>>& B) {
+        // 这题A、B虽说是稀疏数组，但没按稀疏数组的格式存储，还是普通矩阵
+        // 普通矩阵乘法：C[i][j] = A[i][k]*B[k][j]，这里把k循环外移，尽早跳过为0的项
+        if (A.empty() || B.empty()) return {};
+        const int I = A.size(), K = A[0].size(), J = B[0].size();
+        vector<vector<int>> C(I, vector<int>(J, 0));
+        for (int i = 0; i < I; i++) {
+            for (int k = 0; k < K; k++) {
+                if (A[i][k] == 0) continue;
+                for (int j = 0; j < J; j++) {
+                    if (B[k][j] == 0) continue;
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
