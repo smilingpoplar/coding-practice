19fc51596f22133780ec7fef4025e6068a81d6d1
smilingpoplar <smilingpoplar@gmail.com>
2018-04-09 01:14:58 +0800

    update

---
+++ leetcode/subdomain-visit-count.cpp
@@ -0,0 +1,39 @@
+//
+//  subdomain-visit-count
+//  https://leetcode.com/problems/subdomain-visit-count/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> subdomainVisits(vector<string>& cpdomains) {
+        unordered_map<string, int> cnt;
+        for (auto &cpdomain : cpdomains) {
+            auto pos = cpdomain.find(' ');
+            if (pos == string::npos) continue;
+            int count = stoi(cpdomain.substr(0, pos));
+            auto domain = cpdomain.substr(pos + 1);
+            cnt[domain] += count;
+            for (int i = 0; i < domain.size(); i++) {
+                if (domain[i] == '.') cnt[domain.substr(i + 1)] += count;       
+            }
+        }
+        
+        vector<string> ans;
+        for (auto &e : cnt) {
+            ans.push_back(to_string(e.second) + " " + e.first);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
