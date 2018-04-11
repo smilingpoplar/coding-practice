f2f435f47311a30e92435fe91115fd0e4e40d3a1
smilingpoplar <smilingpoplar@gmail.com>
2018-04-11 17:52:27 +0800

    update

---
+++ leetcode/all-paths-from-source-to-target.cpp
@@ -0,0 +1,38 @@
+//
+//  all-paths-from-source-to-target
+//  https://leetcode.com/problems/all-paths-from-source-to-target/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> ans;
+        vector<int> path = { 0 };
+        dfs(0, graph, path, ans);
+        return ans;
+    }
+    
+    void dfs(int node, vector<vector<int>>& graph, vector<int> &path, vector<vector<int>> &ans) {
+        if (node == (int)graph.size() - 1) {
+            ans.push_back(path);
+            return;
+        } 
+        for (int neighbor : graph[node]) {
+            path.push_back(neighbor);
+            dfs(neighbor, graph, path, ans);
+            path.pop_back();
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
