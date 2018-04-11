9d60ae6b20ee2fe7e93198d8d115f93bf1169cbd
smilingpoplar <smilingpoplar@gmail.com>
2018-04-11 15:07:45 +0800

    update

---
+++ leetcode/chalkboard-xor-game.cpp
@@ -0,0 +1,26 @@
+//
+//  chalkboard-xor-game
+//  https://leetcode.com/problems/chalkboard-xor-game/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool xorGame(vector<int>& nums) {
+        int x = 0;
+        for (int num : nums) x ^= num;
+        // 如果xor!=0，Alice要想赢只能指望Bob拿最后一个，即数有偶数个
+        return x == 0 || nums.size() % 2 == 0;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
