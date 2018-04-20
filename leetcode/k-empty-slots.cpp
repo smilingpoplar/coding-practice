2a5b9324b33ec47315c5ad0278bfbab40cf51d63
smilingpoplar <smilingpoplar@gmail.com>
2018-04-20 22:31:53 +0800

    modify

--- leetcode/k-empty-slots.cpp
+++ leetcode/k-empty-slots.cpp
@@ -21,18 +21,18 @@ public:
             days[flowers[i] - 1] = i + 1;
         }
         // 要找相隔k的两个位置left和right（right-left==k+1），使所有位置x（left<x<right）都满足days[x]>days[left]和days[x]>days[right]
-        // 滑动窗口法。维持[left,right]窗口，测试left<x<right中x是否都满足条件，不满足则移动窗口left=x，都满足则left=right。
-        // 整个过程x是一直往右移动的，当x==right时当前窗口中都满足条件。
+        // 维持[left,right]的滑动窗口，测试left<x<right中x是否都满足条件。用一直往右移动的x下标，当x<right时触发不满足则右移窗口left=x；
+        // 当x==right时也会触发不满足，这时其实窗口中都满足条件，记录答案，再右移窗口left=x。因为x==right时右移了窗口，不可能有x>right。
         int ans = INT_MAX;
         int left = 0, right = k + 1;
-        for (int x = 1; right < N; x++) {
+        for (int x = left + 1; right < N; x++) {
             if (days[x] > days[left] && days[x] > days[right]) continue;
             // x不满足条件了
             if (x == right) { // 窗口中都满足条件
                 int day = max(days[left], days[right]);
                 ans = min(ans, day);                
             }
-            left = x, right = x + k + 1;
+            left = x, right = left + k + 1;
         }
         return ans != INT_MAX ? ans : -1;
     }
