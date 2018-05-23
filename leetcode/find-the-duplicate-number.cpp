83611d7d0be4dff3300dcfc42f670b5ec62657a8
smilingpoplar <smilingpoplar@gmail.com>
2018-05-24 01:11:05 +0800

    update

--- leetcode/find-the-duplicate-number.cpp
+++ leetcode/find-the-duplicate-number.cpp
@@ -14,8 +14,9 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // 把nums[i]看做链表的next指针：i=>nums[i]=>nums[nums[i]]=>...
-        // 这题变成找链表环的入口点，快慢指针。由鸽笼原理，一定有环
+        // 由鸽笼原理，一定有环
+        // 把nums[i]看做下标i的next下标，相当于链表的next指针，
+        // 找重复的数变成链表中找环的入口点，快慢指针法
         int fast = 0, slow = 0;
         while (true) {
             fast = nums[nums[fast]];
