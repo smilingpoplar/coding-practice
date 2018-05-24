a05c8ee265fafe33be5e37b194014a42f3b3b0dc
smilingpoplar <smilingpoplar@gmail.com>
2018-05-25 00:21:30 +0800

    update

--- leetcode/contains-duplicate-iii.cpp
+++ leetcode/contains-duplicate-iii.cpp
@@ -16,7 +16,7 @@ class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int> &nums, int k, int t) {
         if (k < 1 || t < 0) return false;
-        set<long> win; // 总共k+1个元素，win中保存前k个元素以跟当前元素比较，保证窗口长度<=k
+        set<long> win; // 元素要跟它前面k个元素比较判重，保持win长度<=k
         for (int i = 0; i < nums.size(); i++) {
             // win中是否存在x，使nums[i]-x<=t且x-nums[i]<=t，nums[i]-t<=x<=nums[i]+t
             // 只要找>=nums[i]-t的数，看它是否满足<=nums[i]+t
@@ -24,8 +24,7 @@ public:
             if (it != win.end() && *it <= (long)nums[i] + t) return true;
             
             win.insert(nums[i]);
-            // 保证窗口长度<=k，有效范围[left,i]，i-left+1<=k，left>=i-k+1
-            if (win.size() > k) win.erase(nums[i - k]); // 删除超出left的
+            if (win.size() > k) win.erase(nums[i - k]);
 
         }
         return false;
@@ -38,24 +37,23 @@ public:
     bool containsNearbyAlmostDuplicate(vector<int> &nums, int k, int t) {
         if (k < 1 || t < 0) return false;
         unordered_map<long, long> buckets; // idx=>num
-        // 总共k+1个元素，buckets中保存前k个元素以跟当前元素比较，保证buckets.size()<=k
+        // buckets保存前k个数的桶，保持size(buckets)<=k
         for (int i = 0; i < nums.size(); i++) {            
-            auto idx = bucketIndex(nums[i], t);
+            auto idx = bucketIdx(nums[i], t);
             // 重复发生在自己桶中
             if (buckets.count(idx)) return true;
-            // 重复发生在左右相邻两桶
-            if (buckets.count(idx - 1) && nums[i] - buckets[idx - 1] <= t) return true;
-            if (buckets.count(idx + 1) && buckets[idx + 1] - nums[i] <= t) return true;
+            // 重复发生在相邻两桶
+            if (buckets.count(idx-1) && nums[i] - buckets[idx-1] <= t) return true;
+            if (buckets.count(idx+1) && buckets[idx+1] - nums[i] <= t) return true;
 
             buckets[idx] = nums[i];
-            // 保证buckets.size()<=k，有效范围[left,i]，i-left+1<=k，left>=i-k+1
-            if (buckets.size() > k) buckets.erase(bucketIndex(nums[i - k], t)); // 删除超出left的
+            if (buckets.size() > k) buckets.erase(bucketIdx(nums[i-k], t));
         }
         return false;
     }
     
-    // |x-y|<=t有[0..t]个值，桶长t+1能使落在一个桶中的值肯定重复
-    long bucketIndex(long num, long t) {
+    // |x-y|<=t有[0..t]个值，桶长t+1时落在一个桶中的值肯定重复
+    long bucketIdx(long num, long t) {
         return (num - INT_MIN) / (t + 1);
     }
 };
