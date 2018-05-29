c8da517a924da9af2770b2f3855082110169c170
smilingpoplar <smilingpoplar@gmail.com>
2018-05-29 16:15:39 +0800

    update

--- leetcode/longest-repeating-character-replacement.cpp
+++ leetcode/longest-repeating-character-replacement.cpp
@@ -15,21 +15,18 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         unordered_map<char, int> count;
-        int maxCntEndingHere = 0;
+        int maxSameTillNow = 0; // 最长有效窗口是 maxSameTillNow+k
         int start = 0, end = 0, ans = 0;
         while (end < s.size()) {
-            maxCntEndingHere = max(maxCntEndingHere, ++count[s[end]]);
+            maxSameTillNow = max(maxSameTillNow, ++count[s[end]]);
             end++;
-            // 不变式：滑动窗口s[start,end)维护着长为 当前最长有效窗口长maxCntEndingHere+k 的一个窗口
-            // 注意，该滑动窗口本身可以不是有效窗口
-            // 尝试扩展end（end++），若扩展无效则start--，相当于窗口右移
-            if (end - start > maxCntEndingHere + k) {
+            // 若是无效窗口、再移动start使窗口有效
+            if (end - start > maxSameTillNow + k) {
                 count[s[start]]--;
                 start++;
             }
-            ans = max(ans, end - start);            
         }
-        return ans;
+        return end - start;
     }
 };
 
