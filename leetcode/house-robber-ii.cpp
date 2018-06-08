f1d5ee9a5c6e8d6ceaed286cdad0707ddc807d7c
smilingpoplar <smilingpoplar@gmail.com>
2018-06-08 21:57:38 +0800

    update

--- leetcode/house-robber-ii.cpp
+++ leetcode/house-robber-ii.cpp
@@ -15,17 +15,16 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         if (nums.empty()) return 0;
-        const int N = (int)nums.size();
+        const int N = nums.size();
         if (N == 1) return nums[0];
-        // 因为首尾循环，首尾元素不能同时选择，根据不选尾元素还是不选首元素，
+        // 循环数组首尾不能同时选择，根据不能选尾元素还是不能选首元素，
         // 分成无循环的两个子问题rob(nums,0,n-2)和rob(nums,1,n-1)
-        return max(rob(nums, 0, N - 2), rob(nums, 1, N - 1));
+        return max(subRob(nums, 0, N - 2), subRob(nums, 1, N - 1));
     }
-private:
     // rob nums[from,to]
-    int rob(const vector<int> &nums, int from, int to) {
-        // 设dp[i]表示抢了nums[0..i]后的最大值，0<=i<=N-1
-        // dp[i] = max( nums[i]+dp[i-2], dp[i-1] )，初始dp[-2]=dp[-1]=0
+    int subRob(const vector<int> &nums, int from, int to) {
+        // 设dp[i]表示抢了nums[from..i]后的最大值，from<=i<=to
+        // dp[i] = max( nums[i]+dp[i-2], dp[i-1] )，初始dp[from-2]=dp[from-1]=0
         // 看递推式当前项只依赖于前两项，前两项分别用prev2和prev1表示
         int prev2 = 0, prev1 = 0;
         for (int i = from; i <= to; i++) {
