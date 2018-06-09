3ac86145859d121ed97a10335bddc2f6331b1581
smilingpoplar <smilingpoplar@gmail.com>
2018-06-09 15:00:44 +0800

    update

--- leetcode/remove-boxes.cpp
+++ leetcode/remove-boxes.cpp
@@ -14,23 +14,23 @@ class Solution {
 public:
     int removeBoxes(vector<int>& boxes) {
         const int N = boxes.size();
-        // 设dp[i][j][k]表示boxes[i..j]且前面有k个与boxes[i]同色且待拿的盒子时的最大得分
-        // 考虑现在拿boxes[i]还是留着以后和后面同色的一起拿
+        // 设dp[i][j][k]表示boxes[i..j]且前面有连续k个未拿盒子和boxes[i]同色时的最大得分
+        // 考虑现在拿boxes[i]、还是将来和后面某个同色的一起拿
         vector<vector<vector<int>>> dp(N, vector<vector<int>>(N, vector<int>(N, 0)));
         // i == j
         for (int i = 0; i < N; i++) {
             for (int k = 0; k <= i; k++) {
-                dp[i][i][k] = (k + 1) * (k + 1);
+                dp[i][i][k] = (k + 1) * (k + 1); // 现在拿boxes[i]
             }
         }
         // i < j
         for (int i = N - 2; i >= 0; i--) {
             for (int j = i + 1; j < N; j++) {
-                for (int k = 0; k <= i; k++) { // 前面有k个与boxes[i]同色
-                    dp[i][j][k] = (k + 1) * (k + 1) + dp[i + 1][j][0]; // 现在拿boxes[i]
+                for (int k = 0; k <= i; k++) { // 前面有连续k个未拿盒子与boxes[i]同色
+                    dp[i][j][k] = (k + 1) * (k + 1) + dp[i+1][j][0]; // 现在拿boxes[i]
                     for (int m = i + 1; m <= j; m++) {
-                        if (boxes[m] == boxes[i]) { // 以后和后面同色的boxes[m]一起拿
-                            dp[i][j][k] = max(dp[i][j][k], dp[m][j][k + 1] + dp[i + 1][m - 1][0]);
+                        if (boxes[m] == boxes[i]) { // 将来和后面同色的boxes[m]一起拿
+                            dp[i][j][k] = max(dp[i][j][k], dp[m][j][k+1] + dp[i+1][m-1][0]);
                         }
                     }
                 }
