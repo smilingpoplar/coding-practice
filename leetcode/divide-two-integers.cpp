43e3074b9650985559bf669c25c292510849eab7
smilingpoplar <smilingpoplar@gmail.com>
2018-07-28 16:30:08 +0800

    update

--- leetcode/divide-two-integers.cpp
+++ leetcode/divide-two-integers.cpp
@@ -16,24 +16,23 @@ public:
         if (divisor == 0) return INT_MAX;
         if (dividend == INT_MIN && divisor == -1) return INT_MAX;
         
-        int sign = ((dividend > 0) ^ (divisor > 0)) ? -1 : 1;
-        // abs(INT_MIN)溢出成INT_MIN，用"unsigned int"避免int溢出
-        unsigned int uDividend = abs(dividend);
-        unsigned int uDivisor = abs(divisor);
+        int sign = (dividend ^ divisor) >= 0 ? 1 : -1;
+        // abs(INT_MIN)溢出成INT_MIN，用"unsigned"避免int溢出
+        unsigned uDividend = abs(dividend);
+        unsigned uDivisor = abs(divisor);
         
-        int result = 0;
+        int ans = 0;
         while (uDivisor <= uDividend) {
-            unsigned int d = uDivisor;
+            unsigned d = uDivisor;
             int multiple = 1;
             while (d <= (uDividend >> 1)) { // (d<<1)<=uDividend可能溢出
                 d <<= 1;
                 multiple <<= 1;
             }
             uDividend -= d;
-            result += multiple;
+            ans += multiple;
         }
-        
-        return sign * result;
+        return sign * ans;
     }
 };
 
--- leetcode/median-of-two-sorted-arrays.cpp
+++ leetcode/median-of-two-sorted-arrays.cpp
@@ -15,11 +15,10 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
         // 数组a在任意位置i分成a1[0..i-1]、a2[i..M-1]两部分，数组b在任意位置j分成b1[0..j-1]、b2[j..N-1]两部分。
-        // 把a1和b1放一起构成整体的left部分，a2和b2放一起构成整体的right部分。我们先找两数组中第k大的数。
-        // 如果能保证：1. len(left)=k，即i+j==k  2. max(left)<=min(right)，即a[i-1]<=b[j]且b[j-1]<=a[i]，
-        // 那么第k大数为 max(left)。现在我们找中位数（或两中位数的第一个），有k=(M+N+1)/2。
-        // 综上，要在0<=i<=M中找i，j=k-i，使a[i-1]<=b[j]且b[j-1]<=a[i]，用二分搜索。
-        // 不妨使M<=N，则M<=k<=N。由0<=i<=min(k,M)=M，i+j==k，得0<=k-M<=j<=k<=N，对应j值都合法。
+        // 把a1和b1放一起构成整体的left部分，a2和b2放一起构成整体的right部分。设k=len(left)=i+j，
+        // 则要使max(left)第k大的数，只需max(left)<=min(right)，即a[i-1]<=b[j]且b[j-1]<=a[i] (1)。
+        // 现在要找中位数（或两中位数的第一个），k=(M+N+1)/2。综上，二分搜索0<=i<=M、j=k-i，验证(1)式。
+        // 不妨使M<=N，则M<=中位数k<=N。由0<=i<=min(k,M)=M，j=k-i，得0<=k-M<=j<=k<=N，对应j值都合法。
         const int M = a.size();
         const int N = b.size();
         if (M > N) return findMedianSortedArrays(b, a);
@@ -29,7 +28,7 @@ public:
         while (l <= u) {
             int i = l + (u - l) / 2;
             int j = k - i;
-            if (i > 0 && a[i-1] > b[j]) { // i>0时 j<N
+            if (i > 0 && a[i-1] > b[j]) {
                 // 需要a[i-1]<=b[j]，现在i太大了，要在左半边找
                 u = i - 1; 
             } else if (i < M && b[j-1] > a[i]) { // i<M时 j>0
