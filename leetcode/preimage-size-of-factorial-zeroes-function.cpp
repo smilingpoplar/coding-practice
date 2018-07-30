6261ea849abeecf0d290030ea8db1d69f4fb96da
smilingpoplar <smilingpoplar@gmail.com>
2018-07-31 01:30:39 +0800

    update

--- leetcode/preimage-size-of-factorial-zeroes-function.cpp
+++ leetcode/preimage-size-of-factorial-zeroes-function.cpp
@@ -14,15 +14,16 @@ using namespace std;
 class Solution {
 public:
     int preimageSizeFZF(int K) {
-        // f(x)是x!末尾0的个数，取决于1..x中因子5的个数
-        // 因子5的个数每连续5个数的至少加1，存在5个连续数、它们的因子5个数相同，所以f(x)=K的x存在的话就有5个
-        // 因为f(x)是个递增函数，用二分法看f(x)=K的x值存不存在
+        // f(x)是x!末尾0的个数，取决于1..x中因子5的个数。
+        // 因子5的个数每连续5个数相等，之后的5个数至少加1。所以只要f(x)=K的x存在，一定是有5个。
+        // 二分搜索使f(x)=K的x，x的上限取5*K。因为x至少贡献x/5个因子5，5*K就可贡献K个因子5。
         long l = 0, u = 5l * K;
         while (l <= u) {
             long mid = l + (u - l) / 2;
             int count = countZeros(mid);
             if (count == K) return 5;
-            else if (count > K) u = mid - 1;
+
+            if (count > K) u = mid - 1;
             else l = mid + 1;
         }
         return 0;
