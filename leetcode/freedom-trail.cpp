abccff216e77dc966f59a7bce2ad52616a03bdde
smilingpoplar <smilingpoplar@gmail.com>
2018-08-01 23:01:50 +0800

    update

--- leetcode/freedom-trail.cpp
+++ leetcode/freedom-trail.cpp
@@ -13,32 +13,61 @@ using namespace std;
 class Solution {
 public:
     int findRotateSteps(string ring, string key) {
-        unordered_map<char, vector<int>> mp; // ring_char => pos_list
-        for (int i = 0; i < ring.size(); i++) {
-            mp[ring[i]].push_back(i);
+        // 设dp[r][k]表示从ring[r]对齐12点开始，输入所有key[k..]的字符，最少需要旋转多少次。
+        // 考虑输入key[k]最少需要旋转多少次。遍历ring中字符，当ring[i]==key[k]时，把ring[i]转到对齐12点，
+        // 需要旋转 diff = abs(i-r), step = min(diff, R-diff) 次
+        // dp[r][k] = min{ step + dp[i][k+1] | for all ring[i]==key[k] }
+        // 初始dp[][K]=0，表示key输入完成的情况
+        const int R = ring.size();
+        const int K = key.size();
+        vector<vector<int>> dp(R, vector<int>(K + 1, INT_MAX));
+        for (int i = 0; i < R; i++) dp[i][K] = 0;
+        
+        // dp[][k]在k维上依赖于dp[][k+1]，要先算k这维，且逆序遍历k
+        for (int k = K - 1; k >= 0; k--) {
+            for (int r = 0; r < R; r++) {
+                for (int i = 0; i < R; i++) {
+                    if (ring[i] == key[k]) {
+                        int diff = abs(i - r);
+                        int step = min(diff, R - diff);
+                        dp[r][k] = min(dp[r][k], step + dp[i][k+1]);
+                    }
+                }
+            }
         }
+        return dp[0][0] + K;
+    }
+};
+
+/*
+class Solution {
+public:
+    int findRotateSteps(string ring, string key) {
         vector<vector<int>> memo(ring.size(), vector<int>(key.size(), -1));
-        return search(ring, 0, key, 0, mp, memo);
+        return search(ring, 0, key, 0, memo);
     }
     
-    // 原先ring[rIdx]正对12点，现在要变成key[kIdx]这个字符正对12点
-    int search(const string &ring, int rIdx, const string &key, int kIdx, 
-               unordered_map<char, vector<int>> &mp, vector<vector<int>> &memo) {
-        if (kIdx == key.size()) return 0;
-        if (memo[rIdx][kIdx] != -1) return memo[rIdx][kIdx];
+    // 从ring[r]对齐12点开始，旋转成ring中的key[k]字符对齐12点
+    int search(const string &ring, int r, const string &key, int k, vector<vector<int>> &memo) {
+        if (k == key.size()) return 0;
+        if (memo[r][k] != -1) return memo[r][k];
         
         int ans = INT_MAX;
-        for (int rNext : mp[key[kIdx]]) {
-            int steps = abs(rIdx - rNext);
-            steps = min(steps, (int)ring.size() - steps);
-            steps++; // press button;
-            steps += search(ring, rNext, key, kIdx + 1, mp, memo);
-            ans = min(ans, steps);
+        const int R = ring.size();
+        for (int i = 0; i < R; i++) {
+            if (ring[i] == key[k]) {
+                int steps = abs(i - r);
+                steps = min(steps, R - steps);
+                steps++; // press button;
+                steps += search(ring, i, key, k + 1, memo);
+                ans = min(ans, steps);
+            }
         }
-        memo[rIdx][kIdx] = ans;
+        memo[r][k] = ans;
         return ans;
     }
 };
+*/
 
 int main(int argc, const char * argv[]) {    
     return 0;
