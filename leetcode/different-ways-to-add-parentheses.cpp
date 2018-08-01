ec9e01d2a6c9c49d552807676ae4a6b1fd1a1c10
smilingpoplar <smilingpoplar@gmail.com>
2018-08-01 16:33:05 +0800

    update

--- leetcode/different-ways-to-add-parentheses.cpp
+++ leetcode/different-ways-to-add-parentheses.cpp
@@ -14,36 +14,36 @@ using namespace std;
 class Solution {
 public:
     vector<int> diffWaysToCompute(string input) {
-        vector<int> result;
+        vector<int> ans;
         for (int i = 0; i < input.size(); i++) {
             char c = input[i];
             if (c == '+' || c == '-' || c == '*') { // 分治
-                auto result1 = diffWaysToCompute(input.substr(0, i));
-                auto result2 = diffWaysToCompute(input.substr(i + 1));
-                for (auto n1 : result1) {
-                    for (auto n2 : result2) {
+                auto ans1 = diffWaysToCompute(input.substr(0, i));
+                auto ans2 = diffWaysToCompute(input.substr(i + 1));
+                for (auto n1 : ans1) {
+                    for (auto n2 : ans2) {
                         if (c == '+') {
-                            result.push_back(n1 + n2);
+                            ans.push_back(n1 + n2);
                         } else if (c == '-') {
-                            result.push_back(n1 - n2);
+                            ans.push_back(n1 - n2);
                         } else {
-                            result.push_back(n1 * n2);
+                            ans.push_back(n1 * n2);
                         }
                     }
                 }
             }
         }
-        if (result.empty()) { // input是纯数字串
-            result.push_back(stoi(input));
+        if (ans.empty()) { // input是纯数字串
+            ans.push_back(stoi(input));
         }
-        return result;
+        return ans;
     }
 };
 
 int main(int argc, const char * argv[]) {
     Solution solution;
-    auto result = solution.diffWaysToCompute("2*3-4*5");
-    for (auto num : result) {
+    auto ans = solution.diffWaysToCompute("2*3-4*5");
+    for (auto num : ans) {
         cout << num << " ";
     }
     
--- leetcode/expression-add-operators.cpp
+++ leetcode/expression-add-operators.cpp
@@ -15,31 +15,30 @@ class Solution {
 public:
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
-        search(num, target, 0, 0, 0, "", ans);
+        search(num, target, 0, "", 0, 0, ans);
         return ans;
     }
     
-    void search(string num, int target, int idx,
-                long prevExprEval, long prevValue,
-                string expr, vector<string> &ans) {
+    void search(string num, int target, int idx, string exprStr, long exprEval, 
+                long prevNum, vector<string> &ans) {
         if (idx == num.size()) {
-            if (prevExprEval == target)
-                ans.push_back(expr);
+            if (exprEval == target) ans.push_back(exprStr);
             return;
         }
-        
+
+        // [0,idx)是exprStr, [idx,i]是currStr, [i+1,..]进递归
         for (int i = idx; i < num.size(); i++) {
-            if (i > idx && num[idx] == '0') continue; // 多位数却以"0"开头
-            auto curr = num.substr(idx, i - idx + 1);
-            long currValue = stol(curr);
+            if (i > idx && num[idx] == '0') continue; // 多位数、却以"0"开头
+            auto currStr = num.substr(idx, i - idx + 1);
+            long currNum = stol(currStr);
             if (idx == 0) {
-                search(num, target, i + 1, currValue, currValue, curr, ans);
+                search(num, target, i + 1, currStr, currNum, currNum, ans);
             } else {
-                search(num, target, i + 1, prevExprEval + currValue, currValue, expr + "+" + curr, ans);
-                search(num, target, i + 1, prevExprEval - currValue, -currValue, expr + "-" + curr, ans);
-                // 比如 prevExprEval=1+2+3，现在遇到*4，要prevExprEval-3+3*4
-                search(num, target, i + 1, prevExprEval - prevValue + prevValue * currValue,
-                       prevValue * currValue, expr + "*" + curr, ans);
+                search(num, target, i + 1, exprStr + "+" + currStr, exprEval + currNum, currNum, ans);
+                search(num, target, i + 1, exprStr + "-" + currStr, exprEval - currNum, -currNum, ans);
+                // 比如 exprEval=1+2+3，现在遇到*4，要exprEval-3+3*4
+                search(num, target, i + 1, exprStr + "*" + currStr, 
+                       exprEval - prevNum + prevNum * currNum, prevNum * currNum, ans);
             }
         }
     }
