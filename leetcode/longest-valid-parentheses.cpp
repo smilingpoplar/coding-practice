b7c25c97fc6774a4bf43707be2a2bd8df177cc28
smilingpoplar <smilingpoplar@gmail.com>
2018-08-02 23:01:53 +0800

    update

--- leetcode/longest-valid-parentheses.cpp
+++ leetcode/longest-valid-parentheses.cpp
@@ -15,13 +15,47 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int ans = 0;
+        const int N = s.size();
+        int left = 0, right = 0; // 左括号数left，右括号数right
+        // 从左往右数的过程中确保left>=right
+        for (int i = 0; i < N; i++) {
+            if (s[i] == '(') left++;
+            else if (s[i] == ')') right++;
+            
+            if (left == right) { // 已配对
+                ans = max(ans, 2 * left);
+            } else if (left < right) {
+                left = right = 0;
+            }
+        }
+        // 从右往左数的过程中确保right>=left
+        left = right = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            if (s[i] == '(') left++;
+            else if (s[i] == ')') right++;
+            
+            if (left == right) { // 已配对
+                ans = max(ans, 2 * left);
+            } else if (right < left) {
+                left = right = 0;
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int ans = 0;
         stack<int> stk;
-        stk.push(-1); // 不变式：栈顶记录有效串前一个位置
+        stk.push(-1); // 不变式：栈顶记录上一个无效串尾的位置
         for (int i = 0; i < s.size(); i++) {
             if (s[i] == '(') {
                 stk.push(i);
             } else { // ')'
-                stk.pop(); // 正常是匹配掉
+                stk.pop(); // 总是匹配掉一个
                 if (!stk.empty()) ans = max(ans, i - stk.top());
                 else stk.push(i);
             }
@@ -29,6 +63,7 @@ public:
         return ans;
     }
 };
+*/
 
 int main(int argc, const char * argv[]) {
     Solution solution;
--- leetcode/regular-expression-matching.cpp
+++ leetcode/regular-expression-matching.cpp
@@ -48,7 +48,7 @@ public:
         for (int i = M; i >= 0; i--) {
             for (int j = N - 1; j >= 0; j--) {
                 bool matchOne = i < M && (s[i] == p[j] || p[j] == '.');
-                if (j + 2 <= N && p[j+1] == '*') {
+                if (j + 1 < N && p[j+1] == '*') {
                     dp[i][j] = dp[i][j+2] || (matchOne && dp[i+1][j]);
                 } else {
                     dp[i][j] = matchOne && dp[i+1][j+1];
