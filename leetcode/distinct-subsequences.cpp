09898ad332b76a60af1495798809a05f8994eddc
smilingpoplar <smilingpoplar@gmail.com>
2018-08-05 00:00:13 +0800

    update

--- leetcode/distinct-subsequences.cpp
+++ leetcode/distinct-subsequences.cpp
@@ -14,26 +14,23 @@ using namespace std;
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        // 动态规划，设f(i,j)表示s[0,i)中子序列t[0,j)的个数 (0<=i<=M, 0<=j<=N)
-        // 若s[i-1]==t[j-1]，f(i,j) = f(i-1,j-1)/*使用s[i-1]*/ + f(i-1,j)/*不使用s[i-1]*/
-        // 若s[i-1]!=t[j-1]，f(i,j) = f(i-1,j)
-        // 初值：f(i,0)=1，f(0,j>0)=0
-        // 递推式中第i项仅由第i-1项决定，因此在i循环中可降维：
-        // 若s[i-1]==t[j-1]，f(j)=f(j-1)+f(j) // 要逆序遍历才能保证下一循环时f(j-1)的值没被覆盖
-        // 若s[i-1]!=t[j-1]，f(j)=f(j)
-        // 即仅当s[i-1]==t[j-1]时，f(j)+=f(j-1) // 逆序遍历
-        const int M = (int)s.size();
-        const int N = (int)t.size();
-        vector<int> f(N + 1, 0);
-        f[0] = 1;
-        for (int i = 1; i <= M; i++) {
-            for (int j = N; j >= 1; j--) {
-                if (s[i - 1] == t[j - 1]) {
-                    f[j] += f[j - 1];
+        // 设dp[i,j]表示s[i..]中子序列等于t[j..]的个数 (0<=i<=M, 0<=j<=N）
+        // 若s[i]==t[j]，dp[i,j] = dp[i+1,j+1]/*使用s[i]*/ + dp[i+1,j]/*不用s[i]*/
+        // 若s[i]!=t[j]，dp[i,j] = dp[i+1,j]
+        // 初值：dp[i,N]=1，dp[M,j<N]=0
+        // 省略i这一维，i仍从右向左遍历，要让dp[j+1]表示两行合并前的原值dp[i+1,j+1]，j从左向右遍历
+        const int M = s.size(), N = t.size();
+        vector<int> dp(N + 1, 0); // 对应i==M
+        dp[N] = 1;
+
+        for (int i = M; i >= 0; i--) {
+            for (int j = 0; j < N; j++) {
+                if (s[i] == t[j]) {
+                    dp[j] += dp[j+1];
                 }
             }
         }
-        return f[N];
+        return dp[0];
     }
 };
 
