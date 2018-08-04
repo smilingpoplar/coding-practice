c66e15e21c5a02079a5994ee02f6d7f3629a7cef
smilingpoplar <smilingpoplar@gmail.com>
2018-08-04 15:27:28 +0800

    update

--- leetcode/scramble-string.cpp
+++ leetcode/scramble-string.cpp
@@ -11,67 +11,37 @@
 
 using namespace std;
 
-/*
 class Solution {
 public:
     bool isScramble(string s1, string s2) {
-        if (s1.size() != s2.size()) return false;
-        if (s1 == s2) return true;
-        
-        // 简单递归会超时，因为重复计算了子问题，后面可以用动态规划解决
-        // 这里加个剪枝条件（所有字符出现的次数应相同），可以通过测试
-        const int ASCII_SIZE = 256;
-        vector<int> count(ASCII_SIZE, 0);
-        for (char c : s1) ++count[c];
-        for (char c : s2) --count[c];
-        for (int i = 0; i < ASCII_SIZE; i++) {
-            if (count[i] != 0) return false;
-        }
-        
-        const int N = (int)s1.size();
-        for (int i = 1; i < N; i++) {
-            if ((isScramble(s1.substr(0, i), s2.substr(0, i)) && isScramble(s1.substr(i), s2.substr(i))) ||
-                (isScramble(s1.substr(0, i), s2.substr(N - i)) && isScramble(s1.substr(i), s2.substr(0, N - i))))
-                return true;
-        }
-        
-        return false;
-    }
-};
-*/
+        // 设dp[n,i,j]表示两个等长串s1[i,i+n)和s2[j,j+n)是否scramble，0<=i且i+n<=N，0<=j且j+n<N，
+        // 每个串由二叉树分叉成两部分，遍历1<=k<n<=N种划分可能，
+        // dp[n,i,j] = ( dp[k,i,j] && dp[n-k,i+k,j+k] ) || ( dp[k,i,j+n-k] && dp[n-k,i+k,j] )
+        // 初始n==1时，dp[1,i,j] = s1[i]==s2[j]
 
-class Solution {
-public:
-    bool isScramble(string s1, string s2) {
-        // 动态规划，设f(n,i,j)表示s1[i,i+n)和s2[j,j+n)是否scramble，1<=n<=N，0<=i<=N-n，0<=j<=N-n
-        // f(n,i,j) = ( f(k,i,j)&&f(n-k,i+k,j+k) ) || ( f(k,i,j+n-k)&&f(n-k,i+k,j) )，1<=k<n<=N，0<=i<=N-n，0<=j<=N-n
-
-        if (s1.size() != s2.size()) return false;
-        const int N = (int)s1.size();
-        vector<vector<vector<bool>>> f(N + 1, vector<vector<bool>>(N, vector<bool>(N, false)));
+        const int N = s1.size();
+        if (N != s2.size()) return false;
+        vector<vector<vector<bool>>> dp(N + 1, vector<vector<bool>>(N, vector<bool>(N, false)));
         
         // n==1
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                f[1][i][j] = s1[i] == s2[j];
+                dp[1][i][j] = s1[i] == s2[j];
             }
         }
         
-        for (int n = 1; n <= N; n++) {
-            for (int i = 0; i <= N - n; i++) {
-                for (int j = 0; j <= N - n; j++) {
-                    for (int k = 1; k < n; k++) { // n>=2进循环
-                        if ((f[k][i][j] && f[n - k][i + k][j + k]) ||
-                            (f[k][i][j + n - k] && f[n - k][i + k][j])) {
-                            f[n][i][j] = true;
-                            break;
-                        }
+        for (int n = 2; n <= N; n++) {
+            for (int i = 0; i + n <= N; i++) {
+                for (int j = 0; j + n <= N; j++) {
+                    for (int k = 1; k < n && !dp[n][i][j]; k++) {
+                        dp[n][i][j] = ( dp[k][i][j] && dp[n-k][i+k][j+k] ) 
+                                    || ( dp[k][i][j+n-k] && dp[n-k][i+k][j] );
                     }
                 }
             }
         }
         
-        return f[N][0][0];
+        return dp[N][0][0];
     }
 };
 
