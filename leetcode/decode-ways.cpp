bee6d9dec1bc57ac3313fc900fb828a852db4d0a
smilingpoplar <smilingpoplar@gmail.com>
2018-08-04 17:08:36 +0800

    update

--- leetcode/decode-ways.cpp
+++ leetcode/decode-ways.cpp
@@ -14,30 +14,20 @@ using namespace std;
 class Solution {
 public:
     int numDecodings(string s) {
-        // 设f(i)表示s[0,i-1]的解码数，0<=i<=N
-        // 先f(i)=0，若s[i-1]合法，f(i)+=f(i-1)；若s[i-2,i-1]合法，f(i)+=f(i-2)
-        if (s.empty()) return 0;
-        const int N = (int)s.size();
-        vector<int> f(N + 1, 0);
-        f[0] = 1;
-        for (int i = 1; i <= N; i++) {
-            if (isValid(s, i - 1, i - 1)) f[i] += f[i - 1];
-            if (isValid(s, i - 2, i - 1)) f[i] += f[i - 2];
+        // 设dp[i]表示s[i..]的解码数，0<=i<=N
+        // 若isValid(s[i,i]), dp[i] += dp[i+1]；若isValid(s[i,i+1])，dp[i] += dp[i+2]
+        // 初始dp[N] = 1
+        const int N = s.size();
+        vector<int> dp(N + 1, 0);
+        dp[N] = 1;
+        
+        for (int i = N - 1; i >= 0; i--) {
+            if (s[i] != '0') 
+                dp[i] += dp[i+1];
+            if (i + 1 < N && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) 
+                dp[i] += dp[i+2];
         }
-        return f[N];
-    }
-private:
-    // s[start,end]是否合法
-    bool isValid(const string &s, int start, int end) {
-        if (start < 0 || start > end) return false;
-        if (start == end) {
-            return '1' <= s[end] && s[end] <= '9';
-        }
-        if (start + 1 == end) {
-            return (s[start] == '1' && '0' <= s[end] && s[end] <= '9')
-            || (s[start] == '2' && '0' <= s[end] && s[end] <= '6');
-        }
-        return false;
+        return dp[0];
     }
 };
 
