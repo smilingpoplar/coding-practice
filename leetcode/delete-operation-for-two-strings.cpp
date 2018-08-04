2ccf9f513acf46789211d49edec0902205d61b86
smilingpoplar <smilingpoplar@gmail.com>
2018-08-05 00:15:51 +0800

    update

--- leetcode/delete-operation-for-two-strings.cpp
+++ leetcode/delete-operation-for-two-strings.cpp
@@ -13,23 +13,22 @@ using namespace std;
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        // 设dp[i,j]表示word1[0..i-1]和word2[0..j-1]的最长公共子序列长
-        // 初始dp[0,..]=dp[..,0]=0，所求为dp[n1,n2]
-        // word1[i-1]==word2[j-1] => dp[i,j] = 1 + dp[i-1,j-1], 
-        //           !=           =>         = max(dp[i,j-1], dp[i-1,j])
-        const int n1 = word1.size();
-        const int n2 = word2.size();
+        // 设dp[i,j]表示word1[i..]和word2[j..]的最长公共子序列长
+        // word1[i]==word2[j] => dp[i,j] = 1 + dp[i+1,j+1], 
+        //         !=         =>         = max(dp[i,j+1], dp[i+1,j])
+        // 初始dp[n1,..]=dp[..,n2]=0，所求dp[0,0]
+        const int n1 = word1.size(), n2 = word2.size();
         vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
-                if (word1[i-1] == word2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                if (word1[i] == word2[j]) {
+                    dp[i][j] = 1 + dp[i+1][j+1];
                 } else {
-                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                    dp[i][j] = max(dp[i][j+1], dp[i+1][j]);
                 }
             }
         }
-        return n1 + n2 - 2 * dp[n1][n2];        
+        return n1 + n2 - 2 * dp[0][0];
     }
 };
 
