44554e03c524277e5dc7f3dba390f18c0dea3ee3
smilingpoplar <smilingpoplar@gmail.com>
2018-08-05 13:41:17 +0800

    update

--- leetcode/maximum-product-subarray.cpp
+++ leetcode/maximum-product-subarray.cpp
@@ -14,25 +14,25 @@ using namespace std;
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // 因为最大乘积可能来自负负得正，所以除保留当前最大乘积，还得保留当前最小乘积
-        // 设f(i)表示nums[?,i]的最大乘积，g(i)表示nums[?,i]的最小乘积，0<=i<=N-1
-        // 当nums[i]>=0时，f(i) = max( f(i-1)*nums[i], nums[i] )，g(i) = min( g(i-1)*nums[i], nums[i] )
-        // 当nums[i]<0时，f(i) = max( g(i-1)*nums[i], nums[i]) ), g(i) = min( f(i-1)*nums[i], nums[i] )
-        // 综上，f(i) = max( f(i-1)*nums[i], nums[i], g(i-1)*nums[i] ), g(i) = min( f(i-1)*nums[i], nums[i], g(i-1)*nums[i] )
-        // 递推式只依赖于前一项，在i的递增循环中可降维，设f表示nums[?,i]的最大乘积，g表示nums[?,i]的最小乘积
+        // 因为最大乘积可能来自负负得正，所以除了保留当前最大乘积f[i]，还要保留当前最小乘积g[i]
+        // 设f[i]表示以i结尾的nums[..i]的最大乘积，g(i)表示以i结尾的nums[..i]的最小乘积，0<=i<N
+        // 当nums[i]>=0时，f[i] = max( f[i-1]*nums[i], nums[i] )，g[i] = min( g[i-1]*nums[i], nums[i] )
+        // 当nums[i]<0时， f[i] = max( g(i-1]*nums[i], nums[i] )，g[i] = min( f[i-1]*nums[i], nums[i] )
+        // 综上，f[i] = max( f[i-1]*nums[i], nums[i], g[i-1]*nums[i] ), 
+        // g[i] = min( f[i-1]*nums[i], nums[i], g[i-1]*nums[i] )
+        // 在i维上只依赖于i-1项，可省掉i维，i仍从左到右遍历，
         // f = max( f*nums[i], nums[i], g*nums[i] ), g = min( f*nums[i], nums[i], g*nums[i] )
-        const int N = (int)nums.size();
-        int maxProduct = INT_MIN;
-        int f = 1;
-        int g = 1;
-        for (int i = 0; i < N; i++) {
-            int tmpF = f * nums[i];
-            int tmpG = g * nums[i];
-            f = max({tmpF, nums[i], tmpG});
-            g = min({tmpF, nums[i], tmpG});
-            if (f > maxProduct) maxProduct = f;
+        const int N = nums.size();
+        int ans = INT_MIN;
+        int f = 1, g = 1;
+        for (int num : nums) {
+            int tmpF = f * num;
+            int tmpG = g * num;
+            f = max({tmpF, num, tmpG});
+            g = min({tmpF, num, tmpG});
+            if (f > ans) ans = f;
         }
-        return maxProduct;
+        return ans;
     }
 };
 
