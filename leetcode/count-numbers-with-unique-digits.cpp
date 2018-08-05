8dfd2af4d5205e09cc7e06aec77e71aef90a6bf0
smilingpoplar <smilingpoplar@gmail.com>
2018-08-05 23:53:33 +0800

    update

--- leetcode/count-numbers-with-unique-digits.cpp
+++ leetcode/count-numbers-with-unique-digits.cpp
@@ -15,19 +15,18 @@ class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
         // dp[0]  1
-        // dp[1]  9+dp[0]
-        // dp[2]  9*9+dp[1]
-        // dp[3]  9*9*8+dp[2]
+        // dp[1]  dp[0]+9
+        // dp[2]  dp[1]+9*9
+        // dp[3]  dp[2]+9*9*8
         // ...
-        // dp[k]  9*9*8*7*6*5*4*3*2*1*0+dp[k-1]，k为11
+        // dp[k]  dp[k-1]+9*9*8*7*6*5*4*3*2*1*0，k为11
         if (n == 0) return 1;
-        int prev = 1;
-        int prod = 9;
+        int prev = 1, prod = 9; // 对应dp[1]行
         for (int i = 2; i <= min(n, 10); i++) {
             prev += prod;
-            prod *= (11 - i);
+            prod *= 11 - i;
         }
-        return prod + prev;
+        return prev + prod;
     }
 };
 
