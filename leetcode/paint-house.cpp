18b507c050a70491794f6d29546d6c87584fa574
smilingpoplar <smilingpoplar@gmail.com>
2018-08-05 18:51:57 +0800

    update

--- leetcode/paint-house.cpp
+++ leetcode/paint-house.cpp
@@ -14,16 +14,16 @@ using namespace std;
 class Solution {
 public:
     int minCost(vector<vector<int>>& costs) {
-        // 设dp[i][j]表示到房子i漆上色j的最小代价，有dp[i][j]=min(dp[i-1][k])+cost[i][j], k!=j
-        // 递推式i这维只依赖于i-1项，去掉i这维，i仍从左往右遍历。j这维依赖方向不确定，要使用临时变量newdp[]。
-        // dp[j]=min(dp[k])+cost[i][j], k!=j。初始dp[j]=cost[0][j]。
+        // 设dp[i][c]表示前[0..i]房子、第i房颜色为c时的最小代价，有dp[i][c]=min(dp[i-1][not_c])+cost[i][c]
+        // 递推式i这维只依赖于i-1项，去掉i这维，i仍从左往右遍历。c这维的依赖方向不确定，要使用临时变量newdp[]。
+        // dp[c]=min(dp[not_c])+cost[i][c]。初始dp[c]=cost[0][c]。
         if (costs.empty()) return 0;
         const int N = costs.size(), C = costs[0].size();
         vector<int> dp = costs[0];
         for (int i = 1; i < N; i++) {
             vector<int> newdp(C);
-            for (int j = 0; j < C; j++) {
-                newdp[j] = min(dp[(j+1) % C], dp[(j+2) % C]) + costs[i][j];
+            for (int c = 0; c < C; c++) {
+                newdp[c] = min(dp[(c+1) % C], dp[(c+2) % C]) + costs[i][c];
             }
             swap(newdp, dp);
         }
