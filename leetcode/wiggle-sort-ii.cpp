2953dd31a59e8b14a3001f361c473e713f8d1de6
smilingpoplar <smilingpoplar@gmail.com>
2018-08-06 18:00:50 +0800

    update

--- leetcode/wiggle-sort-ii.cpp
+++ leetcode/wiggle-sort-ii.cpp
@@ -15,11 +15,20 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         const int N = nums.size();
-        int median = findKthLargest(nums, 0, N - 1, (N + 1) / 2);
-        // 虚拟下标映射：i => (2*i+1) % (N|1)
+        // 按逆序处理，所以下面找kthLargest、三路划分先找>median
+        // (N+1)/2是中位数，因为取下整，偶数个时指向偏前
+        int median = findKthLargest(nums, (N + 1) / 2);
+        // 这里也可以调用nth_element()（第n个最小的数）找偏后的中位数
+        // auto midptr = nums.begin() + N / 2;
+        // nth_element(nums.begin(), midptr, nums.end());
+        // int median = *midptr;
+        
+        // 把[mid,..]放到偶位、[0,mid)放到奇位的下标映射：i => (2*i+1) % (N|1)
         // 见 https://leetcode.com/problems/wiggle-sort-ii/discuss/77677/O(n)+O(1)-after-median-Virtual-Indexing
         auto idx = [&](int i) { return (2*i+1) % (N|1); };
-        // 三路划分：>median、==median、<median
+        // 三路划分：>median、==median、 ?  、<median
+        //                 i         j   k
+        // i指向>median待写入位置，j指向待处理位置，k指向<median待写入位置
         int i = 0, j = 0, k = nums.size() - 1;
         while (j <= k) {
             if (nums[idx(j)] > median) {
@@ -35,12 +44,14 @@ public:
         }
     }
 
-    int findKthLargest(vector<int>& nums, int l, int u, int k) {
-        int p = partition(nums, l, u);
-        int order = p - l + 1; // nums[p]是第几大的数
-        if (k == order) return nums[p];
-        if (k < order) return findKthLargest(nums, l, p - 1, k);
-        return findKthLargest(nums, p + 1, u, k - order);
+    int findKthLargest(vector<int>& nums, int k) {
+        int l = 0, u = (int)nums.size() - 1;
+        while (l <= u) {
+            int p = partition(nums, l, u); // p跟下标k-1比较，因为第k大数的下标应该是k-1
+            if (k - 1 == p) return nums[p];
+            if (k - 1 < p) u = p - 1;
+            else l = p + 1;
+        }
     }
 
     int partition(vector<int> &nums, int l, int u) {
--- leetcode/wiggle-sort.cpp
+++ leetcode/wiggle-sort.cpp
@@ -14,7 +14,7 @@ using namespace std;
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        // 从左往右，把不符合要求的数对交换
+        // 从左往右，把不符合要求的相邻数对换
         for (int i = 0; i + 1 < nums.size(); i++) {
             if ((i % 2 == 0 && nums[i] > nums[i+1]) || (i % 2 == 1 && nums[i] < nums[i+1])) {
                 swap(nums[i], nums[i+1]);
