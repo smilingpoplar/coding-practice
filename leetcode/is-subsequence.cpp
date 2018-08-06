d0f1bcf90b79fe960b36ce7052ac90da5e7d8e68
smilingpoplar <smilingpoplar@gmail.com>
2018-08-06 22:13:37 +0800

    update

--- leetcode/is-subsequence.cpp
+++ leetcode/is-subsequence.cpp
@@ -15,7 +15,7 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         int i = 0;
-        for (int j = 0; j < t.size() && i < s.size(); j++) {
+        for (int j = 0; j < t.size() && i < s.size(); j++) { // 在t中搜索s
             if (s[i] == t[j]) i++;
         }
         return i == s.size();
