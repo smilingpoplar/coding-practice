705fdadee63b1add5c7dbf23f33a302c1c5586a9
smilingpoplar <smilingpoplar@gmail.com>
2018-08-09 01:20:59 +0800

    update

--- leetcode/maximum-vacation-days.cpp
+++ leetcode/maximum-vacation-days.cpp
@@ -15,25 +15,26 @@ class Solution {
 public:
     int maxVacationDays(vector<vector<int>>& flights, vector<vector<int>>& days) {
         // flights[][]是城市间的邻接矩阵，days[][]查某城市哪一周能玩几天
-        // 设dp[i][k]表示当前在城市i、第k周、从今往后的最大假期，下一周的城市j可能是i或者flights[i]可飞的城市，
-        // dp[i][k] = max{ days[j][k+1] + dp[j][k+1] }。初始dp[][K]=0。
-        // dp[][k]只依赖于dp[][k+1]，降掉k这维，用curr[]表示dp[][k]，next[]表示dp[][k+1]。
+        // 设dp[i][k]表示当前在城市i、第k周刚开始、从今往后的最大休假日，
+        // 这周休假的城市j可能是i或者flights[i]可达的城市，
+        // dp[i][k] = max{ days[j][k] + dp[j][k+1] }。初始dp[][K]=0。
+        // 递推式k这维只依赖于k+1项，省掉k这维，k仍从右往左遍历
         if (flights.empty() || days.empty()) return 0;
         const int N = flights.size();
         const int K = days[0].size();
-        vector<int> next(N, 0);
+        vector<int> dp(N, 0);
         for (int k = K - 1; k >= 0; k--) {
-            vector<int> curr(N, 0);
+            vector<int> newdp(N, 0);
             for (int i = 0; i < N; i++) {
-                curr[i] = days[i][k] + next[i]; // 呆城市i
-                for (int j = 0; j < N; j++) {
+                newdp[i] = days[i][k] + dp[i]; // 呆城市i
+                for (int j = 0; j < N; j++) { // 飞城市j
                     if (flights[i][j] == 0) continue;
-                    curr[i] = max(curr[i], days[j][k] + next[j]); // 飞城市j
+                    newdp[i] = max(newdp[i], days[j][k] + dp[j]);
                 }
             }
-            swap(curr, next);
+            swap(newdp, dp);
         }
-        return next[0];
+        return dp[0];
     }
 };
 
