e89841151447a9d8e51708cd06b8415c68cd5953
smilingpoplar <smilingpoplar@gmail.com>
2018-08-13 22:49:44 +0800

    update

--- leetcode/coin-path.cpp
+++ leetcode/coin-path.cpp
@@ -14,19 +14,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> cheapestJump(vector<int>& A, int B) {
-        // "当有两条等长的最小代价路径时，取词典序小的那个"，即当存在[x..i..y]和[x..j..y]时，取[x..i..y]
-        // 要满足这个条件，只需dp从后往前进行，即定义dp[i]表示从A[i]到A[n-1]的最小代价，
-        // 并在dp[i]按序尝试i+1,i+2,...,i+B，先找到的最小代价就满足字典序小
-        // dp[i] = min(dp[i], dp[i+x] + A[i])，1<=x<=B
+        // "两条路径同样代价且同样长时，取词典序小的那个"
+        // 设dp[i]表示从A[i..N-1]的最小代价，在dp[i]按序尝试i+1,i+2,...,i+B，
+        // 先找到的最小代价就满足字典序小。dp[i] = min{ dp[i+x] + A[i] }，1<=x<=B
         // 初始dp[N-1] = 0
         const int N = A.size();
         vector<int> dp(N, INT_MAX);
         dp[N-1] = 0;
-        vector<int> next(N, -1);
+        vector<int> next(N, -1); // 记录路径
         for (int i = N - 2; i >= 0; i--) {
             if (A[i] == -1) continue;
-            int minJ = i + 1, maxJ = min(i + B, N - 1);
-            for (int j = minJ; j <= maxJ; j++) {
+            int maxJ = min(i + B, N - 1);
+            for (int j = i + 1; j <= maxJ; j++) {
                 if (A[j] == -1) continue;
                 int cost = dp[j] + A[i];
                 if (cost < dp[i]) {
@@ -36,7 +35,6 @@ public:
             }
         }
         
-        // 找路径
         vector<int> ans;
         for (int i = 0; i != -1; i = next[i]) {
             ans.push_back(i + 1); // 1-indexed
