754b3ede7c58be560a7c2758cf427c53ad5b448d
smilingpoplar <smilingpoplar@gmail.com>
2018-08-20 12:16:48 +0800

    update

--- leetcode/merge-sorted-array.cpp
+++ leetcode/merge-sorted-array.cpp
@@ -15,9 +15,7 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         // 从后往前合并
-        int i1 = m - 1;
-        int i2 = n - 1;
-        int i = m + n - 1;
+        int i1 = m - 1, i2 = n - 1, i = m + n - 1;
         while (i1 >= 0 && i2 >= 0) {
             nums1[i--] = nums1[i1] >= nums2[i2] ? nums1[i1--] : nums2[i2--];
         }
