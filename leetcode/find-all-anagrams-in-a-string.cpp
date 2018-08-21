afbc7461b9c844be0754842648c1c16e01880456
smilingpoplar <smilingpoplar@gmail.com>
2018-08-21 09:20:33 +0800

    update

--- leetcode/find-all-anagrams-in-a-string.cpp
+++ leetcode/find-all-anagrams-in-a-string.cpp
@@ -19,15 +19,13 @@ public:
         int distinct = count.size(); // 要找多少个不同的字符
         
         vector<int> ans;
-        // 每次end移动一步：若是有效窗口、start再移动至无效窗口
-        int start = 0, end = 0;
-        while (end < s.size()) {
-            if (--count[s[end]] == 0) distinct--;
-            end++;
+        // 每次hi移动一步：若是有效窗口、lo再移动至无效窗口
+        for (int lo = 0, hi = 0; hi < s.size(); hi++) {
+            if (--count[s[hi]] == 0) distinct--;
             while (distinct == 0) { // 有效窗口
-                if (end - start == p.size()) ans.push_back(start);
-                if (count[s[start]]++ == 0) distinct++;
-                start++;
+                if (hi - lo + 1 == p.size()) ans.push_back(lo);
+                if (count[s[lo]]++ == 0) distinct++;
+                lo++;
             }
         }
         return ans;
