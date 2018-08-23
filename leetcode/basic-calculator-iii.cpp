54d0552ed775f2578db58d0c52cc20ae89c001d7
smilingpoplar <smilingpoplar@gmail.com>
2018-08-23 17:15:19 +0800

    update

--- leetcode/basic-calculator-iii.cpp
+++ leetcode/basic-calculator-iii.cpp
@@ -13,37 +13,56 @@ using namespace std;
 
 class Solution {
 public:
-    int calculate(string s) {
-        int i = 0;
-        return parseExpr(s, i);
-    }
-    
-    // 解析一层括号
-    int parseExpr(const string &s, int &i) {
-        const int N = s.size();
-        char op = '+'; // 上一个未处理op
-        vector<int> nums;
-        for (; i < N && op != ')'; i++) { // 尚有未处理op
-            if (s[i] == ' ') continue;
-            int num = 0;
-            if (isdigit(s[i])) {
-                while (i < N && isdigit(s[i]))
-                    num = num * 10 + s[i++] - '0';
-            } else if (s[i] == '(') {
-                num = parseExpr(s, ++i);
-            } 
-            // 遇到新操作+-*/或结束，处理上一个op
-            if (op == '+') nums.push_back(num);
-            else if (op == '-') nums.push_back(-num);
-            else if (op == '*') nums.back() *= num;
-            else if (op == '/') nums.back() /= num;
-            op = s[i];
-        }
+    int calculate(string &s) {
+        // 中缀转后缀，用nums栈和ops栈
+        // num、左括号'('先入栈，遇到右括号')'时不断弹出计算直到弹出'('，
+        // 遇到下一个优先级<=栈顶的op时、先弹出计算再把op入栈，左括号'('的优先级最小。
+        unordered_map<char, int> priority = {
+            {'*', 2}, {'/', 2},
+            {'+', 1}, {'-', 1},
+            {'(', 0},
+        };
         
-        int ans = 0;
-        for (int num : nums) 
-            ans += num;
-        return ans;
+        stack<int> nums;
+        stack<char> ops;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (c == ' ') continue;
+            if (c == '(') {
+                ops.push(c);
+            } else if (isdigit(c)) {
+                int num = c - '0';
+                while (i + 1 < s.size() && isdigit(s[i+1]))
+                    num = num * 10 + s[++i] - '0';
+                nums.push(num);
+            } else if (c == ')') {
+                while (ops.top() != '(') calc(nums, ops);
+                ops.pop(); // 弹出'('
+            } else { // +-*/
+                while (!ops.empty() && priority[c] <= priority[ops.top()]) calc(nums, ops);
+                ops.push(c);
+            }
+        }
+        while (!ops.empty()) calc(nums, ops);
+        
+        return nums.top();
+    }
+
+    void calc(stack<int> &nums, stack<char> &ops) {
+        if (ops.empty() || nums.size() < 2) return;
+        char op = ops.top(); ops.pop();
+        int val2 = nums.top(); nums.pop();
+        int val1 = nums.top(); nums.pop();
+        
+        if (op == '*') {
+            nums.push(val1 * val2);
+        } else if (op == '/') {
+            nums.push(val1 / val2);
+        } else if (op == '+') {
+            nums.push(val1 + val2);
+        } else if (op == '-') {
+            nums.push(val1 - val2);
+        }
     }
 };
 
