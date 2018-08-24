45b2149622ac7ada2e0f58a069d83d4573f559b7
smilingpoplar <smilingpoplar@gmail.com>
2018-08-24 10:40:31 +0800

    update

--- leetcode/ugly-number-ii.cpp
+++ leetcode/ugly-number-ii.cpp
@@ -16,8 +16,7 @@ public:
     int nthUglyNumber(int n) {
         vector<int> seq;
         seq.push_back(1);
-        // 每个因子对应一个要相乘的丑数，用以生成下个丑数
-        int i2 = 0, i3 = 0, i5 = 0; // ix是索引
+        int i2 = 0, i3 = 0, i5 = 0;
         for (int i = 1; i < n; i++) {
             int next = min({ seq[i2] * 2, seq[i3] * 3, seq[i5] * 5 });
             seq.push_back(next);
@@ -25,8 +24,7 @@ public:
             if (next == seq[i3] * 3) i3++;
             if (next == seq[i5] * 5) i5++;
         }
-        
-        return seq[n-1];
+        return seq.back();
     }
 };
 
