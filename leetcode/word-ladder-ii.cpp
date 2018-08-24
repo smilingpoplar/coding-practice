4b0b915e4a64afb8f8971a0092bacfbaa6dc2095
smilingpoplar <smilingpoplar@gmail.com>
2018-08-24 18:07:18 +0800

    update

--- leetcode/word-ladder-ii.cpp
+++ leetcode/word-ladder-ii.cpp
@@ -17,66 +17,54 @@ class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
         unordered_set<string> dict;
-        for (auto &word : wordList) {
-            dict.insert(word);
-        }
+        for (auto &word : wordList) dict.insert(word);
         
+        // 分层bfs
         unordered_set<string> currLevel;
         currLevel.insert(beginWord);
         unordered_set<string> visited;
-
-        unordered_map<string, unordered_set<string>> prevs;
+        unordered_map<string, unordered_set<string>> prevs; // 记录词变换
         bool found = false;
-        while (!currLevel.empty()) {
+        while (!currLevel.empty() && !found) {
             // 将同层词的visited[]全部设置，避免向同层词扩展
-            for (auto &word : currLevel) {
-                visited.insert(word);
-            }
+            for (auto &word : currLevel) visited.insert(word);
             unordered_set<string> nextLevel;
             for (auto &word : currLevel) {
                 auto nextWords = getNextWords(word, endWord, dict);
                 for (auto &nextWord : nextWords) {
-                    if (!visited.count(nextWord)) {
-                        nextLevel.insert(nextWord);
-                        prevs[nextWord].insert(word);
-                        if (nextWord == endWord) found = true;
-                    }
+                    if (visited.count(nextWord)) continue;
+                    nextLevel.insert(nextWord);
+                    prevs[nextWord].insert(word);
+                    if (nextWord == endWord) found = true;
                 }
             }
             swap(nextLevel, currLevel);
-            // BFS时如果找到一条最短路径，那么所有最短路径一定都在这一层，不再考虑下一层
-            if (found) break;
         }
         
-        return genPaths(endWord, beginWord, prevs);
+        vector<vector<string>> ans;
+        vector<string> path;
+        genPaths(endWord, beginWord, prevs, path, ans);
+        return ans;
     }
     
 private:
     unordered_set<string> getNextWords(const string &word, 
                 const string &endWord, const unordered_set<string> &dict) {
         unordered_set<string> ans;
-        for (size_t i = 0; i < word.size(); i++) {
-            string newWord(word);
+        string nextWord(word);
+        for (size_t i = 0; i < nextWord.size(); i++) {
             for (char c = 'a'; c <= 'z'; c++) {
-                if (newWord[i] == c) continue;
+                if (nextWord[i] == c) continue;
 
-                swap(c, newWord[i]);                    
-                if (dict.count(newWord)) ans.insert(newWord);
-                swap(c, newWord[i]);
+                swap(c, nextWord[i]);                    
+                if (dict.count(nextWord)) ans.insert(nextWord);
+                swap(c, nextWord[i]);
             }
         }
         return ans;
     }
     
-    vector<vector<string>> genPaths(const string &word, const string &beginWord, 
-                                    unordered_map<string, unordered_set<string>> &prevs) {
-        vector<vector<string>> ans;
-        vector<string> path;
-        rGenPaths(word, beginWord, prevs, path, ans);
-        return ans;
-    }
-    
-    void rGenPaths(const string &word, const string &beginWord, 
+    void genPaths(const string &word, const string &beginWord, 
                 unordered_map<string, unordered_set<string>> &prevs,
                 vector<string> &path, vector<vector<string>> &ans) {
         path.push_back(word); // 先反向存，最后reverse
@@ -84,7 +72,7 @@ private:
             ans.push_back(vector<string>(path.rbegin(), path.rend()));
         } else {
             for (auto &p : prevs[word]) {
-                rGenPaths(p, beginWord, prevs, path, ans);
+                genPaths(p, beginWord, prevs, path, ans);
             }
         }
         path.pop_back();
