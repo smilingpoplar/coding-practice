4ddd1ae69589c17369eb3b808f5df8f60e03a044
smilingpoplar <smilingpoplar@gmail.com>
2018-08-25 17:43:25 +0800

    update

--- leetcode/combination-sum-ii.cpp
+++ leetcode/combination-sum-ii.cpp
@@ -17,11 +17,11 @@ public:
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> comb;
-        search(candidates, target, 0, comb, ans);
+        search(candidates, 0, target, comb, ans);
         return ans;
     }
 
-    void search(const vector<int> candiatates, int target, int idx,
+    void search(const vector<int> candiatates, int idx, int target,
              vector<int> &comb, vector<vector<int>> &ans) {
         if (target < 0) return;
         if (target == 0) {
@@ -33,7 +33,7 @@ public:
             // 有重复元素，相同元素只选第一个
             if (i > idx && candiatates[i] == candiatates[i-1]) continue;
             comb.push_back(candiatates[i]);
-            search(candiatates, target - candiatates[i], i + 1, comb, ans);
+            search(candiatates, i + 1, target - candiatates[i], comb, ans);
             comb.pop_back();
         }
     }
