ab5607105a9aa51cd6c20c3c78f929bca3601a83
smilingpoplar <smilingpoplar@gmail.com>
2018-08-28 18:54:34 +0800

    update

---
+++ leetcode/friends-of-appropriate-ages.cpp
@@ -0,0 +1,38 @@
+//
+//  friends-of-appropriate-ages
+//  https://leetcode.com/problems/friends-of-appropriate-ages/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numFriendRequests(vector<int>& ages) {
+        unordered_map<int, int> cnt;
+        for (int age : ages) cnt[age]++;
+        
+        int ans = 0;
+        for (auto &eA : cnt) {
+            int ageA = eA.first, cntA = eA.second;
+            for (auto &eB : cnt) {
+                int ageB = eB.first, cntB = eB.second;
+                if (ageB <= 0.5 * ageA + 7) continue;
+                if (ageB > ageA) continue;
+                if (ageB > 100 && ageA < 100) continue;
+                if (ageA != ageB) ans += cnt[ageA] * cnt[ageB];
+                else ans += cnt[ageA] * (cnt[ageA] - 1); // 不能和自己交朋友
+            }
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
