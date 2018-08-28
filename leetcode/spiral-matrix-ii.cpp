a131d0730c942ca1c33fc1208ba85893ee8635cd
smilingpoplar <smilingpoplar@gmail.com>
2018-08-28 12:08:08 +0800

    update

--- leetcode/spiral-matrix-ii.cpp
+++ leetcode/spiral-matrix-ii.cpp
@@ -15,36 +15,34 @@ class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
         vector<vector<int>> matrix(n, vector<int>(n));
-        int r1 = 0, r2 = n - 1;
-        int c1 = 0, c2 = n - 1;
-        int count = 0;
+        int r1 = 0, r2 = n - 1, c1 = 0, c2 = n - 1;
+        int cnt = 0;
         while (true) {
             // right
-            if (r1 > r2 || c1 > c2) break;
+            if (c1 > c2) break;
             for (int c = c1; c <= c2; c++) {
-                matrix[r1][c] = ++count;
+                matrix[r1][c] = ++cnt;
             }
             r1++;
             // down
-            if (r1 > r2 || c1 > c2) break;
+            if (r1 > r2) break;
             for (int r = r1; r <= r2; r++) {
-                matrix[r][c2] = ++count;
+                matrix[r][c2] = ++cnt;
             }
             c2--;
             // left
-            if (r1 > r2 || c1 > c2) break;
+            if (c1 > c2) break;
             for (int c = c2; c >= c1; c--) {
-                matrix[r2][c] = ++count;
+                matrix[r2][c] = ++cnt;
             }
             r2--;
             // up
-            if (r1 > r2 || c1 > c2) break;
+            if (r1 > r2) break;
             for (int r = r2; r >= r1; r--) {
-                matrix[r][c1] = ++count;
+                matrix[r][c1] = ++cnt;
             }
             c1++;
         }
-        
         return matrix;
     }
 };
@@ -52,9 +50,9 @@ public:
 int main(int argc, const char * argv[]) {
     Solution solution;
     auto matrix = solution.generateMatrix(3);
-    for (const auto &row : matrix) {
-        for (const auto num: row) {
-            cout << num << " ";
+    for (auto &row : matrix) {
+        for (auto val: row) {
+            cout << val << " ";
         }
         cout << endl;
     }
