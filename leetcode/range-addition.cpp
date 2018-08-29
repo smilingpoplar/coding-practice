8340133620df89e0f3b70b032619491b7b1d3a9a
smilingpoplar <smilingpoplar@gmail.com>
2018-08-29 22:28:52 +0800

    update

--- leetcode/range-addition.cpp
+++ leetcode/range-addition.cpp
@@ -14,16 +14,15 @@ using namespace std;
 class Solution {
 public:
     vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
-        // 要执行许多更新操作，想到把更新操作简化。
-        // 这里把更新操作记在范围边界上，相当于只记录在范围边界发生的变化量。
-        // 对于[startIdx, endIdx, inc]，在startIdx记录+inc，在endIdx+1记录-inc。
-        // 最后将所有变化量累加，相当于让变化生效。
+        // 只在区间边界记录变化量：
+        // 对于[startIdx, endIdx, inc]，在startIdx记录+inc，在endIdx+1记录-inc
         vector<int> ans(length, 0);
         for (auto &update : updates) {
             int start = update[0], end = update[1], inc = update[2];
             ans[start] += inc;
-            if (end + 1 < length) ans[end + 1] -= inc;
+            if (end + 1 < length) ans[end+1] -= inc;
         }
+        // 最后将变化量累加
         for (int i = 1; i < length; i++) {
             ans[i] += ans[i-1];
         }
