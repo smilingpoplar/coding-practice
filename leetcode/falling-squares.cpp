560f6bc22604d3a6a4f951feb925c2d41546d575
smilingpoplar <smilingpoplar@gmail.com>
2018-09-03 18:39:10 +0800

    update

--- leetcode/falling-squares.cpp
+++ leetcode/falling-squares.cpp
@@ -11,6 +11,55 @@
 using namespace std;
 
 class Solution {
+    struct Interval {
+        int left;
+        int right;
+        int height;
+    };
+    struct Cmp {
+        bool operator()(const Interval &a, const Interval &b) {
+            return a.right < b.right;
+        }
+    };
+    set<Interval, Cmp> st;
+    
+    bool isOverlap(const Interval &a, const Interval &b) {
+        return a.left < b.right && b.left < a.right;
+    }
+public:
+    vector<int> fallingSquares(vector<pair<int, int>>& pos) {
+        vector<int> ans;
+        int highest = 0;
+        for (auto &p : pos) {
+            int left = p.first, right = p.first + p.second, height = p.second;
+            // 删除[left..right)区间
+            // 首个与[left..right)重叠的区间toFind.right > left
+            auto it = st.upper_bound({INT_MIN, left, 0});
+            Interval toRemove = {left, right};
+            vector<Interval> afterRemove;
+            int base = 0; // 新块儿放置的高度是重叠区间的最大高度
+            while (it != st.end() && isOverlap(*it, toRemove)) {
+                if (it->left < toRemove.left) 
+                    afterRemove.push_back({ it->left, toRemove.left, it->height });
+                if (toRemove.right < it->right)
+                    afterRemove.push_back({ toRemove.right, it->right, it->height });
+                base = max(base, it->height);
+                it = st.erase(it);
+            }
+            st.insert(afterRemove.begin(), afterRemove.end());
+            // 插入新块儿
+            height += base;
+            st.insert({left, right, height});
+
+            highest = max(highest, height);
+            ans.push_back(highest);
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
 public:
     vector<int> fallingSquares(vector<pair<int, int>>& pos) {
         // 每个块儿看会不会和前面块儿重叠，重叠时要提高当前块儿的摆放高度
@@ -32,6 +81,7 @@ public:
         return ans;
     }
 };
+*/
 
 int main(int argc, const char * argv[]) {    
     return 0;
