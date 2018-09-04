3028d98000099b1c9f12ef9529d5dd4165805951
smilingpoplar <smilingpoplar@gmail.com>
2018-09-04 11:08:33 +0800

    update

--- leetcode/maximum-depth-of-binary-tree.cpp
+++ leetcode/maximum-depth-of-binary-tree.cpp
@@ -21,8 +21,6 @@ class Solution {
 public:
     int maxDepth(TreeNode *root) {
         if (!root) return 0;
-        if (!root->left) return 1 + maxDepth(root->right);
-        if (!root->right) return 1 + maxDepth(root->left);
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
