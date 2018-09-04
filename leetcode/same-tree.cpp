a85fa5077561534233d1889bdfa360b3f86e7af3
smilingpoplar <smilingpoplar@gmail.com>
2018-09-04 22:10:33 +0800

    update

--- leetcode/same-tree.cpp
+++ leetcode/same-tree.cpp
@@ -23,8 +23,8 @@ public:
         if (!p && !q) return true;
         if (!p || !q) return false;
         return p->val == q->val
-        && isSameTree(p->left, q->left)
-        && isSameTree(p->right, q->right);
+            && isSameTree(p->left, q->left)
+            && isSameTree(p->right, q->right);
     }
 };
 
