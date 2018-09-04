aba100214f1852ca3f3544523b47900c78396da6
smilingpoplar <smilingpoplar@gmail.com>
2018-09-04 12:29:09 +0800

    update

---
+++ leetcode/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,35 @@
+//
+//  smallest-subtree-with-all-the-deepest-nodes
+//  https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        return getHeight(root).second;
+    }
+    
+    // 返回：(height, substreeWithAllDeepest)
+    pair<int, TreeNode*> getHeight(TreeNode *root) {
+        if (!root) return {0, NULL};
+        auto left = getHeight(root->left);
+        auto right = getHeight(root->right);
+        
+        int h1 = left.first, h2 = right.first;
+        if (h1 > h2) return {1 + h1, left.second};
+        if (h1 < h2) return {1 + h2, right.second};
+        return {1 + h1, root};
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
