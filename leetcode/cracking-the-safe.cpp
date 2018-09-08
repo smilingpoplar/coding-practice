dba5aa5ee0a90eb9ac33d7f9e2f5755137775bab
smilingpoplar <smilingpoplar@gmail.com>
2018-09-08 23:49:27 +0800

    update

--- leetcode/cracking-the-safe.cpp
+++ leetcode/cracking-the-safe.cpp
@@ -14,14 +14,14 @@ using namespace std;
 class Solution {
 public:
     string crackSafe(int n, int k) {
-        // 前n-1个数字的序列构成一个节点，共k^(n-1)个。每个节点指向去掉开头、再拼上[0..k-1]中某数的节点，也就是所有节点。
-        // 这题就是要找出遍历所有边的路径，即欧拉回路。因为这是个全连接图，有向图每个节点的入度等于出度，所以存在欧拉回路。
-        // 用边的后序遍历求逆欧拉路径，类似用点的后序遍历求逆拓扑序。用(节点+下一节点头)表示边。
+        // 前n-1个数字的序列构成节点，共k^(n-1)个。每个节点指向去掉开头、再拼上[0..k-1]中某数的节点，即指向所有节点。
+        // 要找出遍历所有边的欧拉路径。因为这是个全连接图，有向图每个节点的入度等于出度，所以存在欧拉回路。
+        // 用(节点+下一节点头)表示边，用visited记录访问过的边。
         string src(n - 1, '0');
         unordered_set<string> visited;
         string ans;
         dfs(src, k, visited, ans);
-        ans.append(src); // 用后序遍历
+        ans += src; // 后序遍历
         // reverse(ans.begin(), ans.end()); // u->..->v..->v..->u，全连接图的欧拉回路正好对称
         return ans;
     }
@@ -33,7 +33,7 @@ public:
             if (visited.count(edge)) continue;
             visited.insert(edge);
             dfs(edge.substr(1), k, visited, ans);
-            ans.append(s); // 用后序遍历
+            ans += s; // 后序遍历
         }
     }
 };
