6cd05851105380c078c4db947c9905da07ce2707
smilingpoplar <smilingpoplar@gmail.com>
2018-09-09 10:32:15 +0800

    update

--- leetcode/redundant-connection-ii.cpp
+++ leetcode/redundant-connection-ii.cpp
@@ -14,17 +14,25 @@ using namespace std;
 class Solution {
 public:
     vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        // 两种可并存的情况：
+        // 冗余边指向非根节点 => 该节点有两个父节点
+        //      指向祖父节点 => 有环
+        //
+        // 先找有两个父节点的节点，找到的话把两条父边作为冗余候选，从图中去掉其中一条。
+        // 再用并查集找环，若新边的两端已同属一个集合则有环。
+        // 若上一步有候选，这一步还有环，说明冗余边是保留的候选边；
+        //        无           有              后加入的导致有环的边；
+        //        有           无              去掉的侯选边。
         const int N = edges.size();
-        // 找有两个父节点的节点
         vector<int> parent(N + 1, 0);
         vector<int> candA, candB;
         for (auto &edge : edges) {
             if (parent[edge[1]] == 0) {
                 parent[edge[1]] = edge[0];
-            } else { // 找到
+            } else { // 有两个父节点，将两条父边作为冗余候选
                 candA = {parent[edge[1]], edge[1]};
                 candB = edge;
-                edge[1] = -1; // 去掉一条父边
+                edge[1] = -1; // 从图中去掉candB
             }
         }
         // 并查集找环
@@ -33,12 +41,12 @@ public:
             if (edge[1] == -1) continue; // 上一步中去掉的边
             int pu = find(edge[0], parent), pv = find(edge[1], parent);
             if (pu == pv) { // 有环
-                if (!candA.empty()) return candA; // 上一步中有两个父节点的节点并去掉一条父边，却还是有环，说明去掉了错误的边
-                return edge; // 不存在有两个父节点的节点，去掉新加入的导致有环的边
+                if (!candA.empty()) return candA; // 有候选
+                return edge; // 无候选
             }
             parent[pu] = pv; // unite
         }
-        return candB; // 无环，去掉后加入的导致出现两个父节点的边
+        return candB; // 无环
     }
     
     int find(int x, vector<int> &parent) {
