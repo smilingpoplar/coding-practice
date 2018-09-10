cd41252abe583922f5f779f04f9a5911fc65945b
smilingpoplar <smilingpoplar@gmail.com>
2018-09-10 17:01:43 +0800

    update

--- leetcode/binary-tree-upside-down.cpp
+++ leetcode/binary-tree-upside-down.cpp
@@ -25,7 +25,7 @@ public:
     TreeNode* upsideDownBinaryTree(TreeNode* root) {
         // 已知右节点为叶节点、左节点非空，或右节点为空。
         // 此题要将二叉树右转90度：左节点变根节点、右节点变左节点、根节点变右节点。
-        // 递归，假设本函数能处理好左子树，只需考虑当前节点、左节点、右节点这三者的关系。
+        // 递归，假设本函数能处理好左子树，只需考虑根节点、左节点、右节点这三者的关系变换。
         if (!root || !root->left) return root;
         auto newRoot = upsideDownBinaryTree(root->left);
         // root->left变成root->right、root的父节点
@@ -36,6 +36,29 @@ public:
         return newRoot;
     }
 };
+
+/*
+class Solution {
+public:
+    TreeNode* upsideDownBinaryTree(TreeNode* root) {
+        // 已知右节点为叶节点、左节点非空，或右节点为空。
+        // 此题要将二叉树右转90度：左节点变根节点、右节点变左节点、根节点变右节点。
+        // 以左节点视角考虑原先的根节点、左节点、右节点这三者的关系。
+        TreeNode *curr = root, *parent = NULL, *sibling = NULL;
+        while (curr) {
+            auto left = curr->left;
+
+            curr->left = sibling;
+            sibling = curr->right;
+            curr->right = parent;
+            parent = curr;
+            
+            curr = left;
+        }
+        return parent;
+    }
+};
+*/
 
 int main(int argc, const char * argv[]) {
     return 0;
