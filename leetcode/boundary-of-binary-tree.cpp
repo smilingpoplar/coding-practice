1a9b7f80d9ba1e75bbd31d752bef1516a7e1f68d
smilingpoplar <smilingpoplar@gmail.com>
2018-09-12 00:45:51 +0800

    update

--- leetcode/boundary-of-binary-tree.cpp
+++ leetcode/boundary-of-binary-tree.cpp
@@ -22,9 +22,47 @@ using namespace std;
  */
 class Solution {
 public:
+    vector<int> boundaryOfBinaryTree(TreeNode *root) {
+        // 输出根节点、左子树的非叶左边界、叶节点、右子树的非叶右边界
+        if (!root) return {};
+        vector<int> ans;
+        ans.push_back(root->val);
+        leftBoundary(root->left, ans);
+        leaves(root, ans);
+        rightBoundary(root->right, ans);
+        return ans;
+    }
+    
+    void leftBoundary(TreeNode *root, vector<int> &ans) {
+        if (!root || (!root->left && !root->right)) return;
+        ans.push_back(root->val);
+        if (root->left) leftBoundary(root->left, ans);
+        else leftBoundary(root->right, ans);
+    }
+    
+    void leaves(TreeNode *root, vector<int> &ans) {
+        if (!root) return;
+        if (!root->left && !root->right) {
+            ans.push_back(root->val);
+        } else {
+           leaves(root->left, ans);
+           leaves(root->right, ans);
+        }        
+    }
+    
+    void rightBoundary(TreeNode *root, vector<int> &ans) {
+        if (!root || (!root->left && !root->right)) return;
+        if (root->right) rightBoundary(root->right, ans);
+        else rightBoundary(root->left, ans);
+        ans.push_back(root->val);
+    }
+};
+
+/*
+class Solution {
+public:
     vector<int> boundaryOfBinaryTree(TreeNode* root) {
-        // 这题的意思是：输出先序遍历过程中的左边界、叶节点、右边界
-        // 为防止重复，输出：根节点、左子树中非叶节点的左边界和叶节点、右子树中叶节点和非叶节点的右边界
+        // 输出根节点、左子树中非叶左边界和叶节点、右子树中叶节点和非叶右边界
         if (!root) return {};
         vector<int> ans;
         ans.push_back(root->val);
@@ -33,22 +71,25 @@ public:
         return ans;
     }
     
-    void traverse(TreeNode *root, bool isLeftBoundary, bool isRightBoundary, vector<int> &ans) {
+    void traverse(TreeNode *root, bool leftbd, bool rightbd, vector<int> &ans) {
         if (!root) return;
         if (!root->left && !root->right) {
             ans.push_back(root->val);
             return;
         }
-        if (isLeftBoundary) ans.push_back(root->val); // 先序
-        // 1. root是左边界且左子树存在，则root->left是左边界；
-        // 2. root是左边界但左子树不存在，则root->right是左边界；
-        // 3. root是右边界且右子树存在，则root->right是右边界；
-        // 4. root是右边界但右子树不存在，则root->left是右边界。
-        traverse(root->left, isLeftBoundary && root->left, isRightBoundary && !root->right, ans);  // root->left相关：1、4
-        traverse(root->right, isLeftBoundary && !root->left, isRightBoundary && root->right, ans); // root->right相关：2、3
-        if (isRightBoundary) ans.push_back(root->val); // 后序
+
+        if (leftbd) ans.push_back(root->val); // 先序
+        // 1. root是左边界、左子树存在   => root->left是左边界
+        // 2.       左边界、左子树不存在 => root->right是左边界
+        // 3.       右边界、右子树存在   => root->right是右边界
+        // 4.       右边界、右子树不存在 => root->left是右边界
+        // root->left相关：1、4，root->right相关：2、3
+        traverse(root->left, leftbd && root->left, rightbd && !root->right, ans);
+        traverse(root->right, leftbd && !root->left, rightbd && root->right, ans);
+        if (rightbd) ans.push_back(root->val); // 后序
     }
 };
+*/
 
 int main(int argc, const char * argv[]) {
     return 0;
