fb3d92dc32e459206ac6b02d4713ad7df9c9987c
smilingpoplar <smilingpoplar@gmail.com>
2018-09-11 22:05:39 +0800

    update

--- leetcode/validate-binary-search-tree.cpp
+++ leetcode/validate-binary-search-tree.cpp
@@ -18,44 +18,37 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/* 用long类型来躲避烦人的INT_MIN,INT_MAX边界测试，有些tricky
+/*
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
         return isValidBST(root, LONG_MIN, LONG_MAX);
     }
-private:
-    // bst的所有节点 (min,max)
-    bool isValidBST(TreeNode *root, long min, long max) {
+
+    bool isValidBST(TreeNode *root, long lower, long upper) {
         if (!root) return true;
-        return min < root->val && root->val < max
-        && isValidBST(root->left, min, root->val)
-        && isValidBST(root->right, root->val, max);
+        if (root->val <= lower || root->val >= upper) return false;
+        return isValidBST(root->left, lower, root->val) && isValidBST(root->right, root->val, upper);
     }
 };
 */
 
-// 更一般地，用中序遍历法，没有INT_MIN,INT_MAX边界值的问题，bst的中序遍历应是个单调递增序列
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        // 中序遍历，用current表示调用栈[current,stack]的栈顶
-        auto current = root;
-        vector<TreeNode *> stack;
+        // 确保bst的中序遍历单调递增
+        auto curr = root;
+        stack<TreeNode *> stk;
         TreeNode *prev = NULL;
-        while (current || !stack.empty()) {
-            if (current) {
-                stack.push_back(current);
-                current = current->left;
-            } else {
-                current = stack.back();
-                stack.pop_back();
-                
-                if (prev && current->val <= prev->val) return false;
-                prev = current;
-                
-                current = current->right;
-            }
+        while (curr || !stk.empty()) {
+            while (curr) {
+                stk.push(curr);
+                curr = curr->left;
+            } 
+            auto node = stk.top(); stk.pop();                
+            if (prev && prev->val >= node->val) return false;
+            prev = node;
+            curr = node->right;
         }
         return true;
     }
