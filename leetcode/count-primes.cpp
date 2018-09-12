efa79bc8a7e277bd91dc1f5d6d4c58940992768c
smilingpoplar <smilingpoplar@gmail.com>
2018-09-13 01:40:36 +0800

    update

--- leetcode/count-primes.cpp
+++ leetcode/count-primes.cpp
@@ -14,18 +14,18 @@ using namespace std;
 class Solution {
 public:
     int countPrimes(int n) {
-        // 筛法
-        vector<bool> isPrime(n, true);
+        // 素数筛法
+        vector<bool> prime(n, true);
         for (int i = 2; i * i < n; i++) {
-            if (!isPrime[i]) continue;
+            if (!prime[i]) continue;
             for (int j = i * i; j < n; j += i) {
-                isPrime[j] = false;
+                prime[j] = false;
             }
         }
         
         int count = 0;
         for (int i = 2; i < n; i++) {
-            if (isPrime[i]) ++count;
+            if (prime[i]) count++;
         }
         return count;
     }
