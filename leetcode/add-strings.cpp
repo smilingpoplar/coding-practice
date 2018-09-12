417e34044afebf21ad686ab006da995968f650b4
smilingpoplar <smilingpoplar@gmail.com>
2018-09-13 00:03:26 +0800

    update

--- leetcode/add-strings.cpp
+++ leetcode/add-strings.cpp
@@ -15,7 +15,7 @@ class Solution {
 public:
     string addStrings(string num1, string num2) {
         string ans;
-        int i = num1.size() - 1, j = num2.size() - 1;
+        int i = (int)num1.size() - 1, j = (int)num2.size() - 1;
         int carry = 0;
         while (i >= 0 || j >= 0 || carry > 0) {
             if (i >= 0) carry += num1[i--] - '0';
