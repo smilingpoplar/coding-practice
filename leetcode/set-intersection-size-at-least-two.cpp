5abfad07309915f3866855c5fd289749ec0acec2
smilingpoplar <smilingpoplar@gmail.com>
2018-09-16 00:01:13 +0800

    update

--- leetcode/set-intersection-size-at-least-two.cpp
+++ leetcode/set-intersection-size-at-least-two.cpp
@@ -18,14 +18,18 @@ public:
             if (a[1] == b[1]) return a[0] > b[0];
             return a[1] < b[1];
         });
+        
         int ans = 0;
-        int last2 = INT_MIN, last1 = INT_MIN; // 要创建的集合S的最后两个数
+        // 要创建的集合S的最后两个数。区间按end排序，可保证这两数递增，按start排序就不行。
+        int last2 = INT_MIN, last1 = INT_MIN;
         for (auto &interval : intervals) {
-            if (last1 < interval[0]) { // 当前区间与S不相交，往S中加入当前区间的最后两个数
+            if (last1 < interval[0]) {
+                // 与S不相交，往S中加入当前区间的最后两个数
                 ans += 2;
                 last1 = interval[1];
                 last2 = last1 - 1;
-            } else if (last2 < interval[0] && interval[0] <= last1) { // 往S中加入当前区间的最后一个数
+            } else if (last2 < interval[0] && interval[0] <= last1) {
+                // 有一个数与S相交，往S中加入当前区间的最后一个数
                 ans += 1;
                 last2 = last1;
                 last1 = interval[1];
