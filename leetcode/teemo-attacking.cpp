a88d50498185aa2bf22e27d0e2f17d734b18db97
smilingpoplar <smilingpoplar@gmail.com>
2018-09-15 22:14:32 +0800

    update

--- leetcode/teemo-attacking.cpp
+++ leetcode/teemo-attacking.cpp
@@ -13,17 +13,12 @@ using namespace std;
 class Solution {
 public:
     int findPoisonedDuration(vector<int>& timeSeries, int duration) {
-        // 前闭后开区间 [time, time+duration)
+        if (timeSeries.empty()) return 0;
         int ans = 0;
-        int start = 0, end = 0;
-        for (int time : timeSeries) {
-            if (time >= end) { // 新区间
-                ans += end - start;
-                start = time;
-            }
-            end = time + duration;
+        for (int i = 1; i < timeSeries.size(); i++) {
+            ans += min(duration, timeSeries[i] - timeSeries[i-1]);
         }
-        ans += end - start;
+        ans += duration;
         return ans;
     }
 };
