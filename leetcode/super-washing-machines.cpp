b47a268ccf61cfbc004d71cf8eee26a054063cd1
smilingpoplar <smilingpoplar@gmail.com>
2018-09-23 23:40:30 +0800

    update

--- leetcode/super-washing-machines.cpp
+++ leetcode/super-washing-machines.cpp
@@ -20,9 +20,9 @@ public:
         if (sum % N != 0) return -1;
 
         int avg = sum / N;
-        // 想象洗衣机是节点，一次move就是连接一条粗细为1的管道
+        // 想象洗衣机是节点，一次move就是连接一条1单位粗的管道
         // 各机器除了要自己能发出流量：out[i] = machines[i]-avg
-        // 还要保证通过它流向一侧的流量：abs(sum(out[0..i]))，sum为正时向右流、为负时向左流
+        // 还要保证从一侧（含自己）通过它的流量：abs(sum(out[0..i]))，sum为正时向右流、为负时向左流
         int ans = 0;
         int toRight = 0;
         for (int i = 0; i < N; i++) {
