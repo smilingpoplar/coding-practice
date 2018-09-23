df40ba941f0d69de7db485b2969c1bf659a238f2
smilingpoplar <smilingpoplar@gmail.com>
2018-09-23 18:35:00 +0800

    update

--- leetcode/chalkboard-xor-game.cpp
+++ leetcode/chalkboard-xor-game.cpp
@@ -16,7 +16,7 @@ public:
     bool xorGame(vector<int>& nums) {
         int x = 0;
         for (int num : nums) x ^= num;
-        // 如果xor!=0，Alice要想赢只能指望Bob拿最后一个，即数有偶数个
+        // 如果xor!=0，Alice要想赢只能指望Bob拿最后一个（即数有偶数个）
         return x == 0 || nums.size() % 2 == 0;
     }
 };
