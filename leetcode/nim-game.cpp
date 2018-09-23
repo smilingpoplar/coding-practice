748f6f5d6984237a1f6454490030c4b917e365d4
smilingpoplar <smilingpoplar@gmail.com>
2018-09-23 17:43:47 +0800

    update

---
+++ leetcode/nim-game.cpp
@@ -0,0 +1,23 @@
+//
+//  bulb-switcher
+//  https://leetcode.com/problems/bulb-switcher/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n % 4 != 0;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
--- leetcode/swap-adjacent-in-lr-string.cpp
+++ leetcode/swap-adjacent-in-lr-string.cpp
@@ -15,8 +15,8 @@ class Solution {
 public:
     bool canTransform(string start, string end) {
         // 把L看作面朝左的人、R看作面朝右的人、X看作空位，那么L朝左走、R朝右走、且不能越过别人
-        // 比较start串和end串除了X外的LR序列是否相同，
-        // 且end的相应L要在start的L的左边（朝左走）、end的相应R要在start的R的右边（朝右走）
+        // 1. 比较start串和end串除了X外的LR序列是否相同
+        // 2. end串的相应L要在start串的L的左边（朝左走）、end串的相应R要在start串的R的右边（朝右走）
         const int M = start.size(), N = end.size();
         int i = 0, j = 0;
         while (true) {
