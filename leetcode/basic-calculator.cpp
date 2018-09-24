2db65b087c453827cfb4bbdb6903ac56e2d608e6
smilingpoplar <smilingpoplar@gmail.com>
2018-09-24 17:50:19 +0800

    update

--- leetcode/basic-calculator.cpp
+++ leetcode/basic-calculator.cpp
@@ -16,8 +16,9 @@ class Solution {
 public:
     int calculate(string &s) {
         // 中缀转后缀，用nums栈和ops栈
-        // num、左括号'('先入栈，遇到右括号')'时不断弹出计算直到弹出'('，
-        // 遇到下一个优先级<=栈顶的op时、先弹出计算再把op入栈，左括号'('的优先级最小。
+        // 遇到下一个优先级<=栈顶的op时、先弹出计算再把op入栈。
+        // 左括号'('先入栈，等遇到右括号')'时不断弹出计算直到'('；
+        // 左括号只能遇右括号时弹出，为防止被其他op计算误弹出，'('的优先级设为最小。
         unordered_map<char, int> priority = {
             {'+', 1}, {'-', 1},
             {'(', 0},
@@ -28,22 +29,25 @@ public:
         for (int i = 0; i < s.size(); i++) {
             char c = s[i];
             if (c == ' ') continue;
-            if (c == '(') {
-                ops.push(c);
-            } else if (isdigit(c)) {
+            if (isdigit(c)) {
                 int num = c - '0';
                 while (i + 1 < s.size() && isdigit(s[i+1]))
                     num = num * 10 + s[++i] - '0';
                 nums.push(num);
+            } else if (c == '(') {
+                ops.push(c);
             } else if (c == ')') {
-                while (ops.top() != '(') calc(nums, ops);
+                while (ops.top() != '(') 
+                    calc(nums, ops);
                 ops.pop(); // 弹出'('
             } else { // +-*/
-                while (!ops.empty() && priority[c] <= priority[ops.top()]) calc(nums, ops);
+                while (!ops.empty() && priority[c] <= priority[ops.top()])
+                    calc(nums, ops);
                 ops.push(c);
             }
         }
-        while (!ops.empty()) calc(nums, ops);
+        while (!ops.empty())
+            calc(nums, ops);
         
         return nums.top();
     }
