a9ccf976e7bcd49692cc3ece16ba7441ce82b71d
smilingpoplar <smilingpoplar@gmail.com>
2018-09-26 19:30:13 +0800

    update

---
+++ leetcode/smallest-range-i.cpp
@@ -0,0 +1,28 @@
+//
+//  smallest-range-i
+//  https://leetcode.com/problems/smallest-range-i/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int smallestRangeI(vector<int>& A, int K) {
+        int theMin = A[0], theMax = A[0];
+        for (int a : A) {
+            theMin = min(theMin, a);
+            theMax = max(theMax, a);
+        }
+        return max(0, theMax - theMin - 2 * K);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
