aa052263fad8c00542d06e6034730bb4f5dd5d8f
smilingpoplar <smilingpoplar@gmail.com>
2018-09-27 00:31:00 +0800

    update

--- leetcode/number-of-digit-one.cpp
+++ leetcode/number-of-digit-one.cpp
@@ -13,28 +13,29 @@ using namespace std;
 class Solution {
 public:
     int countDigitOne(int n) {
-        // 看[0,n]的数中有多少个在某一位为1，要看这一位是 <1、>1、=1：
-        //   当前位<1 => 左边数字*当前位基数，如2025的百位（100、...、199、1100、...、1199）=> 2*100
-        //   当前位>1 => (左边数字+1)*当前位基数，如2225的百位（100、...、199、1100、...、1199、2100、...、2199）=> (2+1)*100
-        //   当前位=1 => 左边数字*当前位基数+低位数字+1，如2125的百位（100、...、199、1100、...、1199、2100、...、2125）=> 2*100+26
+        // 从低位到高位一位位地看，某一位有多少个'1'，要看该位是 <1、>1、=1？
+        // 需要用到左边（不含该位）构成的数higher、右边（不含该位）构成的数lower、该位代表的数base。
+        //   该位<1 => higher*base
+        //      如2025的百位（100、...、199、1100、...、1199）=> 2*100
+        //   该位>1 => (higher+1)*base
+        //      如2225的百位（100、...、199、1100、...、1199、2100、...、2199）=> (2+1)*100
+        //   该位=1 => higher*base+(lower+1)
+        //      如2125的百位（100、...、199、1100、...、1199、2100、...、2125）=> 2*100+26
         if (n <= 0) return 0;
-        int count = 0;
-        int base = 1;
-        while (true) {
-            int lower = n % base;
-            int current = (n / base) % 10;
-            int higher = n / base / 10;
-            if (current < 1) {
-                count += higher * base;
-            } else if (current > 1) {
-                count += (higher + 1) * base;
+        int ans = 0;
+        long base = 1;
+        while (base <= n) {
+            int lower = n % base, curr = (n / base) % 10, higher = (n / base) / 10;
+            if (curr < 1) {
+                ans += higher * base;
+            } else if (curr > 1) {
+                ans += (higher + 1) * base;
             } else {
-                count += higher * base + lower + 1;
+                ans += higher * base + (lower + 1);
             }
-            if (base > n / 10) break; // 防止溢出，从while(base<=n)改到这儿
             base *= 10;
         };
-        return count;
+        return ans;
     }
 };
 
