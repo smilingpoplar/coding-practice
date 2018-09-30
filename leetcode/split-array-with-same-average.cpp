04026497575dda6c02448326e206f0708db39dbb
smilingpoplar <smilingpoplar@gmail.com>
2018-09-30 23:40:52 +0800

    update

--- leetcode/split-array-with-same-average.cpp
+++ leetcode/split-array-with-same-average.cpp
@@ -14,7 +14,7 @@ using namespace std;
 class Solution {
 public:
     bool splitArraySameAverage(vector<int>& A) {
-        // 把A分成B和C，不妨设B是较小的那个（1<=k<=N/2）。B和C的平均数相等，等于整体A的平均数。
+        // 把A分成B和C，不妨设B是较小的那个（1<=k<=N/2）。B和C的均值相等，等于A的整体均值。
         // avgB==avgA, sumB/k==sum/N, sumB==k*sum/N。需要sumB是整数，有(k*sum)%N==0
         // 转变成子集和问题：从A中找k个数，k满足(k*sum)%N==0，且k个数的子集和为k*sum/N
 
@@ -23,22 +23,37 @@ public:
         // 考虑A[i-1]，dp[i][k][v] = dp[i-1][k][v]/*不取A[i-1]*/ || dp[i-1][k-1][v-A[i-1]] /*取A[i-1]*/
         // 递推式在i这维只依赖于i-1项，可省掉i这一维。
         // 01背包问题，逆序遍历k和v：dp[k][v] = dp[k][v] || dp[k-1][v-A[i-1]]
-        const int N = A.size();
+        // 
+        // 这里A[i-1]在范围[0,10000]，和值sum较大，用二维数组dp[k][v]浪费空间，
+        // 改用`vector<unordered_set<int>> dp;`，dp[k]表示从A[0..i-1]中取k个数、能达到的和值集
+        // dp[k].insert(v)表示dp[k][v]==true
+        
         int sum = 0;
-        for (int num : A) sum += num;
-        vector<vector<bool>> dp(N / 2 + 1, vector<bool>(sum + 1, false));
-        dp[0][0] = true;
+        for (int num : A) {
+            sum += num;
+        }
+
+        // 提早判断返回
+        const int N = A.size();
+        bool canSplit = false;
+        for (int k = 1; k <= N / 2 && !canSplit; k++) {
+            if (k * sum % N == 0) canSplit = true;
+        }
+        if (!canSplit) return false;
+        
+        vector<unordered_set<int>> dp(N / 2 + 1);
+        dp[0].insert(0);
         for (int i = 1; i <= N; i++) {
-            // 01背包，逆序遍历k和v
-            for (int k = min(N/2, i); k >= 1; k--) {
-                for (int v = sum; v >= A[i-1]; v--) {
-                    dp[k][v] = dp[k][v] || dp[k-1][v - A[i-1]];
+            // 01背包，逆序遍历k
+            for (int k = min(N / 2, i); k >= 1; k--) {
+                for (int s : dp[k-1]) {
+                    dp[k].insert(s + A[i-1]);
                 }
             }
         }
         
         for (int k = 1; k <= N / 2; k++) {
-            if (k * sum % N == 0 && dp[k][k * sum / N]) return true;
+            if (k * sum % N == 0 && dp[k].count(k * sum / N)) return true;
         }
         return false;
     }
