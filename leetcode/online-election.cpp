f3e33ce33370f4f913c927196401a809c6a781a9
smilingpoplar <smilingpoplar@gmail.com>
2018-09-30 15:32:56 +0800

    update

---
+++ leetcode/online-election.cpp
@@ -0,0 +1,39 @@
+//
+//  online-election
+//  https://leetcode.com/problems/online-election/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class TopVotedCandidate {
+    vector<int> times, winners;
+public:
+    TopVotedCandidate(vector<int> persons, vector<int> times) : times(times) {
+        const int N = times.size();
+        vector<int> votes(N, 0);
+        int winner = 0;
+        for (int person : persons) {
+            votes[person]++;
+            if (votes[person] >= votes[winner]) {
+                winner = person;
+            }
+            winners.push_back(winner);
+        }
+    }
+    
+    int q(int t) {
+        // 找到<=t的时刻
+        int idx = --upper_bound(times.begin(), times.end(), t) - times.begin();
+        return winners[idx];
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
