2fe751f4d9a84f8d0c8d00335807ee4c507e1375
smilingpoplar <smilingpoplar@gmail.com>
2018-10-24 13:24:13 +0800

    update

--- leetcode/consecutive-numbers-sum.cpp
+++ leetcode/consecutive-numbers-sum.cpp
@@ -14,8 +14,8 @@ using namespace std;
 class Solution {
 public:
     int consecutiveNumbersSum(int N) {
-        // 假设N可分成k个连续数之和：N=x+(x+1)+(x+2)+...+(x+k-1)=k*x+k*(k-1)/2，
-        // k*x = N-k*(k-1)/2。x是>=1的整数 => (N-k*(k-1)/2)%k==0 且 k(k+1)<=2*N。
+        // 假设N可分成k个连续数之和：N=x+(x+1)+(x+2)+...+(x+k-1)=kx+k(k-1)/2，kx=N-k(k-1)/2。
+        // x是>=1的整数 => 1. (N-k(k-1)/2)%k==0  2. N-k(k-1)/2>=k，k(k+1)<=2N
         int ans = 0;
         for (int k = 1; k * (k + 1) <= 2 * N; k++) {
             if ((N - k * (k - 1) / 2) % k == 0) ans++;
