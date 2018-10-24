a7d8946e01ed08f479bffa9cff0e4e7b1573e2ff
smilingpoplar <smilingpoplar@gmail.com>
2018-10-24 17:28:43 +0800

    update

--- leetcode/numbers-at-most-n-given-digit-set.cpp
+++ leetcode/numbers-at-most-n-given-digit-set.cpp
@@ -18,7 +18,7 @@ public:
         // 1. N是n位数，<n位的数有d^1+d^2+...+d^(n-1)个
         // 2. =n位且<N的数先看第一位a[0]，有(D中<a[0]的个数)*d^(n-1)+(若可取到a[0]，再看第二位...)
         //        = lessCnt(a[0])*d^(n-1)+lessCnt(a[1])*d^(n-2)+...lessCnt(a[n-1])*d^0
-        // 3. 若可取到N，再加上1
+        // 3. 若可取到N本身，再加上1
         const auto NS = to_string(N);
         const int d = D.size(), n = NS.size();
         // case 1
@@ -29,11 +29,11 @@ public:
         }
 
         // case 2
-        vector<bool> containsDigit(10, false);
+        vector<bool> hasDigit(10, false);
         vector<int> lessCnt(10, 0);
         for (auto &digitS: D) {
             int digit = digitS[0] - '0';
-            containsDigit[digit] = true;
+            hasDigit[digit] = true;
             for (int i = digit + 1; i < 10; i++) 
               lessCnt[i]++;
         }
@@ -43,7 +43,7 @@ public:
             int digit = NS[idx] - '0';
             ans += lessCnt[digit] * base;
             base /= d;
-            if (!containsDigit[digit]) break;
+            if (!hasDigit[digit]) break;
         }
         // case 3
         if (idx == n) ans++;
