b3dbd3a6cafa6e5bc4747ae7bdb82819793a42eb
smilingpoplar <smilingpoplar@gmail.com>
2018-10-24 12:10:53 +0800

    update

--- leetcode/max-points-on-a-line.cpp
+++ leetcode/max-points-on-a-line.cpp
@@ -25,38 +25,38 @@ public:
     int maxPoints(vector<Point>& points) {
         const int N = points.size();
         int ans = 0;
-        unordered_set<int> dups; // 重复出现的点
-        // 每个点跟它后面的点比较，看相同斜率的有多少，斜率用最简分数表示
+        unordered_set<int> dups; // 已见过的重复点
+        // 每个点跟它后面的点比较，看相同斜率的有多少个，斜率用最简分数表示
         for (int i = 0; i < N; i++) {
             if (dups.count(i)) continue;
             
-            int sameICnt = 1; // 与第i点相同的多少个
-            map<pair<int, int>, int> slopeICnt; // 过i点的那些斜率
+            int sameICnt = 1; // 与点i斜率相同的多少个
+            map<pair<int, int>, int> slopeICnt; // 过点i的那些斜率
             int maxSlopeICnt = 0; // 过i点的同斜率直线最多有几条
             for (int j = i + 1; j < N; j++) {
                 auto &a = points[i], &b = points[j];
                 if (a.x == b.x && a.y == b.y) {
-                    dups.insert(j); // 记录重复的点，后面循环不再处理
-                    ++sameICnt;
+                    dups.insert(j); // 记录重复点
+                    sameICnt++;
                     continue;
                 }
                 pair<int, int> slope = {0, 0}; // 不妨用{0,0}记垂直线的无效斜率
                 if (a.x != b.x) {
                     int diffX = a.x - b.x, diffY = a.y - b.y;
-                    int g = getGcd(diffX, diffY);
+                    int g = gcd(diffX, diffY);
                     diffX /= g, diffY /= g;
                     slope = {diffX, diffY};
                 }
                 slopeICnt[slope]++;
                 maxSlopeICnt = max(maxSlopeICnt, slopeICnt[slope]);
             }
-            // 过i点同斜率的直线几条、就是除i外同直线的点有几个，再加上i点的个数，就是与i有关的同直线的点有几个
+            // 与点i同线的最多点 = 除i外同斜率的最多点 + 与i重合的点
             ans = max(ans, maxSlopeICnt + sameICnt);
         }
         return ans;
     }
     
-    int getGcd(int a, int b) {
+    int gcd(int a, int b) {
         while (b) {
             int tmp = a % b;
             a = b;
