5659f34b1732c767e5897f29572621506cb9131c
smilingpoplar <smilingpoplar@gmail.com>
2018-10-24 13:07:36 +0800

    update

--- leetcode/reaching-points.cpp
+++ leetcode/reaching-points.cpp
@@ -14,15 +14,14 @@ using namespace std;
 class Solution {
 public:
     bool reachingPoints(int sx, int sy, int tx, int ty) {
-        // 反向考虑，tx,ty大的那个一定是和，辗转相减得小的那个
+        // 反向考虑，tx,ty大的那个一定是和，
+        // 辗转相减（辗转相除）看能否收敛到(sx,sy+k*sx)或(sx+k*sy,sy)
         while (tx > sx && ty > sy) {
             if (tx > ty) tx %= ty;
             else ty %= tx;
         }
-        // tx==sx，ty只能每次再减小tx
-        if (tx == sx && (ty - sy) % tx == 0) return true;
-        // ty==sy，tx只能每次再减小ty
-        if (ty == sy && (tx - sx) % ty == 0) return true;
+        if (tx == sx && (ty - sy) % sx == 0) return true;
+        if (ty == sy && (tx - sx) % sy == 0) return true;
         return false;
     }
 };
