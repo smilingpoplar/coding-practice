2407acc85e067bc4a37fe1b276f4eec274f86cdf
smilingpoplar <smilingpoplar@gmail.com>
2018-10-25 16:12:06 +0800

    update

---
+++ leetcode/flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,34 @@
+//
+//  flip-string-to-monotone-increasing
+//  https://leetcode.com/problems/flip-string-to-monotone-increasing/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int minFlipsMonoIncr(string S) {
+        // 设子问题S[0..i]以'0'结尾时minFlips为dp[i][0]，以'1'结尾时minFlips为dp[i][1]
+        //  dp[i][0] = dp[i-1][0] + (S[i]=='1')
+        //  dp[i][1] = min(dp[i-1][0], dp[i-1][1]) + (S[i]=='0')
+        // 递推式在i这维上只依赖i-1项，省掉i这维：
+        //  dp[1] = min(dp[0], dp[1]) + (S[i]=='0')
+        //  dp[0] = dp[0] + (S[i]=='1')  // dp[0]覆盖放后面
+        int f0 = 0, f1 = 0;
+        for (char c : S) {
+            f1 = min(f0, f1) + (c == '0');
+            f0 += (c == '1');
+        }
+        return min(f0, f1);
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ leetcode/sort-array-by-parity-ii.cpp
@@ -0,0 +1,29 @@
+//
+//  sort-array-by-parity-ii
+//  https://leetcode.com/problems/sort-array-by-parity-ii/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& A) {
+        const int N = A.size();
+        for (int i = 0, j = 1; i < N; i += 2, j += 2) {
+            while (i < N && A[i] % 2 == 0) i += 2;
+            while (j < N && A[j] % 2 == 1) j += 2;
+            if (i < N) swap(A[i], A[j]);
+        }
+        return A;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
