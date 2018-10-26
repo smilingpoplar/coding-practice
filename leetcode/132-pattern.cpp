086501b8133df136888f2734a270be399673d8ed
smilingpoplar <smilingpoplar@gmail.com>
2018-10-26 12:26:07 +0800

    update

--- leetcode/132-pattern.cpp
+++ leetcode/132-pattern.cpp
@@ -15,15 +15,19 @@ using namespace std;
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
+        // 132模式：按顺序的三个数a1、a2、a3，第一数最小、第二数最大，a1<a3<a2。
+        // 固定第二个数a2，看它左边的最小数、右边小于它的最大数能否满足条件。
         const int N = nums.size();
         if (N < 3) return false;
-        // 当前数左边找最小数
+        // 先计算某数（含）左边的最小数
         vector<int> leftMin(N);
         leftMin[0] = nums[0];
         for (int i = 1; i < N; i++) {
             leftMin[i] = min(nums[i], leftMin[i-1]);
         }
-        // 当前数右边找小于它的最大数，相当于：从右往左找下一个更大的数
+        // 某数右边小于它的最大数 =转为=> 从右往左找下一个更大的数
+        // 不是等价命题，但这题可转。
+        // 因为递减栈刚开始的数最大，如果小于它的最大数都不满足条件，其他更不行。
         stack<int> stk;
         for (int j = N - 1; j >= 1; j--) {
             int rMax = INT_MIN;
@@ -31,7 +35,7 @@ public:
                 rMax = stk.top();
                 stk.pop();
             }
-            stk.push(nums[j]); // nums[j]是rMax左边的下一个更大的数、rMax是nums[j]右边小于它的最大数
+            stk.push(nums[j]);
             
             if (leftMin[j-1] < rMax) return true;
         }
