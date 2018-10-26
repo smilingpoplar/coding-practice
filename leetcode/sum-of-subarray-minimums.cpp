78a6e1a843e1a877bdead17d28da8959cac8a31a
smilingpoplar <smilingpoplar@gmail.com>
2018-10-26 11:10:23 +0800

    update

--- leetcode/sum-of-subarray-minimums.cpp
+++ leetcode/sum-of-subarray-minimums.cpp
@@ -14,21 +14,21 @@ using namespace std;
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& A) {
-        // 求各个连续子段最小值的和。
-        // 什么时候A[i2]是子段的最小值？子段可往左扩展到下一个<A[i2]的位置i1（不含i1），
-        // 往右扩展到下一个<A[i2]的位置i3（不含i3），则A[i2]作最小值的子段有(i2-i1)*(i3-i2)个
-        // 找下一个<A[i2]的位置 <=> 用栈找波峰，波峰的两侧就是下一个<波峰的位置
+        // 求所有子段最小值的和。那A[i]什么时候是子段的最小值？
+        // 从位置i往左扩展到下一个<A[i]的位置i1，往右扩展到下一个<A[i]的位置k，
+        // 以A[i]作最小值的子段往左有(i-i1)个、往右有(i2-i)个，总共有(i-i1)*(i2-i)个
+        // 找下一个更小的数 <=> 用栈找波峰，波峰的两侧就是下一个<波峰的数
         const int MOD = 1e9 + 7;
         int ans = 0;
         stack<int> stk; // 保存idx，递增栈
         A.push_back(0); // 右哨兵
-        for (int i3 = 0; i3 < A.size(); i3++) {
-            while (!stk.empty() && A[i3] < A[stk.top()]) {
-                int i2 = stk.top(); stk.pop();
+        for (int i2 = 0; i2 < A.size(); i2++) {
+            while (!stk.empty() && A[i2] < A[stk.top()]) {
+                int i = stk.top(); stk.pop();
                 int i1 = stk.empty() ? -1 : stk.top();
-                ans = (ans + A[i2] * (i2 - i1) * (i3 - i2)) % MOD;
+                ans = (ans + A[i] * (i - i1) * (i2 - i)) % MOD;
             }
-            stk.push(i3);
+            stk.push(i2);
         }
         return ans;
     }
