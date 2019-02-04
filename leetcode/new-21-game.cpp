af999f308b490b8cc413a00372ef69ee5d07202e
smilingpoplar <smilingpoplar@gmail.com>
2019-02-04 14:52:07 +0800

    update

--- leetcode/new-21-game.cpp
+++ leetcode/new-21-game.cpp
@@ -13,21 +13,25 @@ using namespace std;
 
 class Solution {
 public:
-    // 求得分在[K,N]间的概率
+    // <K时不断得[1..W]分，求最后得分在[K..N]间的概率
     double new21Game(int N, int K, int W) {
-        if (K == 0 || K + W - 1 <= N) return 1;
-        // 设dp[i]表示得分i的概率，dp[i]= sum(dp[i-W..i-1])/W；
-        // 可是dp[K..i-1]已是终止态，不能再从它们跳到dp[i]；
+        if (K == 0 || K - 1 + W <= N) return 1;
+        // 设dp[i]表示得分为i的概率，dp[i]= sum(dp[i-W..i-1])/W；
+        // 但dp[K..i-1]已是终止态，不能再从它们跳到dp[i]，
         // 所以 dp[i] = sum(dp[i-W .. min(K-1,i-1)]) / W
         // 初始dp[0]=1
+        // 
+        // 记范围和sum(dp[i-W .. min(K-1,i-1)]) = wSum
+        // i增大时，维护滑动窗口wSum，扩展右边收缩左边
         vector<double> dp(N + 1, 0);
         dp[0] = 1;
-        double sum = dp[0], ans = 0;
+        double wSum = dp[0], ans = 0;
         for (int i = 1; i <= N; i++) {
-            dp[i] = sum / W;
-            if (i < K) sum += dp[i]; // min(K-1,i-1)=i-1
-            else ans += dp[i]; // i>=K，恰好放else语句
-            if (i >= W) sum -= dp[i-W];
+            dp[i] = wSum / W;
+            // 维护wSum
+            if (i < K) wSum += dp[i]; // min(K-1,i-1)=i-1，扩展窗口右边
+            else ans += dp[i]; // i>=K，不用扩展窗口右边；统计[K..N]间的概率
+            if (i >= W) wSum -= dp[i-W];
         }
         return ans;
     }
