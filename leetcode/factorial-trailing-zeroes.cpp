72953faec67031d56ba7aa0551841361fdf47423
smilingpoplar <smilingpoplar@gmail.com>
2019-02-19 13:17:05 +0800

    update

--- leetcode/factorial-trailing-zeroes.cpp
+++ leetcode/factorial-trailing-zeroes.cpp
@@ -13,7 +13,7 @@ using namespace std;
 class Solution {
 public:
     int trailingZeroes(int n) {
-        // n!末尾0的个数 等于 [1,n]中因子5的个数
+        // n!末尾0的个数 等于 [1..n]中因子5的个数
         // 即 n/5 + n/25 + n/125 + ...
         int ans = 0;
         while (n /= 5) {
