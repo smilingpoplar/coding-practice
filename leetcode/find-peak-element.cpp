5f4c1d558e445725aaa58bdf7aef29e03c7099c4
smilingpoplar <smilingpoplar@gmail.com>
2019-02-19 14:36:50 +0800

    update

--- leetcode/find-peak-element.cpp
+++ leetcode/find-peak-element.cpp
@@ -25,14 +25,14 @@ public:
 };
 */
 
-/*
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         // 找波峰，对应找下一个更小的数，找nums[i]>nums[i+1]的位置i
+        // 因为nums[i]要跟nums[i+1]比较，i二分搜索的范围是[0..N-2]
         const int N = nums.size();
-        int l = 0, u = N - 1;
-        while (l + 1 < u) { // N>=3的情况
+        int l = -1, u = N - 1;
+        while (l + 1 < u) {
             int mid = l + (u - l) / 2;
             if (nums[mid] > nums[mid+1]) {
                 u = mid;
@@ -40,17 +40,28 @@ public:
                 l = mid;
             }
         }
-        // N==1或2的情况
-        return nums[l] > nums[u] ? l : u;
+        return u;
     }
-};
-*/
 
-class Solution {
-public:
+    // 三种写法都可以
+    /*
     int findPeakElement(vector<int>& nums) {
-        // 找波峰，对应找下一个更小的数，找nums[i]>nums[i+1]的位置i
-        // nums[i]要跟nums[i+1]比较，i二分搜索范围是[0..N-2]
+        const int N = nums.size();
+        int l = 0, u = N - 2;
+        while (l <= u) {
+            int mid = l + (u - l) / 2;
+            if (nums[mid] > nums[mid+1]) {
+                u = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+    */
+
+    /*
+    int findPeakElement(vector<int>& nums) {
         const int N = nums.size();
         int l = 0, u = N - 1; // 前闭后开，l相关的要改
         while (l < u) {
@@ -63,6 +74,7 @@ public:
         }
         return u;
     }
+    */
 };
 
 int main(int argc, const char * argv[]) {
