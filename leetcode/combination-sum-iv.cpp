add71532e24ecce479e5a9e1fe8104df4a292e7e
smilingpoplar <smilingpoplar@gmail.com>
2019-02-20 18:39:06 +0800

    update

--- leetcode/combination-sum-iv.cpp
+++ leetcode/combination-sum-iv.cpp
@@ -14,10 +14,10 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         // 和为i的子集排列数，递推式 dp[i]=sum(dp[i-num])
-        // 子集排列数的写法刚好 将背包问题的外层物品循环改内层
+        // 这写法刚好 将背包问题的外层物品循环改内层
         vector<int> dp(target + 1, 0);
         dp[0] = 1;
-        for (int i = 0; i <= target; i++) {
+        for (int i = 1; i <= target; i++) {
             for (int num : nums) {
                 if (i - num >= 0) dp[i] += dp[i-num];
             }
