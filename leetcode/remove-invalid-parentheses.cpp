cbca56f0af0f5c112d029a20910760be89c19461
smilingpoplar <smilingpoplar@gmail.com>
2019-02-20 17:45:32 +0800

    update

--- leetcode/remove-invalid-parentheses.cpp
+++ leetcode/remove-invalid-parentheses.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 class Solution {
 public:
-    // https://leetcode.com/problems/remove-invalid-parentheses/discuss/75027/Easy-Short-Concise-and-Fast-Java-DFS-3-ms-solution
     vector<string> removeInvalidParentheses(string s) {
         vector<string> ans;
         search(s, 0, 0, "()", ans);
@@ -21,18 +20,24 @@ public:
     }
     
     void search(const string &s, int idx, int lastRmIdx, const string &paren, vector<string> &ans) { 
+        // 先从左往右扫，确保多出的左括号数leftDiff>=0；
+        //   再从右往左扫，确保多出的右括号数>=0（从右往左就是将原串取反、改查右括号）。
+        // 从左往右遇到leftDiff<0时前缀串不合法，要从[lastRmIdx..i]间删掉一个')'，lastRmIdx是上次删除位置。
+        //   将删除限制在[lastRmIdx..i]间（而不是在[0..i]间），是为防止因删除顺序不同而使剩下括号串重复。
+        // 删除时遇到连续')'时只删第一个，也是为了防止剩下括号串重复。
         int leftDiff = 0;
         for (int i = idx; i < s.size(); i++) {
             if (s[i] == paren[0]) leftDiff++;
             else if (s[i] == paren[1]) leftDiff--;
             if (leftDiff >= 0) continue;
-            // i前面有个')'要删除，只删除连续')'的第一个
+            // [lastRmIdx..i]间有个')'要删除，只删连续')'的第一个
             for (int j = lastRmIdx; j <= i; j++) {
                 if (s[j] == paren[1] && (j == lastRmIdx || s[j-1] != s[j])) {
-                    search(s.substr(0, j) + s.substr(j + 1), i, j, paren, ans); // 删除')'，并进入子问题
+                    // 删除s[j]，递归从i开始（原i+1因删除前移）
+                    search(s.substr(0, j) + s.substr(j + 1), i, j, paren, ans);
                 }
             }
-            return; // 不删除')'的话，后面已无效
+            return; // 不删除')'，串已无效，后面不用再看
         }
 
         string reversed(s.rbegin(), s.rend());
--- leetcode/valid-parenthesis-string.cpp
+++ leetcode/valid-parenthesis-string.cpp
@@ -13,16 +13,21 @@ using namespace std;
 class Solution {
 public:
     bool checkValidString(string s) {
-        int lo = 0, hi = 0; // 左括号剩余数的可能取值范围[lo, hi]
+        // '*' 可表示 '('、')'、空串，设多出的左括号数leftDiff的范围为[lo, hi]。
+        // 1. 遇到'('，左括号多1，lo++, hi++；
+        // 2. 遇到')'，左括号少1，lo--, hi--；
+        // 3. 遇到'*'，它可以是'('、空串、')'，[lo, hi]内的每个值都可-1、+0、+1，整体扩张lo--, hi++。
+        // 综上，遇到'('时lo++、其他lo--；遇到')'时hi--，其他hi++。 
+        int lo = 0, hi = 0;
         for (char c : s) {
             lo += (c == '(') ? 1 : -1;
             hi += (c == ')') ? -1 : 1;
-            // 在这过程中，hi一旦小于0，[lo, hi]没有>=0的有效值，串无效；
-            // 而lo小于0的值可以舍弃，lo=max(lo, 0)
+            // 在扫描过程中，一旦hi<0，leftDiff<0，串无效；
+            // 而lo<0的部分可以直接丢弃，lo=max(lo, 0)
             if (hi < 0) return false;
             lo = max(lo, 0);
         }
-        // 最终[lo,hi]含0串才有
+        // 最终能够leftDiff==0，串才有效
         return lo == 0;
     }
 };
