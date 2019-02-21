45fc4945fa7a2385c6ed2425f0af35d35f4d9b11
smilingpoplar <smilingpoplar@gmail.com>
2019-02-21 23:34:32 +0800

    update

---
+++ leetcode/validate-stack-sequences.cpp
@@ -0,0 +1,33 @@
+//
+//  validate-stack-sequences
+//  https://leetcode.com/problems/validate-stack-sequences/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> stk;
+        int j = 0;
+        for (int num : pushed) {
+            stk.push(num);
+            // num先入栈，栈顶与popped[j]相同时弹出
+            while (!stk.empty() && stk.top() == popped[j]) {
+                stk.pop();
+                j++;
+            }
+        }
+        return j == popped.size();
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
