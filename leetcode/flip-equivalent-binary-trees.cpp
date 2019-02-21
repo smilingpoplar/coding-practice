df98dce05e3d0fcff718555914b5b9dda62634ea
smilingpoplar <smilingpoplar@gmail.com>
2019-02-21 18:34:18 +0800

    update

---
+++ leetcode/flip-equivalent-binary-trees.cpp
@@ -0,0 +1,27 @@
+//
+//  flip-equivalent-binary-trees
+//  https://leetcode.com/problems/flip-equivalent-binary-trees/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        if (!root1 && !root2) return true;
+        if (!root1 || !root2) return false;
+        if (root1->val != root2->val) return false;
+        return (flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right))
+            || (flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left));
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
