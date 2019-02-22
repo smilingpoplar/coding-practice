8a4f4445bcf9f162c2b4ac686b7355e928eee96e
smilingpoplar <smilingpoplar@gmail.com>
2019-02-22 22:07:06 +0800

    update

---
+++ leetcode/distinct-subsequences-ii.cpp
@@ -0,0 +1,34 @@
+//
+//  distinct-subsequences-ii
+//  https://leetcode.com/problems/distinct-subsequences-ii/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int distinctSubseqII(string S) {
+        const int MOD = 1e9 + 7;
+        // 用count[26]记录S[0..i]以各字母结尾的子序列各有多少
+        vector<int> count(26, 0);
+        long sum = 0; // sum为count[]的和
+        for (char c : S) {
+            // 以字母c结尾的子序列有sum+1个，sum为原先子序列扩展个c，1是新的字母c序列
+            int idx = c - 'a';
+            int diff = (sum + 1 - count[idx] + MOD) % MOD;
+            count[idx] = (count[idx] + diff) % MOD;
+            sum = (sum + diff) % MOD;
+        }
+        return sum;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ leetcode/minimum-cost-for-tickets.cpp
@@ -0,0 +1,39 @@
+//
+//  minimum-cost-for-tickets
+//  https://leetcode.com/problems/minimum-cost-for-tickets/
+//
+//  Created by smilingpoplar on 15/6/7.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // 设dp[i]表示day[1..i]的minCost。i不在days中时，dp[i]=dp[i-1]；否则，
+        // dp[i] = min( dp[i-1]+costs[0], dp[max(0,i-7)]+costs[1], dp[max(0,i-30)]+costs[2] )
+        unordered_set<int> ds(days.begin(), days.end());
+        const int TOTAL_DAYS = 365;
+        vector<int> dp(TOTAL_DAYS + 1);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= TOTAL_DAYS; i++) {
+            if (!ds.count(i)) {
+                dp[i] = dp[i-1];
+            } else {
+                dp[i] = min({dp[i-1] + costs[0], 
+                             dp[max(0,i-7)] + costs[1], 
+                             dp[max(0,i-30)] + costs[2]});
+            }
+        }
+        return dp[TOTAL_DAYS];
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
