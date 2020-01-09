2c2688e7615f600f8f52cbae0652663b8f5c0141
smilingpoplar <smilingpoplar@gmail.com>
2020-01-09 23:18:19 +0800

    update

--- leetcode/remove-duplicate-letters.cpp
+++ leetcode/remove-duplicate-letters.cpp
@@ -13,18 +13,18 @@ using namespace std;
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        // 要使剩余串的字典序最小，需要留下递增栈，找下一个更小的数
-        // 已入选栈中不再考虑；某字母有剩余时才可pop()
-        vector<int> cnt(128, 0); // 各字母有多少富余
+        // 要使剩余串的字典序最小<=>递增栈<=>找下一个更小的数
+        // 限制条件：1.某字母有富余时才可pop() 2.已入栈的不再选择
+        vector<int> cnt(128, 0); // 各字母计数
         for (char c : s) cnt[c]++;
         vector<bool> picked(128, false);
 
         string stk;
         for (char c : s) {
             cnt[c]--;
-            if (picked[c]) continue;
+            if (picked[c]) continue; // 条件2
             while (!stk.empty() && c < stk.back() 
-                    && cnt[stk.back()] > 0) {
+                    && cnt[stk.back()] > 0) { // 条件1
                 int top = stk.back(); stk.pop_back();
                 picked[top] = false;
             }
