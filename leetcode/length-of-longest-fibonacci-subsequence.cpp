ac42fdec8028ed9219fadde58e8df9639fe3be9a
smilingpoplar <smilingpoplar@gmail.com>
2020-01-12 21:29:20 +0800

    update

--- leetcode/length-of-longest-fibonacci-subsequence.cpp
+++ leetcode/length-of-longest-fibonacci-subsequence.cpp
@@ -15,19 +15,19 @@ class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& A) {
         // 设dp[i][j]表示以A[i]、A[j]结尾的长>=2的最长Fib子序列长
-        // 将A中值作value=>idx的映射，i,j的前一个索引idx=mp[A[j]-A[i]]，
+        // 将A中值作val=>idx的映射，i,j的前一个索引idx=mp[A[j]-A[i]]，
         // dp[i][j] = dp[idx][i] + 1
         const int N = A.size();
-        unordered_map<int, int> mp; // value=>idx
+        unordered_map<int, int> mp; // val=>idx
         vector<vector<int>> dp(N, vector<int>(N, 0));
 
         int ans = 0;
         for (int j = 0; j < N; j++) {
             mp[A[j]] = j;
             for (int i = 0; i < j; i++) {
-                int value = A[j] - A[i];
-                if (value < A[i] && mp.count(value)) {
-                    int idx = mp[value];
+                int val = A[j] - A[i];
+                if (val < A[i] && mp.count(val)) {
+                    int idx = mp[val];
                     dp[i][j] = dp[idx][i] + 1; // dp[i][j]现在>=3
                     ans = max(ans, dp[i][j]);                        
                 } else {
