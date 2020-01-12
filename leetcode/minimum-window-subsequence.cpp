e268d7c2a638d59fe7c6ff55ea80b3f315ecbcd9
smilingpoplar <smilingpoplar@gmail.com>
2020-01-12 23:19:58 +0800

    update

--- leetcode/minimum-window-subsequence.cpp
+++ leetcode/minimum-window-subsequence.cpp
@@ -14,12 +14,12 @@ using namespace std;
 class Solution {
 public:
     string minWindow(string S, string T) {
-        // 设dp[i][j]=k表示在S[0..i)中找T[0..j)子序列、找到的最右起始索引，
+        // 设dp[i][j]=k表示在S[0..i)中找T[0..j)子序列、找到的最右起始索引（最右=>最小窗口），
         // 找到时S[k..i)是包含T[0..j)的最小窗口，找不到k==-1。
         // 若S[i-1]==T[j-1]，需在S[0..i-1)中找T[0..j-1)，dp[i][j]=dp[i-1][j-1]
         // 若S[i-1]!=T[j-1]，需在S[0..i-1)中找T[0..j)，dp[i][j]=dp[i-1][j]
         // 所以 dp[i][j] = (S[i-1]==T[j-1]) ? dp[i-1][j-1] : dp[i-1][j]
-        // 初始时dp[i][0]=i /*S[k..i)为空串*/, dp[0][j>0]=-1。所求为dp[M][N]。
+        // 初始时dp[i][0]=i /*S[k..i)为空串*/, dp[0][j>0]=-1。
         const int M = S.size(), N = T.size();
         vector<vector<int>> dp(M + 1, vector<int>(N + 1, -1));
         for (int i = 0; i <= M; i++) dp[i][0] = i;
