4e68dad9130e28e271c9f40f1512b2febaf1c8d4
smilingpoplar <smilingpoplar@gmail.com>
2020-01-12 20:29:34 +0800

    update

--- leetcode/k-th-smallest-in-lexicographical-order.cpp
+++ leetcode/k-th-smallest-in-lexicographical-order.cpp
@@ -20,16 +20,16 @@ public:
             if (k > count) { // 走右兄弟节点
                 curr++;
                 k -= count;
-            } else { // 不是当前节点，走最左子节点
+            } else { // 走最左子节点
                 curr *= 10;
                 k--;
             }
         }
         return curr;
     }
-    
-    // 以数p为根的子树含多少[1,n]间的数？
-    // 可累加[p,p+1)、[p *10,(p+1) *10)、[p *100,(p+1) *100)、... 与[1,n]重合的部分
+
+    // 想象十叉树中，以数p为根的子树含多少[1,n]间的数？
+    // 可累加[p,p+1)、[p*10,(p+1)*10)、[p*100,(p+1)*100)、... 与[1,n]重合的部分
     int countNums(int p, long n) {
         int count = 0;
         long start = p, end = p + 1;
