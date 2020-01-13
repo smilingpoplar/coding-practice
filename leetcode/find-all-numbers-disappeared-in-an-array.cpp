dcafef30c315434926436dcca9947ef9654cb03e
smilingpoplar <smilingpoplar@gmail.com>
2020-01-13 11:46:52 +0800

    update

--- leetcode/find-all-numbers-disappeared-in-an-array.cpp
+++ leetcode/find-all-numbers-disappeared-in-an-array.cpp
@@ -20,7 +20,7 @@ public:
         }
         vector<int> ans;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] > 0) ans.push_back(i + 1);
+            if (nums[i] > 0) ans.push_back(i+1);
         }
         return ans;
     }
--- leetcode/first-missing-positive.cpp
+++ leetcode/first-missing-positive.cpp
@@ -16,12 +16,10 @@ public:
     int firstMissingPositive(vector<int>& nums) {
         // 期望变成数组[1..n]。位置i应放数i+1，即nums[i]==i+1；
         // 数x应在位置x-1，即nums[i]应在位置nums[i]-1，nums[nums[i]-1]==nums[i]。
-        // 旋转置换：
-        // 若nums[nums[i]-1]!=nums[i]，交换位置nums[i]-1和位置i处的值，然后继续处理位置i。
+        // 旋转置换：若nums[nums[i]-1]!=nums[i]，交换位置nums[i]-1和位置i处的值。
         const int N = nums.size();
         for (int i = 0; i < N; i++) {
-            while (1 <= nums[i] && nums[i] <= N 
-                    && nums[nums[i]-1] != nums[i]) {
+            while (1 <= nums[i] && nums[i] <= N && nums[nums[i]-1] != nums[i]) {
                 swap(nums[nums[i]-1], nums[i]);
             }
         }
