9eb51a3e602d3a298ed48e231ae6610ffe7dbc20
smilingpoplar <smilingpoplar@gmail.com>
2020-01-13 22:01:32 +0800

    update

--- leetcode/burst-balloons.cpp
+++ leetcode/burst-balloons.cpp
@@ -13,13 +13,12 @@ using namespace std;
 class Solution {
 public:
     int maxCoins(vector<int>& nums) {
-        // 最后戳破的气球k将区间[0..n-1]分成两区间[0..k-1]、[k+1..n-1]，两区间的得分互不影响，这就是子问题。
-        // 对任意区间nums[i..j]考虑其最后一戳k，设dp[i][j]表示a[i..j]戳气球的最大得分，
-        // dp[i][j] = nums[i-1]*nums[k]*nums[j+1]（戳破气球k得分）+ dp[i][k-1] + dp[k+1][j]，i<=k<=j
+        // 对区间nums[i..j]考虑其最后一戳k，在此之前一定戳完了区间nums[i..k-1]和nums[k+1..j]，
+        // 设dp[i][j]表示nums[i..j]戳气球的最大得分，
+        // dp[i][j] = max{ nums[i-1]*nums[k]*nums[j+1]（戳气球k得分）+ dp[i][k-1] + dp[k+1][j] }，i<=k<=j
         // i维上i依赖于k+1(>i)，i逆序遍历；j维上j依赖于k-1(<j)，j正序遍历
 
-
-        // 先把nums前后添加1扩展成a，设N是扩展后的长度，则1<=i<=j<=N-2，所求dp[1][N-2]    
+        // 先把nums前后添加1扩展成a，设N是扩展后的长度，则1<=i<=j<=N-2，所求为dp[1][N-2]
         const int N = (int)nums.size() + 2;
         vector<int> a(N);
         a[0] = a[N-1] = 1;
