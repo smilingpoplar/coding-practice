f8731738dd93302638d046d41d96298119b4a629
smilingpoplar <smilingpoplar@gmail.com>
2020-01-14 00:37:54 +0800

    update

--- leetcode/student-attendance-record-ii.cpp
+++ leetcode/student-attendance-record-ii.cpp
@@ -13,14 +13,13 @@ using namespace std;
 class Solution {
 public:
     int checkRecord(int n) {
-        // 各个条件作dp的一个维度，设dp[i][a][l]表示s[0..i)、*最多*有a个A、*最多*有l个连续L的有效记录数
+        // 各个条件作dp的一个维度，设dp[i][a][l]表示s[0..i)、*最多*有a个A、末尾*最多*有l个连续L的有效记录数
         // 原问题求dp[n][1][2]，初始dp[0][][]=1（因为“最多”，所以空串都是有效记录）
         // dp[i][a][l] = dp[i-1][a-1][2] //..A，前面子串末尾最多可有2个连续L
         //               + dp[i-1][a][l-1] //..L
         //               + dp[i-1][a][2] //..P
         // 递推式在i维上只依赖于i-1项，省掉i这维，i仍从左往右遍历
-        // 三维降维要用临时变量
-        // ndp[a][l] = dp[a-1][2] + dp[a][l-1] + dp[a][2]
+        // 三维降维要用临时变量：ndp[a][l] = dp[a-1][2] + dp[a][l-1] + dp[a][2]
         const int MOD = 1e9 + 7;
         vector<vector<int>> dp(2, vector<int>(3, 1)); // i==0
         for (int i = 1; i <= n; i++) {
