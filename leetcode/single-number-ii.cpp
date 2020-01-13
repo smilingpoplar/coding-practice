23bfa5c34a905dff6077742a904586c8c0e2bc52
smilingpoplar <smilingpoplar@gmail.com>
2020-01-13 13:46:20 +0800

    update

--- leetcode/single-number-ii.cpp
+++ leetcode/single-number-ii.cpp
@@ -16,25 +16,24 @@ public:
     int singleNumber(vector<int>& nums) {
         // 泛化成：其他数出现k次，某个数出现p次（p不能被k整除），找这单个数
         // bit运算各个位相互独立，下面用int同时统计相互独立的各个位，逻辑上可把整个int当作一个bit
-        // 要统计某位上出现1的次数，需要个mod k计数器（该计数器共m>=lgk位），设xm,..,x2,x1表示该计数器的位，
-        // 1. 进位规则：..., x4^=x3&x2&x1&nums[i], x3^=x2&x1&nums[i], x2^=x1&nums[i], x1^=nums[i]
-        //    即 后面位和新数nums[i] 都为1时才进位到当前位
-        // 2. 计数为k时直接清零：当xm,..,x1的各个位分别等于k的各个位km,..,k1时，计数为k；判断计数为k的表达式为
-        //    (xm==km)&..&(x1==k1)，分量yj:(xj==kj)等价于kj==1时取xj、kj==0时取xj'(xj'表示xj取非)，
-        //    因此，判断计数为k的表达式为y1&..&ym；
-        //    计数为k时直接清零的 mask = ~(y1&..&ym)
-        //    有了mask后清零：..., x4&=mask, x3&=mask, x2&=mask, x1&=mask
-        // 3. 最终只要计数器的计数>0，即x1|x2|x3|x4|...，该统计位为1
-        int x1 = 0, x2 = 0;
+        // 要统计某位上出现1的次数，需要个mod k计数器（该计数器共m>=lgk位），设bm,..,b2,b1表示该计数器的位，
+        // 1. 进位规则：..., b4^=b3&b2&b1&nums[i], b3^=b2&b1&nums[i], b2^=b1&nums[i], b1^=nums[i]
+        //    即 从最高位开始更新，后面位和新数nums[i]都为1时才进位
+        // 2. 计数为k时直接清零：当bm,..,b1的各个位分别等于k的各个位km,..,k1时，计数为k，故判断计数为k的表达式
+        //    为(bm==km)&..&(b1==k1)。分量(bj==kj)表示于kj==1时取bj、kj==0时取~bj
+        //    计数为k时直接清零的 mask = ~((b1==k1)&..&(bm==km))
+        //    有了mask后清零：..., b4&=mask, b3&=mask, b2&=mask, b1&=mask
+        // 3. 最终只要计数>0，即b1|b2|b3|b4|...，该逻辑位为1
+        int b1 = 0, b2 = 0;
         int mask = 0;
         for (int num : nums) {
-            x2 ^= x1 & num;
-            x1 ^= num;
-            mask = ~(x1 & x2); // k =3 =(11)_2
-            x2 &= mask;
-            x1 &= mask;
+            b2 ^= b1 & num;
+            b1 ^= num;
+            mask = ~(b1 & b2); // k =3 =(11)_2
+            b2 &= mask;
+            b1 &= mask;
         }
-        return x1 | x2;
+        return b1 | b2;
     }
 };
 
