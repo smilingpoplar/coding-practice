c8a6c2e46901fa78d996880761358095b47130a1
smilingpoplar <smilingpoplar@gmail.com>
2020-01-15 15:38:09 +0800

    update

--- leetcode/house-robber-ii.cpp
+++ leetcode/house-robber-ii.cpp
@@ -17,19 +17,18 @@ public:
         if (nums.empty()) return 0;
         const int N = nums.size();
         if (N == 1) return nums[0];
-        // 若不抢房子i，就能从i处将循环数组切断，变成无循环数组的子问题。
-        // 相邻两个房子不能同时抢，从相邻两个房子分别切开，就能得到两个无循环子问题。
-        // 不妨不能同时抢首尾两个房子，得到无循环的子问题 rob(nums,1,n-1)和rob(nums,0,n-2)
+        // 相邻房子不能同时抢，从某对相邻房子中间切开，就能得两个无循环的子问题。
+        // 比如从首尾这对相邻房子处切开：rob(nums,1,n-1)和rob(nums,0,n-2)
         return max(robSub(nums, 1, N - 1), robSub(nums, 0, N - 2));
     }
     // rob nums[from,to]
     int robSub(const vector<int> &nums, int from, int to) {
-        // 设dp[i]表示抢了nums[0..i]后的最大值，0<=i<N
+        // 设dp[i]表示抢了nums[0..i]后的最大值，from<=i<=to
         // dp[i] = max( dp[i-1], nums[i]+dp[i-2] )，初始dp[-2]=dp[-1]=0
-        // 看递推式只依赖于前两项，前两项分别用prev2和prev1表示
+        // 递推式只依赖于前两项，记前两项为prev2和prev1
         int prev2 = 0, prev1 = 0;
         for (int i = from; i <= to; i++) {
-            int curr = max(nums[i] + prev2, prev1);
+            int curr = max(prev1, nums[i] + prev2);
             prev2 = prev1;
             prev1 = curr;
         }
