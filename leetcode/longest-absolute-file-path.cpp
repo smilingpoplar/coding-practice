b757935eaf9af40bd1879b8ef3bd1f8932f28e88
smilingpoplar <smilingpoplar@gmail.com>
2020-01-18 00:52:01 +0800

    update

--- leetcode/longest-absolute-file-path.cpp
+++ leetcode/longest-absolute-file-path.cpp
@@ -18,10 +18,10 @@ public:
         string line;
         stack<int> stk;
         int ans = 0;
-        while (getline(iss, line)) { // 遍历\n每行路径，并将路径长压栈
+        while (getline(iss, line)) { // 遍历\n每行路径，并将绝对路径长压栈
             int indent = 0;
             while (line[indent] == '\t') indent++;
-            while (stk.size() > indent) stk.pop(); // 确保压栈时紧接着父目录位置，即栈元素个数等于缩进数
+            while (stk.size() > indent) stk.pop(); // 父目录数等于缩进数
             int filelen = line.size() - indent;
             int pathlen = (stk.empty() ? 0 : stk.top() + 1) + filelen; // 1是分隔符长
             stk.push(pathlen);
