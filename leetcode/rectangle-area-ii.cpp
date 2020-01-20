4659260ea6e7b05d4677dd70ae99ae4497024a2f
smilingpoplar <smilingpoplar@gmail.com>
2020-01-21 00:33:13 +0800

    update

--- leetcode/rectangle-area-ii.cpp
+++ leetcode/rectangle-area-ii.cpp
@@ -14,21 +14,21 @@ using namespace std;
 class Solution {
 public:
     int rectangleArea(vector<vector<int>>& rectangles) {
-        const int OPEN = 0, CLOSE = 1;
-        vector<vector<int>> events;
-        // 遍历events时要相当于水平线向上扫，所以先按y坐标排序
-        for (auto &rect : rectangles) {
-            events.push_back({rect[1], OPEN, rect[0], rect[2]});
-            events.push_back({rect[3], CLOSE, rect[0], rect[2]});
-        }
-        sort(events.begin(), events.end());
-
         // 扫描线算法：
         // 水平扫描线向上扫，每遇到y边界都累加一横块面积。
         // 面积高为 (y边界-前一y边界)，面积长为 旧区间集合的重叠长。
 
-        // 记得更新区间集合： 
-        // 下边界把横坐标区间[x1,x2]加入集合，上边界把[x1,x2]移出集合。
+        // 要水平线向上扫，所以先按y坐标排序
+        const int LOWER = 0, UPPER = 1;
+        vector<vector<int>> events;
+        for (auto &rect : rectangles) {
+            events.push_back({rect[1], LOWER, rect[0], rect[2]});
+            events.push_back({rect[3], UPPER, rect[0], rect[2]});
+        }
+        sort(events.begin(), events.end());
+
+        // 遇到下边界把横坐标区间[x1..x2]加入集合，
+        // 遇到上边界把[x1..x2]移出集合。
         const int MOD = 1e9 + 7;
         multiset<vector<int>> st; // st{ [x1,x2] }
         int preY = INT_MIN;
@@ -37,11 +37,11 @@ public:
             int y = e[0], type = e[1], x1 = e[2], x2 = e[3];
             if (preY != INT_MIN) {
                 int height = y - preY;
-                int width = intervalsLen(st);
+                int width = getWidth(st);
                 ans = (ans + (long)width * height) % MOD;
             }
             
-            if (type == OPEN) st.insert({x1, x2});
+            if (type == LOWER) st.insert({x1, x2});
             else st.erase(st.find({x1, x2}));
             preY = y;
         }
@@ -49,7 +49,7 @@ public:
     }
     
     // 区间集合的重叠长
-    int intervalsLen(multiset<vector<int>> &st) {
+    int getWidth(multiset<vector<int>> &st) {
         int ans = 0;
         int curr = INT_MIN;
         for (auto &x : st) {
@@ -61,7 +61,7 @@ public:
     }
 
     /*
-    int intervalsLen(multiset<vector<int>> &st) {
+    int getWidth(multiset<vector<int>> &st) {
         int ans = 0;
         int start = 0, end = 0; // [start,end)是上个待合并区间
         for (auto &x : st) {
