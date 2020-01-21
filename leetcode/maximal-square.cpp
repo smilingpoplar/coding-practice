ee6893a0ec8e230d9fa7b520c67d4b9e85792224
smilingpoplar <smilingpoplar@gmail.com>
2020-01-21 16:00:25 +0800

    update

--- leetcode/maximal-square.cpp
+++ leetcode/maximal-square.cpp
@@ -14,7 +14,7 @@ class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
         // 设dp[i][j]表示从子矩阵右下角[i-1,j-1]能向上向左扩展的最大正方形边长。
-        // 当matrix[i-1][j-1]==1时，
+        // 当matrix[i-1][j-1]=='1'时，
         // dp[i][j] = 1 /*右下角的'1'*/ + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) /*即左、上、左上矩阵*/
         // 初始dp[0][..]=dp[..][0]=0
         if (matrix.empty()) return 0;
