7c84fb5a49228d7a657104751a6b1958f5411925
smilingpoplar <smilingpoplar@gmail.com>
2020-02-08 22:22:57 +0800

    update

---
+++ leetcode/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,34 @@
+//
+//  remove-all-adjacent-duplicates-in-string-ii
+//  https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        vector<pair<char, int>> stk;
+        for (char c : s) {
+            if (stk.empty() || stk.back().first != c) {
+                stk.push_back({c, 1});
+            } else if (++stk.back().second == k) {
+                stk.pop_back();
+            }
+        }
+        string ans;
+        for (auto &e : stk) {
+            ans += string(e.second, e.first);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
