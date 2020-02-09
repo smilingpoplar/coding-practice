0539e193d6609e6c041596f2077f6c57205d5db2
smilingpoplar <smilingpoplar@gmail.com>
2020-02-10 02:48:33 +0800

    update

---
+++ leetcode/number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,29 @@
+//
+//  number-of-equivalent-domino-pairs
+//  https://leetcode.com/problems/number-of-equivalent-domino-pairs/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        unordered_map<int, int> cnt; // code=>count
+        int ans = 0;
+        for (auto &d : dominoes) {
+            int code = min(d[0], d[1]) * 10 + max(d[0], d[1]);
+            // 新增code和同值code都能成对
+            ans += cnt[code]++;
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
