9ffa1d2a1718ba0fe473b2f9137c25ff467a3768
smilingpoplar <smilingpoplar@gmail.com>
2020-02-09 19:16:51 +0800

    update

---
+++ leetcode/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,31 @@
+//
+//  smallest-integer-divisible-by-k
+//  https://leetcode.com/problems/smallest-integer-divisible-by-k/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int K) {
+        // K个连续'1'一定能整除K
+        set<int> seen; // 见过的余数
+        int num = 0;
+        for (int i = 1; i <= K; i++) {
+            num = (num * 10 + 1) % K;
+            if (num == 0) return i;
+            if (seen.count(num)) return -1;
+            seen.insert(num);
+        }
+        return -1;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
