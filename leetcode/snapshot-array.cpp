76e933eab94dc0634f1744b91c3319273c8523c9
smilingpoplar <smilingpoplar@gmail.com>
2020-02-09 17:31:14 +0800

    update

---
+++ leetcode/snapshot-array.cpp
@@ -0,0 +1,48 @@
+//
+//  snapshot-array
+//  https://leetcode.com/problems/snapshot-array/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class SnapshotArray {
+    vector<map<int, int>> data; // 每个slot记录历史修改{snapId,val}
+    int snapId = 0;
+public:
+    SnapshotArray(int length) : data(length) {
+    }
+    
+    void set(int index, int val) {
+        data[index][snapId] = val;
+    }
+    
+    int snap() {
+        snapId++;
+        return snapId - 1;
+    }
+    
+    int get(int index, int snap_id) {
+        auto &mp = data[index];
+        // 找<=snap_id时的值
+        auto it = mp.upper_bound(snap_id);
+        if (it == mp.begin()) return NULL;
+        return prev(it)->second;
+    }
+};
+
+/**
+ * Your SnapshotArray object will be instantiated and called as such:
+ * SnapshotArray* obj = new SnapshotArray(length);
+ * obj->set(index,val);
+ * int param_2 = obj->snap();
+ * int param_3 = obj->get(index,snap_id);
+ */
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
---
+++ leetcode/time-based-key-value-store.cpp
@@ -0,0 +1,36 @@
+//
+//  time-based-key-value-store
+//  https://leetcode.com/problems/time-based-key-value-store/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class TimeMap {
+    unordered_map<string, map<int, string>> data; // key=>{timestamp,value}
+    int timestamp = 0;
+public:
+    /** Initialize your data structure here. */
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        data[key][timestamp] = value;
+    }
+    
+    string get(string key, int timestamp) {
+        auto &mp = data[key];
+        // 找<=timestamp时的值
+        auto it = mp.upper_bound(timestamp);
+        if (it == mp.begin()) return "";
+        return prev(it)->second;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
