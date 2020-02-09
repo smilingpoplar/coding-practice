c840a522280bac583e8c949936dbe6338c5bff6b
smilingpoplar <smilingpoplar@gmail.com>
2020-02-10 02:10:10 +0800

    update

---
+++ leetcode/maximum-nesting-depth-of-two-valid-parentheses-strings.cpp
@@ -0,0 +1,35 @@
+//
+//  maximum-nesting-depth-of-two-valid-parentheses-strings
+//  https://leetcode.com/problems/maximum-nesting-depth-of-two-valid-parentheses-strings/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> maxDepthAfterSplit(string seq) {
+        // 括号子序列尽量均匀地分成两组
+        const int N = seq.size();
+        vector<int> ans(N, 0);
+        int depth = 0;
+        for (int i = 0; i < N; i++) {
+            if (seq[i] == '(') {
+                depth++;
+                if (depth % 2 == 0) ans[i] = 1;
+            } else {
+                depth--;
+                if (depth % 2 == 1) ans[i] = 1;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
