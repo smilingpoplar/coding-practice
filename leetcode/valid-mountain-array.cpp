cb49f29b469ed71e811c49ebd5736d028627b85e
smilingpoplar <smilingpoplar@gmail.com>
2020-02-10 02:36:02 +0800

    update

---
+++ leetcode/valid-mountain-array.cpp
@@ -0,0 +1,26 @@
+//
+//  valid-mountain-array
+//  https://leetcode.com/problems/valid-mountain-array/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        const int N = A.size();
+        int i = 0, j = N - 1;
+        while (i + 1 < N && A[i] < A[i+1]) i++;
+        while (j > 0 && A[j] < A[j-1]) j--;
+        return i > 0 && j < N - 1 && i == j;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
