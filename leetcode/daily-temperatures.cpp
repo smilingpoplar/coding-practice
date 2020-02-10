696a0ec2e871a7a786413e8efd3d07584f3c2b17
smilingpoplar <smilingpoplar@gmail.com>
2020-02-10 14:47:27 +0800

    update

--- leetcode/daily-temperatures.cpp
+++ leetcode/daily-temperatures.cpp
@@ -12,12 +12,12 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        const int N = temperatures.size();
+    vector<int> dailyTemperatures(vector<int>& T) {
+        const int N = T.size();
         vector<int> ans(N, 0);
         stack<int> stk;
         for (int i = 0; i < N; i++) {
-            while (!stk.empty() && temperatures[i] > temperatures[stk.top()]) {
+            while (!stk.empty() && T[i] > T[stk.top()]) {
                 int top = stk.top(); stk.pop();
                 ans[top] = i - top;                
             }
