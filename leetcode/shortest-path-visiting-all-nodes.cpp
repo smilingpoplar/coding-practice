88fc355e37c5e7119af4e6fa22c0e7065b35e3b8
smilingpoplar <smilingpoplar@gmail.com>
2020-02-13 00:01:49 +0800

    update

--- leetcode/shortest-path-visiting-all-nodes.cpp
+++ leetcode/shortest-path-visiting-all-nodes.cpp
@@ -44,6 +44,64 @@ public:
     }
 };
 
+/*
+class Solution {
+    const int INF = 1e9;
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        const int N = graph.size();
+        vector<vector<int>> dist(N, vector<int>(N, INF));
+        for (int i = 0; i < N; i++) {
+            for (int j : graph[i]) {
+                dist[i][j] = 1;
+            }
+        }
+        // floyd算法，求所有点对最短距离
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+        
+        return shortestHamilton(dist);
+    }
+    
+    int shortestHamilton(vector<vector<int>> &dist) {
+        // dp求哈密顿路径
+        // 设dp[group][dst]表示经过集合group中的所有节点、
+        // 并最终停在group中dst节点的最短路径，
+        // 在group中选一节点u、在group外选一节点v，
+        // dp[group+{v}] = min(dp[group+{v}], dp[group][u] + dist[u][v] )
+        // 其中dist[u][v]表示u->v的最短路径，可用Floyd算法三重循环松弛得到
+        const int N = dist.size();
+        vector<vector<int>> dp(1<<N, vector<int>(N, INF));
+        for (int i = 0; i < N; i++) {
+            dp[1<<i][i] = 0;
+        }
+        for (int group = 1; group < (1<<N); group++) {
+            // group中选一点u、group外选一点v
+            for (int u = 0; u < N; u++) {
+                for (int v = 0; v < N; v++) {
+                    int uMask = 1<<u, vMask = 1<<v;
+                    if ((group & uMask) && !(group & vMask)) {
+                        dp[group|vMask][v] = min(dp[group|vMask][v],
+                                                 dp[group][u] + dist[u][v]);
+                    }
+                }
+            }
+        }
+        // 从所有点出发的最小值
+        int ans = INF;
+        for (int i = 0; i < N; i++) {
+            ans = min(ans, dp[(1<<N)-1][i]);
+        }
+        return ans;    
+    }
+};
+*/
+
 int main(int argc, const char * argv[]) {
     return 0;
 }
