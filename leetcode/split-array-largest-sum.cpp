4f11e2a3013d33e346a694a0ae64ec0ef6b37054
smilingpoplar <smilingpoplar@gmail.com>
2020-09-18 11:21:19 +0800

    update

--- leetcode/split-array-largest-sum.cpp
+++ leetcode/split-array-largest-sum.cpp
@@ -14,8 +14,6 @@ class Solution {
 public:
     int splitArray(vector<int>& nums, int m) {
         // 各子段和的最大值x在值范围[max(nums), sum(nums)]
-        // x和子段数的关系：x越小、子段越短、子段数越多；x越大、子段越长、子段数越少
-        // 设二分搜索条件valid(x)表示“子段和<=x的子段数”count<=m，满足形式[0 0 ... 0 1 1 ...]
         int mx = 0;
         long sum = 0;
         for (int num : nums) {
@@ -23,10 +21,11 @@ public:
             sum += num;
         }
 
+        // 二分搜素猜子段和的最大值x
         long l = mx, u = sum;
         while (l <= u) {
             long mid = l + (u - l) / 2;
-            if (valid(mid, nums, m)) {
+            if (enough(mid, nums, m)) {
                 u = mid - 1;
             } else {
                 l = mid + 1;
@@ -35,7 +34,9 @@ public:
         return l;        
     }
     
-    bool valid(long x, vector<int> &nums, int m) {
+    bool enough(long x, vector<int> &nums, int m) {
+        // 子段和的最大值x和子段数m的关系，是关于x的递减函数count(x)
+        // 条件式count(x)<=m满足二分搜索的条件形式[0 0 ... 0 1 1 ...]
         int count = 1;
         long sum = 0;
         for (int num : nums) {
