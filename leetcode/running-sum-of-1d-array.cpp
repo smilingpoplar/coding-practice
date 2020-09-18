e847440daa0a74c3a62c5aa1e0a209efe95d0d05
smilingpoplar <smilingpoplar@gmail.com>
2020-09-19 00:25:07 +0800

    update

---
+++ leetcode/running-sum-of-1d-array.cpp
@@ -0,0 +1,23 @@
+//
+//  running-sum-of-1d-array
+//  https://leetcode.com/problems/running-sum-of-1d-array/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        partial_sum(begin(nums), end(nums), begin(nums));
+        return nums;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
