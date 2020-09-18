87138914d899d5c24d0bba183f92d9e4996643a7
smilingpoplar <smilingpoplar@gmail.com>
2020-09-18 22:34:41 +0800

    update

---
+++ leetcode/next-greater-node-in-linked-list.cpp
@@ -0,0 +1,48 @@
+//
+//  maximal-square
+//  https://leetcode.com/problems/maximal-square/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> arr;
+        while (head) {
+            arr.push_back(head->val);
+            head = head->next;
+        }
+        
+        const int N = arr.size();
+        vector<int> ans(N, 0);
+        stack<int> stk; // 单调栈，保存下标
+        for (int i = 0; i < N; i++) {
+            while (!stk.empty() && arr[i] > arr[stk.top()]) {
+                ans[stk.top()] = arr[i];
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
