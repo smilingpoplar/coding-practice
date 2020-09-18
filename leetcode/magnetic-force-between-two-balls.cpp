62cfbecd43df028c45544616558403b036b1353d
smilingpoplar <smilingpoplar@gmail.com>
2020-09-18 17:58:25 +0800

    update

---
+++ leetcode/magnetic-force-between-two-balls.cpp
@@ -0,0 +1,52 @@
+//
+//  magnetic-force-between-two-balls
+//  https://leetcode.com/problems/magnetic-force-between-two-balls/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<int>& position, int m) {
+        // 在允许的不同位置放球，使它们尽可能分散
+        // 最小距离dist的取值在[1..position[-1]-1]
+        // 二分搜索猜dist，找满足条件最后一个，修改标准二分搜索写法：
+        // 1. 条件式中去掉等号，2. 最终要找的位置为lo
+        sort(position.begin(), position.end());
+        int lo = 0, hi = position.back();
+        int ans = 0;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (enough(mid, position, m)) {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
+        }
+        return lo; // 修改2
+    }
+    
+    bool enough(int dist, vector<int>& position, int m) {
+        // 两球间的最小距离dist一确定，position[]中能放的球数count也确定
+        // count(dist)是关于dist的递减函数，count(dist)<m符合二分搜索的条件形式[0..0 1 1..]
+        // 用<m而不是<=m，因为要找满足条件的最后一个，条件式中去掉等号
+        const int N = position.size();
+        int lastPos = INT_MIN, count = 0;
+        for (int i = 0; i < N; i++) {            
+            if (position[i] >= lastPos + dist) {
+                count++;
+                lastPos = position[i];
+            }
+        }
+        return count < m; // 修改1
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
---
+++ leetcode/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,60 @@
+//
+//  maximal-square
+//  https://leetcode.com/problems/maximal-square/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        // 要在bloomDay中找出m个k长的子段
+        if (bloomDay.size() < m * k) return -1;
+        // 等待天数wait为各子段最大值的最小值，值范围[min(bloomDay),max(bloomDay)]
+        int mn = INT_MAX, mx = INT_MIN;
+        for (int day : bloomDay) {
+            mn = min(mn, day);
+            mx = max(mx, day);
+        }
+        
+        // 二分搜索猜等待天数
+        int lo = mn, hi = mx;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (enough(mid, bloomDay, m, k)) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+    
+    bool enough(int wait, vector<int>& bloomDay, int m, int k) {
+        // 等待天数wait一确定，bloomDay根据开没开花变成10数组，
+        // 就能统计有多少个k长的连续1的子段，count(wait)是关于wait的递增函数，
+        // count(wait)>=m满足二分搜索的条件形式[0..0 1 1..]
+        int adjacent = 0, count = 0;
+        for (int day : bloomDay) {
+            if (day <= wait) {
+                adjacent++;
+                if (adjacent >= k) {
+                    count++;
+                    adjacent = 0;
+                }
+            } else {
+                adjacent = 0;
+            }
+        }
+        return count >= m;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
