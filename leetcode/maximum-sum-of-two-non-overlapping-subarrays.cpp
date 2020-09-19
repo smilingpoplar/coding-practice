98445ff92d163483d5077c6410a240dc3cb73c6c
smilingpoplar <smilingpoplar@gmail.com>
2020-09-19 20:32:15 +0800

    update

--- leetcode/maximum-sum-of-two-non-overlapping-subarrays.cpp
+++ leetcode/maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -13,6 +13,7 @@ using namespace std;
 class Solution {
 public:
     int maxSumTwoNoOverlap(vector<int>& A, int L, int M) {
+        // 题目：两个不重叠子段，L在M前或M在L前
         const int N = A.size();
         vector<int> presum(N + 1, 0);
         for (int i = 0; i < N; i++) {
@@ -21,8 +22,10 @@ public:
         
         int Lmax = 0, Mmax = 0, ans = 0;
         for (int i = L + M; i <= N; i++) {
+            // L在M前
             Lmax = max(Lmax, presum[i-M] - presum[i-L-M]);
             ans = max(ans, Lmax + (presum[i] - presum[i-M]));
+            // M在L前
             Mmax = max(Mmax, presum[i-L] - presum[i-L-M]);
             ans = max(ans, Mmax + (presum[i] - presum[i-L]));
         }
