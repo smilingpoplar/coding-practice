944ec1c2b302bbce7e699c248066a070c17b73ed
smilingpoplar <smilingpoplar@gmail.com>
2020-09-19 13:39:41 +0800

    update

--- leetcode/k-empty-slots.cpp
+++ leetcode/k-empty-slots.cpp
@@ -13,28 +13,48 @@ using namespace std;
 
 class Solution {
 public:
-    int kEmptySlots(vector<int>& flowers, int k) {
+    int kEmptySlots(vector<int> &flowers, int k) {
         const int N = flowers.size();
-        vector<int> days(N); // days[]是各个位置的花哪天开，flowers[]是各天开的花在哪个位置
-        for (int i = 0; i < N; i++) {
-            days[flowers[i] - 1] = i + 1;
-        }
-        // 要找相隔k的两个位置lo和hi（hi-lo-1==k），使所有位置i（lo<i<hi）都满足
-        // days[i]>days[lo] && days[i]>days[hi]
-        int ans = INT_MAX;
-        for (int lo = 0, hi = lo + k + 1; hi < N; hi = lo + k + 1) {
-            int i = lo + 1;
-            while (days[i] > days[lo] && days[i] > days[hi]) i++;
-            if (i == hi) { // (lo..hi)中间k个位置满足条件
-                int day = max(days[lo], days[hi]);
-                ans = min(ans, day);
+        // 桶排序，每个桶对应k+1个位置，
+        // 这样所找的k个空槽两端有花的情况就不可能出现在桶内，而只能在桶间
+        // 每个桶保留最大最小值
+        unordered_map<int, vector<int>> buckets; // bucketIdx=>[minOfBucket,maxOfBucket]
+        for (int i = 1; i <= N; i++) {
+            int pos = flowers[i-1];
+            int idx = pos / (k + 1);
+            if (buckets.count(idx)) {
+                buckets[idx][0] = min(buckets[idx][0], pos);
+                buckets[idx][1] = max(buckets[idx][1], pos);
+            } else {
+                buckets[idx] = {pos, pos};
             }
-            // 位置i不能再作中间的空slot
-            lo = i;
+            // 查看左右桶边界
+            if (pos == buckets[idx][0] && buckets.count(idx-1) && pos - buckets[idx-1][1] - 1 == k) return i;
+            if (pos == buckets[idx][1] && buckets.count(idx+1) && buckets[idx+1][0] - pos - 1 == k) return i;
         }
-        return ans != INT_MAX ? ans : -1;
+        return -1;
     }
 };
+
+/*
+class Solution {
+public:
+    int kEmptySlots(vector<int> &flowers, int k) {
+        const int N = flowers.size();
+        set<int> st;
+        for (int i = 1; i <= N; i++) {
+            int pos = flowers[i-1];
+            st.insert(pos);
+            // 看前后位置是否相隔k
+            auto it = st.find(pos);
+            if (it != st.begin() && pos - *prev(it) - 1 == k) return i;
+            if (next(it) != st.end() && *next(it) - pos - 1 == k) return i;
+        }
+        return -1;
+    }
+};
+*/
+
 
 int main(int argc, const char * argv[]) {
     return 0;
