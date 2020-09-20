0560abd71255a235b2362657b22acf80ca30413b
smilingpoplar <smilingpoplar@gmail.com>
2020-09-21 00:03:36 +0800

    update

---
+++ leetcode/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,37 @@
+//
+//  divide-array-in-sets-of-k-consecutive-numbers
+//  https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& nums, int k) {
+        map<int, int> cnt; // num=>count
+        for (int num : nums) {
+            cnt[num]++;
+        }
+        
+        for (auto &e : cnt) {
+            int count = e.second;
+            if (count > 0) { // [num..num+W)都减count
+                int num = e.first;
+                for (int i = 0; i < W; i++) {
+                    cnt[num+i] -= count;
+                    if (cnt[num+i] < 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
