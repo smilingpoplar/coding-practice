5adee8c4a87c5843df4a53ff0073a284d6becde1
smilingpoplar <smilingpoplar@gmail.com>
2020-09-20 22:50:52 +0800

    update

---
+++ leetcode/best-sightseeing-pair.cpp
@@ -0,0 +1,30 @@
+//
+//  best-sightseeing-pair
+//  https://leetcode.com/problems/best-sightseeing-pair/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& A) {
+        // 找一对数，使A[i]+A[j]+i-j最大，即(A[i]+i)+(A[j]-j)最大
+        // 从左往右扫描，记住最大的A[i]+i
+        const int N = A.size();
+        int ans = 0, maxI = A[0] + 0;
+        for (int j = 1; j < N; j++) {
+            ans = max(ans, maxI + A[j] - j);
+            maxI = max(maxI, A[j] + j);
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
