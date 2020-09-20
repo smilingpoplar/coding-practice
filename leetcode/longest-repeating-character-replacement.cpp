2ba4a5783934dc6cff95bf9d1ba203f7adf19524
smilingpoplar <smilingpoplar@gmail.com>
2020-09-21 02:44:02 +0800

    update

--- leetcode/longest-repeating-character-replacement.cpp
+++ leetcode/longest-repeating-character-replacement.cpp
@@ -14,19 +14,19 @@ using namespace std;
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        const int N = s.size();
         unordered_map<char, int> count;
-        int maxCnt = 0;
-        int lo, hi;
-        for (lo = 0, hi = 0; hi < s.size(); hi++) {
+        int maxCnt = 0, lo = 0;
+        for (int hi = 0; hi < N; hi++) {
             // 每一步都尝试将窗口长度推到极限 maxCnt+k
-            // 不收缩的滑动窗口，窗口不符合条件时整体右移一步
+            // 不收缩滑动窗口，窗口不符合条件时整体右移一步
             maxCnt = max(maxCnt, ++count[s[hi]]);
             if (hi - lo + 1 > maxCnt + k) {
                 count[s[lo]]--;
                 lo++;
             }
         }
-        return hi - lo;
+        return N - lo;
     }
 };
 
