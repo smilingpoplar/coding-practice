d8ba6b027598e542e3f6ed050c12738d9fcf1325
smilingpoplar <smilingpoplar@gmail.com>
2020-09-20 23:40:44 +0800

    update

---
+++ leetcode/maximum-width-ramp.cpp
@@ -0,0 +1,38 @@
+//
+//  maximum-width-ramp
+//  https://leetcode.com/problems/maximum-width-ramp/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& A) {
+        // 找相距最远的两个递增数
+        const int N = A.size();
+        stack<int> stk; // 栈底固定为A[0]的递减栈
+        for (int i = 0; i < N; i++) {
+            if (stk.empty() || A[i] < A[stk.top()]) {
+                stk.push(i);
+            }
+        }
+        // 在从右往左A[j]与栈顶比较
+        int ans = 0;
+        for (int j = N - 1; j >= 0; j--) {
+            while (!stk.empty() && A[j] >= A[stk.top()]) {
+                ans = max(ans, j - stk.top());
+                stk.pop();
+            }
+        }
+        return ans;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
