5376382d48a4913c3b71a01135860cb97372c3fe
smilingpoplar <smilingpoplar@gmail.com>
2020-09-20 19:25:56 +0800

    update

---
+++ leetcode/least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,38 @@
+//
+//  least-number-of-unique-integers-after-k-removals
+//  https://leetcode.com/problems/least-number-of-unique-integers-after-k-removals/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        unordered_map<int, int> cnt; // num=>count
+        for (int num : arr) {
+            cnt[num]++;
+        }
+        // 按照个数升序排
+        auto cmp = [&cnt](int a, int b) {
+            return cnt[a] > cnt[b];
+        };
+        priority_queue<int, vector<int>, decltype(cmp)> pq(cmp);
+        for (auto &e : cnt) {
+            pq.push(e.first);
+        }
+        while (k > 0) {
+            k -= cnt[pq.top()];
+            if (k >= 0) pq.pop();
+        }
+        return pq.size();
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
