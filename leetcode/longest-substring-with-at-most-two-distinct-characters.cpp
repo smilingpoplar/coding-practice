3f9e8106bcbca2f5dbc5f89e9076a32ea29355e4
smilingpoplar <smilingpoplar@gmail.com>
2020-09-21 10:49:12 +0800

    update

--- leetcode/longest-substring-with-at-most-two-distinct-characters.cpp
+++ leetcode/longest-substring-with-at-most-two-distinct-characters.cpp
@@ -14,8 +14,8 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
+        int k = 2; // at most 2
         unordered_map<int, int> cnt; // char=>count
-        int k = 2;
         int ans = 0;
         for (int hi = 0, lo = 0; hi < s.size(); hi++) {
             if (cnt[s[hi]]++ == 0) k--;
