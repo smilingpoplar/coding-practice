e89490f1508b95ecad3c1e439b26d31dacaecd68
smilingpoplar <smilingpoplar@gmail.com>
2020-09-22 00:26:19 +0800

    update

---
+++ leetcode/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,29 @@
+//
+//  check-if-n-and-its-double-exist
+//  https://leetcode.com/problems/check-if-n-and-its-double-exist/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> st;
+        for (int a : arr) {
+            if (st.count(a * 2) 
+                || (a % 2 == 0 && st.count(a / 2)))
+                return true;
+            st.insert(a);
+        }
+        return false;
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
---
+++ leetcode/duplicate-zeros.cpp
@@ -0,0 +1,40 @@
+//
+//  duplicate-zeros
+//  https://leetcode.com/problems/duplicate-zeros/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int zeroCnt = 0;
+        for (int a : arr) {
+            if (a == 0) zeroCnt++;
+        }
+        
+        // 虚拟扩展位置到j=N-1+zeroCnt
+        const int N = arr.size();
+        int j = N - 1 + zeroCnt;
+        for (int i = N - 1; i >= 0; i--) {
+            if (arr[i]) {
+                if (j < N) arr[j] = arr[i];
+                j--;
+            } else {
+                if (j < N) arr[j] = 0;
+                j--;
+                if (j < N) arr[j] = 0;
+                j--;
+            }
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
