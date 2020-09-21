17ee67cd6df471c3b09874e21cbf3f2f4ecd4a7a
smilingpoplar <smilingpoplar@gmail.com>
2020-09-22 01:55:04 +0800

    update

--- leetcode/sum-of-subsequence-widths.cpp
+++ leetcode/sum-of-subsequence-widths.cpp
@@ -14,11 +14,10 @@ using namespace std;
 class Solution {
 public:
     int sumSubseqWidths(vector<int>& A) {
-        // 任意A[i]，当它作为子序列最小值时贡献-A[i]，作为子序列最大值时贡献A[i]。排序数组后，
-        // 比A[i]大的数有N-1-i个，A[i]作最小值的子序列有2^(N-1-i)个，对ans贡献2^(N-1-i)*(-A[i])；
-        // 比A[i]小的数有i个，A[i]作最大值的子序列有2^i个，对ans贡献2^i*A[i]；
-        // 所以，A[i]对ans贡献 A[i]*(2^i - 2^(N-1-i))
-        // 1<=N<=20000，2^20000=10^6000，会溢出，要取模
+        // 子序列计算和，与顺序无关，排序数组
+        // 比A[i]大的数有N-1-i个，A[i]作最小值的子序列有2^(N-1-i)个，A[i]对ans贡献2^(N-1-i)*(-A[i])
+        // 比A[i]小的数有i个，A[i]作最大值的子序列有2^i个，对ans贡献2^i*A[i]
+        // 所以，A[i]对ans贡献 A[i] * (2^i - 2^(N-1-i))
         sort(A.begin(), A.end());
         const int N = A.size(), MOD = 1e9 + 7;
         vector<int> pow2(N, 1);
