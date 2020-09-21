a6fdfbb0095c150d8242e0bec3a3dbf699a24a83
smilingpoplar <smilingpoplar@gmail.com>
2020-09-21 13:41:07 +0800

    update

--- leetcode/maximum-width-ramp.cpp
+++ leetcode/maximum-width-ramp.cpp
@@ -53,6 +53,27 @@ int maxWidthRamp(vector<int>& A) {
 }
 */
 
+/*
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& A) {
+        const int N = A.size();
+        multimap<int, int> mp; // num=>idx
+        for (int i = 0; i < N; i++) {
+            mp.insert({A[i], i});
+        }
+        
+        int minIdx = INT_MAX, ans = 0;
+        for (auto &e : mp) { // 按值递增遍历
+            int idx = e.second;
+            ans = max(ans, idx - minIdx);
+            minIdx = min(minIdx, idx);
+        }
+        return ans;
+    }
+};
+*/
+
 int main(int argc, const char * argv[]) {    
     return 0;
 }
