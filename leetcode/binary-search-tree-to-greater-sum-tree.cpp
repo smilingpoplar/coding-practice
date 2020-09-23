c6d997d77b058bb6b6df98f269a0996e7636e048
smilingpoplar <smilingpoplar@gmail.com>
2020-09-23 14:52:38 +0800

    update

---
+++ leetcode/binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,43 @@
+//
+//  binary-search-tree-to-greater-sum-tree
+//  https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+//
+//  Created by smilingpoplar on 18/01/19.
+//  Copyright (c) 2015年 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        int sum = 0;
+        reverseInorder(root, sum);
+        return root;
+    }
+    
+    void reverseInorder(TreeNode *root, int &sum) {
+        if (!root) return;
+        reverseInorder(root->right, sum);
+        sum += root->val;
+        root->val = sum;
+        reverseInorder(root->left, sum);
+    }
+};
+
+int main(int argc, const char * argv[]) {    
+    return 0;
+}
