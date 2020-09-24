bc0c6712d2da5647236c4b39aa837ad9915b6614
smilingpoplar <smilingpoplar@gmail.com>
2020-09-24 13:03:16 +0800

    update

--- leetcode/find-elements-in-a-contaminated-binary-tree.cpp
+++ leetcode/find-elements-in-a-contaminated-binary-tree.cpp
@@ -11,22 +11,38 @@
 using namespace std;
 
 class FindElements {
-    unordered_set<int> st;
+    TreeNode* _root;
 public:
     FindElements(TreeNode* root) {
-        dfs(root, 0);
+        _root = root;
+        recover(root, 0);
     }
     
-    void dfs(TreeNode *root, int val) {
+    void recover(TreeNode *root, int val) {
         if (!root) return;
         root->val = val;
-        st.insert(val);
-        dfs(root->left, val * 2 + 1);
-        dfs(root->right, val * 2 + 2);
+        recover(root->left, val * 2 + 1);
+        recover(root->right, val * 2 + 2);
     }
     
     bool find(int target) {
-        return st.count(target);
+        // 反向搜索的路径
+        stack<int> stk;
+        while (target) {
+            stk.push(target);
+            target = (target - 1) / 2;
+        }
+        stk.push(0);
+        
+        return find(_root, stk);
+    }
+    
+    bool find(TreeNode *root, stack<int> &stk) {
+        if (stk.empty()) return true;
+        if (!root) return false;
+        if (root->val != stk.top()) return false;
+        stk.pop();
+        return find(root->left, stk) || find(root->right, stk);
     }
 };
 
