c3dd621cfdf7245ca053706250ce38cef09fd158
smilingpoplar <smilingpoplar@gmail.com>
2020-09-26 01:29:52 +0800

    update

--- leetcode/min-cost-to-connect-all-points.cpp
+++ leetcode/min-cost-to-connect-all-points.cpp
@@ -13,6 +13,7 @@ class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
         // 最小生成树的Kruskal算法，并查集+优先队列
+        // 完全连接E=O(N^2)，所以O(ElgE)=O(N^2*2N)=O(N^3)
         const int N = points.size();
         vector<int> uf(N); 
         for (int i = 0; i < N; i++) {
@@ -26,7 +27,7 @@ public:
         priority_queue<arr3, vector<arr3>, decltype(cmp)> pq(cmp);
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
-                int cost = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                int cost = distance(i, j, points);
                 pq.push({cost, i, j});
             }
         }
@@ -49,7 +50,56 @@ public:
         if (uf[x] != x) uf[x] = find(uf[x], uf);
         return uf[x];
     }
+
+    int distance(int u, int v, vector<vector<int>> &points) {
+        auto &pu = points[u], &pv = points[v];
+        return abs(pu[0] - pv[0]) + abs(pu[1] - pv[1]);
+    }
 };
+
+/*
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        // 最小生成树的Prim算法，图遍历+优先队列
+        // O(N^2)
+        const int N = points.size();
+        vector<int> dist(N, INT_MAX); // 各节点到最小生成树的距离
+        dist[0] = 0;
+
+        using arr2 = array<int, 2>; // [dist, idx]
+        auto cmp = [](arr2 &a, arr2 &b) {
+            return a[0] > b[0]; // 最小堆
+        };
+        priority_queue<arr2, vector<arr2>, decltype(cmp)> pq(cmp);
+        pq.push({0, 0});
+
+        int ans = 0;
+        vector<int> visited(N, false);
+        while (!pq.empty()) {
+            auto [d, u] = pq.top();  pq.pop();
+            if (visited[u]) continue;
+            visited[u] = true;
+            ans += d;
+
+            for (int v = 0; v < N; v++) {
+                if (visited[v]) continue;
+                int newdist = distance(u, v, points);
+                if (newdist < dist[v]) {
+                    dist[v] = newdist;
+                    pq.push({newdist, v});
+                }
+            }
+        }
+        return ans;
+    }
+
+    int distance(int u, int v, vector<vector<int>> &points) {
+        auto &pu = points[u], &pv = points[v];
+        return abs(pu[0] - pv[0]) + abs(pu[1] - pv[1]);
+    }
+};
+*/
 
 int main(int argc, const char * argv[]) {    
     return 0;
