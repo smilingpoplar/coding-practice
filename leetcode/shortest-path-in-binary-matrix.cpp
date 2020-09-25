a45baa05a2ded359b77c30dd5287a147c0e0376f
smilingpoplar <smilingpoplar@gmail.com>
2020-09-26 02:50:33 +0800

    update

--- leetcode/shortest-path-in-binary-matrix.cpp
+++ leetcode/shortest-path-in-binary-matrix.cpp
@@ -13,17 +13,16 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         // 从左上角到右下角的最短路径，bfs
-        if (grid.empty()) return 0;
         if (grid[0][0] == 1) return -1;
         const int R = grid.size(), C = grid[0].size();
         vector<vector<bool>> visited(R, vector<bool>(C, false));
-        queue<vector<int>> q;
-        q.push({0, 0, 1});
+        using arr3 = array<int, 3>; // [dist, i, j]
+        queue<arr3> q;
+        q.push({1, 0, 0});
         visited[0][0] = true;
 
         while (!q.empty()) {
-            auto info = q.front();  q.pop();
-            int r = info[0], c = info[1], d = info[2];
+            auto [d, r, c] = q.front();  q.pop();
             if (r == R - 1 && c == C - 1) return d;
             
             for (int dr = -1; dr <= 1; dr++) {
@@ -33,7 +32,7 @@ public:
                     int nr = r + dr, nc = c + dc;
                     if (nr < 0 || nr >= R || nc < 0 || nc >= C 
                         || grid[nr][nc] || visited[nr][nc]) continue;
-                    q.push({nr, nc, d + 1});
+                    q.push({d + 1, nr, nc});
                     visited[nr][nc] = true;
                 }
             }
