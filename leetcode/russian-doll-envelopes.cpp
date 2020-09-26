a8459ccf50f0761d0e9751a575ff047f252a5fa2
smilingpoplar <smilingpoplar@gmail.com>
2020-09-26 19:57:26 +0800

    update

--- leetcode/russian-doll-envelopes.cpp
+++ leetcode/russian-doll-envelopes.cpp
@@ -9,40 +9,19 @@
 
 using namespace std;
 
-/*
 class Solution {
 public:
-    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
-        // 设dp[i]表示envelopes[0..i]的最长递增序列长
-        sort(envelopes.begin(), envelopes.end());
-        vector<int> dp(envelopes.size(), 1);
-        int ans = 0;
-        for (int i = 0; i < envelopes.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (envelopes[i].first > envelopes[j].first &&
-                   envelopes[i].second > envelopes[j].second) {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
-            }
-            ans = max(ans, dp[i]);
-        }
-        return ans;
-    }
-};
-*/
-
-class Solution {
-public:
-    int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
         // 按width排序，width相等时按照height递减排序，然后根据height找最长递增子序列，
         // 能避免选中[3,4],[3,5]这种width相等的不该选情况。
-        sort(envelopes.begin(), envelopes.end(), [](pair<int, int> &a, pair<int, int> &b) {
-            return a.first < b.first || (a.first == b.first && a.second > b.second);
+        sort(envelopes.begin(), envelopes.end(), 
+             [](vector<int>& a, vector<int>& b) {
+                return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
         });
-        // O(nlgn)根据height找LIS
+        // 根据height找LIS，O(nlgn)
         vector<int> tails; // 各长度LIS的最小尾元素
         for (auto &e : envelopes) {
-            int height = e.second;
+            int height = e[1];
             auto it = lower_bound(tails.begin(), tails.end(), height);
             if (it != tails.end()) *it = height;
             else tails.push_back(height);
