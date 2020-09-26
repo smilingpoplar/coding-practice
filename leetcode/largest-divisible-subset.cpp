f66557b7a7146d33e84624955b62a34a8bfdb138
smilingpoplar <smilingpoplar@gmail.com>
2020-09-26 20:05:53 +0800

    update

--- leetcode/largest-divisible-subset.cpp
+++ leetcode/largest-divisible-subset.cpp
@@ -12,18 +12,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        // 先排序数组。子集扩展到nums[i]的条件是 nums[i]能整除子集的所有值。
-        // 因为子集末尾的最大值nums[j]能整除子集所有其他值，
-        // 所以nums[i]只需整除nums[j]就能扩展该子集。
+        // 先排序数组。nums[i]能整除某子集的所有值 <=> nums[i]能整除该子集最大值
+        // 设dp[i]表示以nums[i]结尾的nums[0..i]的最大可整除子集长，
+        // 若 nums[i] % nums[j] == 0，j < i，则 dp[i] = max(dp[j]+1)
         sort(nums.begin(), nums.end());
         const int N = nums.size();
-        // 设dp[i]表示以nums[i]结尾的nums[0..i]的最大可整除子集长
         vector<int> dp(N, 1);
-        vector<int> prev(N, -1); // 记录序列
+        vector<int> prev(N, -1);
         int maxlen = 0, maxidx = -1;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0) { // nums[i]能扩展以nums[j]结尾的子集
+                if (nums[i] % nums[j] == 0) { // 能扩展
                     if (dp[j] + 1 > dp[i]) {
                         dp[i] = dp[j] + 1;
                         prev[i] = j;
