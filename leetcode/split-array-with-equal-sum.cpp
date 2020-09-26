04df373ddeff9b655b4b0cf4f7941f6b2c3d9acf
smilingpoplar <smilingpoplar@gmail.com>
2020-09-26 17:22:02 +0800

    update

--- leetcode/split-array-with-equal-sum.cpp
+++ leetcode/split-array-with-equal-sum.cpp
@@ -16,26 +16,26 @@ public:
     bool splitArray(vector<int>& nums) {
         // 题目：除掉i、j、k，数组分成非空的四段，且这四段的和相等        
         const int N = nums.size();
-        vector<int> sum(N, 0); // 用累加数组
-        sum[0] = nums[0];
-        for (int i = 1; i < N; i++)
-            sum[i] = sum[i-1] + nums[i];
+        vector<int> sum(N + 1, 0); // 用累加数组
+        for (int i = 0; i < N; i++) {
+            sum[i+1] = sum[i] + nums[i];
+        }
 
         // 遍历j的各个取值
         for (int j = 3; j <= N - 4; j++) {
-            // j之前存在不同分法的相等小段，用个set记录，而不是单个int。
+            // j之前存在不同分法的相等小段和，用个set记录这些和。
             // 比如`3,2,1,-1,6`，i分在1时两边小段和为5，分在-1时两边小段和为6。
             unordered_set<int> subSums;
             for (int i = 1; i <= j - 2; i++) {
-                if (sum[i-1] == sum[j-1] - sum[i]) {
-                    subSums.insert(sum[i-1]);
+                if (sum[i] == sum[j] - sum[i+1]) {
+                    subSums.insert(sum[i]);
                 }
             }
             if (subSums.empty()) continue;
             // j之后存在与前面相等的小段
             for (int k = j + 2; k <= N - 2; k++) {
-                int subSum = sum[k-1] - sum[j];
-                if (subSum == sum[N-1] - sum[k] && subSums.count(subSum)) 
+                int subSum = sum[k] - sum[j+1];
+                if (subSum == sum[N] - sum[k+1] && subSums.count(subSum)) 
                     return true;
             }
         }
