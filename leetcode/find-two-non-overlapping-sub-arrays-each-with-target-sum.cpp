f1ef384c05e3b139afdfe452895933025244340a
smilingpoplar <smilingpoplar@gmail.com>
2020-09-27 00:16:31 +0800

    update

--- leetcode/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ leetcode/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -11,34 +11,36 @@ using namespace std;
 
 class Solution {
 public:
+    const int INF = 1e9; 
     int minSumOfLengths(vector<int>& arr, int target) {
         // 全为正数，可用滑动数组
-        // 先从左往右扫描，再从右往左扫描
-        const int N = arr.size(), INF = 1e9;
-        vector<int> left(N);
-        for (int hi = 0, lo = 0, sum = 0, leftLen = INF; hi < N - 1; hi++) {
+        auto left = minLens(arr, target, false);
+        auto right = minLens(arr, target, true);
+        int ans = INF;
+        for (int i = 1; i < arr.size(); i++) {
+            ans = min(ans, left[i-1] + right[i]);
+        }
+        return ans == INF ? -1 : ans;
+    }
+
+    vector<int> minLens(vector<int> arr, int target, bool fromRight) {
+        // ans[i]表示arr[0..i]的、和为target的、最小长度
+        if (fromRight) reverse(arr.begin(), arr.end());
+        const int N = arr.size();
+        vector<int> ans(N);
+        int sum = 0, leftMin = INF;
+        for (int hi = 0, lo = 0; hi < N - 1; hi++) {
             sum += arr[hi];
             while (sum > target) {
                 sum -= arr[lo++];
             }
             if (sum == target) {
-                leftLen = min(leftLen, hi - lo + 1);
+                leftMin = min(leftMin, hi - lo + 1);
             }
-            left[hi] = leftLen;
+            ans[hi] = leftMin;
         }
-
-        int ans = INF;
-        for (int lo = N - 1, hi = N - 1, sum = 0, rightLen = INF; lo >= 1; lo--) {
-            sum += arr[lo];
-            while (sum > target) {
-                sum -= arr[hi--];
-            }
-            if (sum == target) {
-                rightLen = min(rightLen, hi - lo + 1);
-                ans = min(ans, left[lo-1] + rightLen);
-            }
-        }
-        return ans == INF ? -1 : ans;
+        if (fromRight) reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
 
