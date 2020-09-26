a0bb406bd6571a99990d9c74ea21956d2fbe3ed3
smilingpoplar <smilingpoplar@gmail.com>
2020-09-27 01:30:17 +0800

    update

--- leetcode/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
+++ leetcode/find-two-non-overlapping-sub-arrays-each-with-target-sum.cpp
@@ -11,36 +11,29 @@ using namespace std;
 
 class Solution {
 public:
-    const int INF = 1e9; 
     int minSumOfLengths(vector<int>& arr, int target) {
         // 全为正数，可用滑动数组
-        auto left = minLens(arr, target, false);
-        auto right = minLens(arr, target, true);
-        int ans = INF;
-        for (int i = 1; i < arr.size(); i++) {
-            ans = min(ans, left[i-1] + right[i]);
-        }
-        return ans == INF ? -1 : ans;
-    }
-
-    vector<int> minLens(vector<int> arr, int target, bool fromRight) {
-        // ans[i]表示arr[0..i]的、和为target的、最小长度
-        if (fromRight) reverse(arr.begin(), arr.end());
-        const int N = arr.size();
-        vector<int> ans(N);
-        int sum = 0, leftMin = INF;
-        for (int hi = 0, lo = 0; hi < N - 1; hi++) {
+        // 设dp[i]表示arr[0..i]的、和为target的、最小长度
+        // 若找到有效窗口[lo..hi]，dp[hi]=min(hi-lo+1,dp[hi-1])
+        // 若没找到，dp[hi]=dp[hi-1]
+        const int N = arr.size(), INF = 1e9;
+        vector<int> dp(N, INF);
+        int sum = 0, ans = INF;
+        for (int hi = 0, lo = 0; hi < N; hi++) {
             sum += arr[hi];
             while (sum > target) {
                 sum -= arr[lo++];
             }
+
             if (sum == target) {
-                leftMin = min(leftMin, hi - lo + 1);
+                int len = hi - lo + 1;
+                dp[hi] = len;
+                // 能求到两最小长度的和
+                if (lo > 0) ans = min(ans, len + dp[lo-1]);
             }
-            ans[hi] = leftMin;
+            if (hi > 0) dp[hi] = min(dp[hi], dp[hi-1]);
         }
-        if (fromRight) reverse(ans.begin(), ans.end());
-        return ans;
+        return ans == INF ? -1 : ans;
     }
 };
 
