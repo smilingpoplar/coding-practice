164324ab658aee966b85c5a7cc2cacab311a1916
smilingpoplar <smilingpoplar@gmail.com>
2020-09-28 02:16:32 +0800

    update

--- leetcode/count-different-palindromic-subsequences.cpp
+++ leetcode/count-different-palindromic-subsequences.cpp
@@ -20,27 +20,27 @@ public:
         // 还可得内层子问题：S[firstIdx(b)+1..lastIdx(b)-1]]。
         // 不同字母的子问题不重叠，count可相加。
         const int N = S.size();
-        vector<set<int>> idxSet(CHAR_CNT); // char => index_set
+        vector<set<int>> idxSets(CHAR_CNT); // char=>idxSet
         for (int i = 0; i < N; i++) {
-            idxSet[S[i] - 'a'].insert(i);
+            idxSets[S[i] - 'a'].insert(i);
         }
 
         vector<vector<int>> memo(N, vector<int>(N, -1)); // S[lo..hi] => count
-        return rCount(0, N - 1, idxSet, memo);
+        return rCount(0, N - 1, idxSets, memo);
     }
     
-    int rCount(int lo, int hi, vector<set<int>> &idxSet, vector<vector<int>> &memo) {
+    int rCount(int lo, int hi, vector<set<int>> &idxSets, vector<vector<int>> &memo) {
         if (lo > hi) return 0;
         if (memo[lo][hi] != -1) return memo[lo][hi];
         
         long ans = 0;
         for (int i = 0; i < CHAR_CNT; i++) {
-            auto itLo = idxSet[i].lower_bound(lo);
-            if (itLo == idxSet[i].end() || *itLo > hi) continue;
-            auto itHi = idxSet[i].upper_bound(hi); 
+            auto itLo = idxSets[i].lower_bound(lo);
+            if (itLo == idxSets[i].end() || *itLo > hi) continue;
+            auto itHi = idxSets[i].upper_bound(hi); 
             itHi--; // *itLo<=hi，<=hi非空，可itHi--
             ans += (*itLo == *itHi) ? 1 : 2; // 最外层贡献c、cc
-            ans += rCount(*itLo + 1, *itHi - 1, idxSet, memo); // 剥掉最外层c..c、内层子问题
+            ans += rCount(*itLo + 1, *itHi - 1, idxSets, memo); // 剥掉最外层c..c、内层子问题
         }
         memo[lo][hi] = ans % MOD;
         return memo[lo][hi];
