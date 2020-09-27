4e5b06cd6c770ea49d85b29abff331ba5eff8b99
smilingpoplar <smilingpoplar@gmail.com>
2020-09-27 16:31:54 +0800

    update

--- leetcode/number-of-dice-rolls-with-target-sum.cpp
+++ leetcode/number-of-dice-rolls-with-target-sum.cpp
@@ -11,24 +11,23 @@ using namespace std;
 
 class Solution {
 public:
-    int numRollsToTarget(int d, int f, int target) {
-        // 取d次数，每次在[1..f]，总和为target
-        // 设dp[i][j]表示取i次数、总和为j的子问题解
-        // dp[i][j] += dp[i-1][j-k], k为[1..f]。
-        // 初始dp[0][0]=1
-        // 第i项只依赖i-1项，省掉第i项，i仍从左往右遍历；
-        // 初始值dp[0][0]=1与后续迭代dp[>0][0]=0冲突，要使用临时变量ndp
-        // ndp[j] += ndp[j-k]，k为[1..f]，j遍历顺序任意
-        // 01背包，分组
+    int numRollsToTarget(int D, int F, int target) {
+        // 题目：取D次数，每次在[1..F]，总和为target
+        // 设dp[d][t]表示取d次数、总和为t的子问题解
+        // dp[d][t] = sum( dp[d-1][t-f] ), f为[1..F]
+        // 初始dp[0][0]=1，dp[>0][0]=0，dp[0][>0]=0
+        // 第d项只依赖d-1项，省掉d这维，d仍从左往右遍历，用临时变量ndp[]
+        // ndp[t] = sum( dp[t-f] )
+        // 带分组的01背包问题
         const int MOD = 1e9 + 7;
-        vector<int> dp(target + 1, 0);
-        dp[0] = 1;
-        
-        for (int i = 0; i < d; i++) {
-            vector<int> ndp(target + 1, 0);
-            for (int k = 1; k <= f; k++) {
-                for (int j = k; j <= target; j++) {
-                    ndp[j] = (ndp[j] + dp[j-k]) % MOD;
+        vector<int> dp(target + 1);
+        dp[0] = 1; // dp[0][0]=0
+
+        for (int d = 1; d <= D; d++) {
+            vector<int> ndp(target + 1); // dp[>0][0]=0
+            for (int t = 1; t <= target; t++) {
+                for (int f = 1; f <= F; f++) {
+                    if (t - f >= 0) ndp[t] = (ndp[t] + dp[t-f]) % MOD;
                 }
             }
             swap(dp, ndp);
