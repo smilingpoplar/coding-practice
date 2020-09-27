983fb228b5d3ba5ce1b5736e8275042f1631f02d
smilingpoplar <smilingpoplar@gmail.com>
2020-09-28 01:06:50 +0800

    update

--- leetcode/longest-palindromic-substring.cpp
+++ leetcode/longest-palindromic-substring.cpp
@@ -28,10 +28,10 @@ public:
                 if (i + 1 <= j - 1) dp[j] = dp[j] && dp[j-1];
 
                 if (dp[j]) {
-                    int length = j - i + 1;
-                    if (length > longest) {
-                        longest = length;
-                        palindrome = s.substr(i, length);
+                    int len = j - i + 1;
+                    if (len > longest) {
+                        longest = len;
+                        palindrome = s.substr(i, len);
                     }
                 }
             }
@@ -45,17 +45,17 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         // 从可能的中心往外扩展找回文
-        int longest = 0, start = 0;
+        int longest = 0, lo = 0;
         for (int i = 0; i < s.size(); i++) {
             int len1 = expand(s, i, i);
             int len2 = expand(s, i, i + 1);
             int len = max(len1, len2);
             if (len > longest) {
                 longest = len;
-                start = i - (len - 1) / 2;
+                lo = i - (len - 1) / 2;
             }
         }
-        return s.substr(start, longest);
+        return s.substr(lo, longest);
     }
     
     int expand(const string &s, int l, int r) {
