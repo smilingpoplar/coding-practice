1d96e7a1370fd259ae462220247da57118b7a3b5
smilingpoplar <smilingpoplar@gmail.com>
2020-09-29 03:11:22 +0800

    update

--- leetcode/knight-dialer.cpp
+++ leetcode/knight-dialer.cpp
@@ -19,23 +19,21 @@ public:
             {4, 6}, {6, 8}, {7, 9}, {4, 8}, {0, 3, 9},
             {}, {0, 1, 7}, {2, 6}, {1, 3}, {2, 4}
         };
-        // cnt[]记录以各数字结尾的拨号个数
-        vector<int> cnt(10, 1);
+        // 动态规划，设dp[i][d]表示i个数以数字d结尾的号码数，
+        // dp[i][dst] = sum(dp[i-1][src])
+        // i只依赖前一项，省掉i这维，ndp[dst] = sum(dp[src])
+        vector<long> dp(10, 1);
         for (int i = 1; i < N; i++) { // 跳N-1次
-            vector<int> ncnt(10, 0);
+            vector<long> ndp(10, 0);
             for (int d = 0; d < 10; d++) {
                 for (int nd : jump[d]) {
-                    ncnt[nd] = (ncnt[nd] + cnt[d]) % MOD;
+                    ndp[nd] += dp[d] % MOD;
                 }
             }
-            swap(ncnt, cnt);
+            swap(ndp, dp);
         }
         
-        int ans = 0;
-        for (int x : cnt) {
-            ans = (ans + x) % MOD;
-        }
-        return ans;
+        return accumulate(begin(dp), end(dp), 0L) % MOD;
     }
 };
 
