e3b1fdef16bce9d5b3e69a3ad49879ce19d31199
smilingpoplar <smilingpoplar@gmail.com>
2020-09-29 13:52:10 +0800

    update

--- leetcode/knight-probability-in-chessboard.cpp
+++ leetcode/knight-probability-in-chessboard.cpp
@@ -15,8 +15,8 @@ public:
         // 设prob[i][j][k]表示马在第i行j列、已跳k步的概率，
         // prob[i][j][k] = 来自8个位置的概率prob[i-dr][j-dc][k-1]的和
         // 递推式在k维只依赖k-1项，可省掉k维，k仍从左往右遍历
-        const vector<int> dr = { -2, -1, 1, 2,  2,  1, -1, -2 };
-        const vector<int> dc = {  1,  2, 2, 1, -1, -2, -2, -1 };
+        const vector<array<int, 2>> d = { {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, 
+                                          {2, -1}, {1, -2}, {-1, -2}, {-2, -1} };
         vector<vector<double>> prob(N, vector<double>(N, 0));
         prob[r][c] = 1;
         
@@ -25,8 +25,8 @@ public:
             vector<vector<double>> nprob(N, vector<double>(N, 0));
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    for (int m = 0; m < dr.size(); m++) {
-                        int pi = i - dr[m], pj = j - dc[m];
+                    for (int m = 0; m < d.size(); m++) {
+                        int pi = i - d[m][0], pj = j - d[m][1];
                         if (0 <= pi && pi < N && 0 <= pj && pj < N) {
                             nprob[i][j] += prob[pi][pj] / 8;
                         }
