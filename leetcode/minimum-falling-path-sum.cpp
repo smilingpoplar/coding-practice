d3fd37b8d0ede64d5609120d7e125921aa0aa8e0
smilingpoplar <smilingpoplar@gmail.com>
2020-09-30 15:59:10 +0800

    update

--- leetcode/minimum-falling-path-sum.cpp
+++ leetcode/minimum-falling-path-sum.cpp
@@ -12,23 +12,15 @@ using namespace std;
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& A) {
-        // 设dp[r][c]表示从第1行下降到A[r][c]时的最小和
-        // dp[r][c] = A[r][c] + min(dp[r-1][c-1], dp[r-1][c], dp[r-1][c+1])
-        // 初始 dp[-1][c] = 0
-        // 省掉第r维，r从左往右遍历，用临时变量ndp
-        // ndp[c] = A[r][c] + min(dp[c-1], dp[c], dp[c+1])
         const int R = A.size(), C = A[0].size();
-        vector<int> dp(C); // r==-1
-        for (int r = 0; r < R; r++) {
-            vector<int> ndp(C);
+        for (int r = 1; r < R; r++) {
             for (int c = 0; c < C; c++) {
-                ndp[c] = A[r][c] + min({c-1 >= 0 ? dp[c-1] : INT_MAX, 
-                                        dp[c], 
-                                        c+1 < C ? dp[c+1] : INT_MAX});
+                A[r][c] = A[r][c] + min({c-1 >= 0 ? A[r-1][c-1] : INT_MAX, 
+                                        A[r-1][c], 
+                                        c+1 < C ? A[r-1][c+1] : INT_MAX});
             }
-            swap(dp, ndp);
         }
-        return *min_element(begin(dp), end(dp));
+        return *min_element(begin(A[C-1]), end(A[C-1]));
     }
 };
 
