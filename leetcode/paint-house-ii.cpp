b563e23d5528885bdeaa5c40aab570bd4ced37ce
smilingpoplar <smilingpoplar@gmail.com>
2020-09-30 12:06:57 +0800

    update

--- leetcode/paint-house-ii.cpp
+++ leetcode/paint-house-ii.cpp
@@ -13,13 +13,12 @@ using namespace std;
 class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
-        // dp的维度对应dfs的递归参数
         // 设dp[i][c]表示前[0..i]房子、第i房颜色为c时的minCost
         // dp[i][c] = min{ dp[i-1][not_c] } + costs[i][c]，初始dp[0][c] = costs[0][c]
-        // 而求min{ dp[i-1][not_c] }，只要维护dp[i-1][]对应的最小min1、第i-1房的颜色min1c、以及第二小min2，
+        // 而求min{ dp[i-1][not_c] }，只要维护dp[i-1][]的最小min1、取min1时的颜色min1c、以及第二小min2
         // 这样 min{ dp[i-1][not_c] } = (c != min1c ? min1 : min2)
         // dp[i][c] = (c != min1c ? min1 : min2) + costs[i][c]
-        // 所求为dp[N-1][]中的最小min1
+        // 所求为dp[N-1][]的最小min1
         if (costs.empty()) return 0;
         const int N = costs.size(), K = costs[0].size();
         int min1 = 0, min1c = -1, min2 = 0;
--- leetcode/paint-house.cpp
+++ leetcode/paint-house.cpp
@@ -13,9 +13,12 @@ using namespace std;
 class Solution {
 public:
     int minCost(vector<vector<int>>& costs) {
-        // 设dp[i][c]表示前[0..i]房子、第i房颜色为c时的最小代价，有dp[i][c]=min(dp[i-1][not_c])+costs[i][c]
-        // 递推式在i维上只依赖i-1项，省掉i这维，i仍从左往右遍历。c这维的依赖方向不确定，要用临时变量ndp[]。
-        // ndp[c]=min(dp[not_c])+costs[i][c]。初始dp[c]=costs[0][c]。
+        // 设dp[i][c]表示前[0..i]房子、第i房颜色为c时的最小代价
+        // 有dp[i][c]=min(dp[i-1][not_c])+costs[i][c]
+        // 初始dp[0][c]=costs[0][c]
+        // 递推式在i维上只依赖i-1项，省掉i这维，i仍从左往右遍历
+        // c这维的依赖方向不确定，要用临时变量ndp[]
+        // ndp[c]=min(dp[not_c])+costs[i][c]
         if (costs.empty()) return 0;
         const int N = costs.size(), C = costs[0].size();
         vector<int> dp = costs[0];
@@ -24,9 +27,9 @@ public:
             for (int c = 0; c < C; c++) {
                 ndp[c] = min(dp[(c+1) % C], dp[(c+2) % C]) + costs[i][c];
             }
-            dp.swap(ndp);
+            swap(dp, ndp);
         }
-        return *min_element(dp.begin(), dp.end());
+        return min({dp[0], dp[1],dp[2]});
     }
 };
 
