033fbd4ec6d3dabda49b54018ef0cdbb6648b566
smilingpoplar <smilingpoplar@gmail.com>
2020-10-01 14:50:30 +0800

    update

--- leetcode/numbers-at-most-n-given-digit-set.cpp
+++ leetcode/numbers-at-most-n-given-digit-set.cpp
@@ -12,40 +12,39 @@ using namespace std;
 
 class Solution {
 public:
-    int atMostNGivenDigitSet(vector<string>& D, int N) {
-        // D中有d个数、若取k个数构成k位数，有d^k个
-        // 1. N是n位数，<n位的数有d^1+d^2+...+d^(n-1)个
-        // 2. =n位且<N的数先看第一位a[0]，有(D中<a[0]的个数)*d^(n-1)+(若可取到a[0]，再看第二位...)
-        //        = lessCnt(a[0])*d^(n-1)+lessCnt(a[1])*d^(n-2)+...lessCnt(a[n-1])*d^0
-        // 3. 若可取到N本身，再加上1
-        const auto NS = to_string(N);
-        const int d = D.size(), n = NS.size();
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        // digits中有d个数（d进制），若取k个数构成k位数，有d^k个
+        // 1. n是N位数，<N位的数有d^1+d^2+...+d^(N-1)个
+        // 2. =N位且<n的数先看第一位strN[0]，有(digits[]中<strN[0]的个数)*d^(N-1)+(若可取到strN[0]，再看第二位...)
+        //        = lessThanCnt(strN[0])*d^(N-1)+lessThanCnt(strN[1])*d^(N-2)+...lessThanCnt(strN[N-1])*d^0
+        // 3. 若可取到n本身，再加上1
+        const auto strN = to_string(n);
+        const int d = digits.size(), N = strN.size();
         // case 1
         int base = 1, ans = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < N; i++) {
             base *= d;
             ans += base;
         }
-
         // case 2
         vector<bool> hasDigit(10, false);
-        vector<int> lessCnt(10, 0);
-        for (auto &digitS: D) {
-            int digit = digitS[0] - '0';
+        vector<int> lessThanCnt(10, 0);
+        for (auto &ch: digits) {
+            int digit = ch[0] - '0';
             hasDigit[digit] = true;
             for (int i = digit + 1; i < 10; i++) 
-              lessCnt[i]++;
+              lessThanCnt[i]++;
         }
         
         int idx;
-        for (idx = 0; idx < n; idx++) {
-            int digit = NS[idx] - '0';
-            ans += lessCnt[digit] * base;
+        for (idx = 0; idx < N; idx++) {
+            int digit = strN[idx] - '0';
+            ans += lessThanCnt[digit] * base;
             base /= d;
             if (!hasDigit[digit]) break;
         }
         // case 3
-        if (idx == n) ans++;
+        if (idx == N) ans++;
         return ans;
     }
 };
