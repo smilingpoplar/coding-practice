97fb4e2e2c7ac094633728a6ddcb4162d6b830cc
smilingpoplar <smilingpoplar@gmail.com>
2020-10-02 11:12:17 +0800

    update

--- leetcode/shortest-distance-to-a-character.cpp
+++ leetcode/shortest-distance-to-a-character.cpp
@@ -13,18 +13,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> shortestToChar(string S, char C) {
-        // 往右扫一遍、往左扫一遍
+        // 正反各扫一遍
+        // 正向看距离左边e的最小距离，反向看距离右边e的最小距离
         const int N = S.size();
-        int pos = -N;
         vector<int> ans(N);
+        int idx = -N;
         for (int i = 0; i < N; i++) {
-            if (S[i] == C) pos = i;
-            ans[i] = i - pos;
+            if (S[i] == C) idx = i;
+            ans[i] = i - idx;
         }
-        // 已知C在S中肯定存在，现在0<=pos<N
-        for (int i = N - 1; i >= 0; i--) {
-            if (S[i] == C) pos = i;
-            ans[i] = min(ans[i], abs(i - pos));
+        for (int i = idx - 1; i >= 0; i--) {
+            if (S[i] == C) idx = i;
+            ans[i] = min(ans[i], idx - i);
         }
         return ans;
     }
