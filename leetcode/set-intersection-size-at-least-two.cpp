4dec0a5c8b862ea6c9c38f2b69c0c3123ea7b89f
smilingpoplar <smilingpoplar@gmail.com>
2020-10-02 20:00:48 +0800

    update

--- leetcode/set-intersection-size-at-least-two.cpp
+++ leetcode/set-intersection-size-at-least-two.cpp
@@ -27,7 +27,7 @@ public:
                 ans += 2;
                 last1 = interval[1];
                 last2 = last1 - 1;
-            } else if (last2 < interval[0] && interval[0] <= last1) {
+            } else if (last2 < interval[0]) {
                 // S与当前区间有一个数相交，要在S中加入当前区间的最后一个数
                 ans += 1;
                 last2 = last1;
