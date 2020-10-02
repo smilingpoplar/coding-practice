5e9f0df3778370540b0f1ba35754b79f3f50f594
smilingpoplar <smilingpoplar@gmail.com>
2020-10-02 16:51:36 +0800

    update

--- leetcode/filling-bookcase-shelves.cpp
+++ leetcode/filling-bookcase-shelves.cpp
@@ -12,19 +12,20 @@ using namespace std;
 class Solution {
 public:
     int minHeightShelves(vector<vector<int>>& books, int shelf_width) {
-        // 设dp[i+1]表示books[0..i]子问题的最小高度，
-        // books[i]可以和前面的书一起books[j..i]放在一层，
-        // dp[i+1] = min(dp[j]+max( books[j..i][1] ))
-        // 其中j满足 j<=i && sum( books[j..i][0] ) <= shelf_width
+        // 设dp[i]表示books[0..i)子问题的最小高度，
+        // books[i-1]可以和books[j..i-1)放在一层，即books[j..i-1]在一层，
+        //  j<i 且 sum(books[j..i-1][0])<=shelf_width
+        // dp[j]是前面层的最小高度，
+        // dp[i] = min{ dp[j] + max{ books[j..i-1][1] }}
         const int N = books.size();
-        vector<int> dp(N + 1, INT_MAX);
+        vector<int> dp(N + 1, 1e9);
         dp[0] = 0;
-        for (int i = 0; i < N; i++) {
-            int levelWidth = 0, levelHeight = 0;
-            for (int j = i; j >= 0 && levelWidth + books[j][0] <= shelf_width; j--) {
-                levelWidth += books[j][0];
-                levelHeight = max(levelHeight, books[j][1]);
-                dp[i+1] = min(dp[i+1], dp[j] + levelHeight);
+        for (int i = 1; i <= N; i++) {
+            int width = 0, height = 0;
+            for (int j = i - 1; j >= 0 && width + books[j][0] <= shelf_width; j--) {
+                width += books[j][0];
+                height = max(height, books[j][1]);
+                dp[i] = min(dp[i], dp[j] + height);
             }
         }
         return dp[N];
