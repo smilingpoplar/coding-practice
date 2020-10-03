871dcfd3e731ae83c2408bce8054c5bd1f6172d8
smilingpoplar <smilingpoplar@gmail.com>
2020-10-03 19:04:50 +0800

    update

--- leetcode/is-graph-bipartite.cpp
+++ leetcode/is-graph-bipartite.cpp
@@ -14,18 +14,18 @@ class Solution {
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         const int N = graph.size();
-        vector<int> colors(N, -1);
+        vector<int> color(N, -1);
         for (int i = 0; i < N; i++) {
-            if (colors[i] == -1 && !canColor(i, 0, graph, colors)) return false;
+            if (color[i] == -1 && !canColor(i, 0, graph, color)) return false;
         }
         return true;
     }
     
-    bool canColor(int node, int c, vector<vector<int>> &graph, vector<int> &colors) {
-        if (colors[node] != -1) return colors[node] == c;
-        colors[node] = c;
+    bool canColor(int node, int c, vector<vector<int>> &graph, vector<int> &color) {
+        if (color[node] != -1) return color[node] == c;
+        color[node] = c;
         for (int neighbor : graph[node]) {
-            if (!canColor(neighbor, 1 - c, graph, colors)) return false;
+            if (!canColor(neighbor, 1 - c, graph, color)) return false;
         }
         return true;
     }
--- leetcode/possible-bipartition.cpp
+++ leetcode/possible-bipartition.cpp
@@ -19,19 +19,19 @@ public:
             adj[e[1]].insert(e[0]);
         }
         
-        vector<int> colors(N + 1, -1);
+        vector<int> color(N + 1, -1);
         for (int i = 1; i <= N; i++) {
-            if (colors[i] == -1 && !canColor(i, 0, adj, colors)) return false;
+            if (color[i] == -1 && !canColor(i, 0, adj, color)) return false;
         }
         return true;
     }
     
-    bool canColor(int idx, int color, vector<unordered_set<int>> &adj, vector<int> &colors) {
-        if (colors[idx] != -1) return colors[idx] == color;
+    bool canColor(int idx, int c, vector<unordered_set<int>> &adj, vector<int> &color) {
+        if (color[idx] != -1) return color[idx] == c;
         
-        colors[idx] = color;
+        color[idx] = c;
         for (auto &next : adj[idx]) {
-            if (!canColor(next, 1 - color, adj, colors)) return false;
+            if (!canColor(next, 1 - c, adj, color)) return false;
         }
         return true;
     }
