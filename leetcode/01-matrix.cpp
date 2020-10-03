9b716e505b08590c48c6f53d35213fc392590216
smilingpoplar <smilingpoplar@gmail.com>
2020-10-03 20:54:22 +0800

    update

--- leetcode/01-matrix.cpp
+++ leetcode/01-matrix.cpp
@@ -12,8 +12,9 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        const int INF = 1e9;
         const int R = matrix.size(), C = matrix[0].size();
-        vector<vector<int>> dist(R, vector<int>(C, INT_MAX));
+        vector<vector<int>> dist(R, vector<int>(C, INF));
 
         // 上=>下、左=>右
         for (int r = 0; r < R; r++) {
@@ -21,12 +22,8 @@ public:
                 if (matrix[r][c] == 0) {
                     dist[r][c] = 0;
                 } else {
-                    if (r > 0 && dist[r-1][c] != INT_MAX) {
-                        dist[r][c] = min(dist[r][c], dist[r-1][c] + 1);
-                    }
-                    if (c > 0 && dist[r][c-1] != INT_MAX) {
-                        dist[r][c] = min(dist[r][c], dist[r][c-1] + 1);
-                    }
+                    if (r > 0) dist[r][c] = min(dist[r][c], dist[r-1][c] + 1);
+                    if (c > 0) dist[r][c] = min(dist[r][c], dist[r][c-1] + 1);
                 }
             }
         }        
@@ -34,12 +31,8 @@ public:
         for (int r = R - 1; r >= 0; r--) {
             for (int c = C - 1; c >= 0; c--) {
                 if (matrix[r][c] != 0) {
-                    if (r + 1 < R && dist[r+1][c] != INT_MAX) {
-                        dist[r][c] = min(dist[r][c], dist[r+1][c] + 1);
-                    }
-                    if (c + 1 < C && dist[r][c+1] != INT_MAX) {
-                        dist[r][c] = min(dist[r][c], dist[r][c+1] + 1);
-                    }
+                    if (r + 1 < R) dist[r][c] = min(dist[r][c], dist[r+1][c] + 1);
+                    if (c + 1 < C) dist[r][c] = min(dist[r][c], dist[r][c+1] + 1);
                 }
             }
         }
