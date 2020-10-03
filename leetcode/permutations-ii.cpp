13d3610ccd7d842a35644fb690a00860226ffea5
smilingpoplar <smilingpoplar@gmail.com>
2020-10-03 15:45:22 +0800

    update

--- leetcode/permutations-ii.cpp
+++ leetcode/permutations-ii.cpp
@@ -18,22 +18,21 @@ public:
         search(nums, 0, ans);
         return ans;
     }
-
+    
     void search(vector<int> &nums, int idx, vector<vector<int>> &ans) {
         const int N = nums.size();
         if (idx == N) {
             ans.push_back(nums);
             return;
         }
-        
-        // 有swap()扰乱排序，排序并判断相邻元素不能去重
-        unordered_set<int> seen;
+
+        unordered_set<int> seen; // 排列去重，略过相同元素
         for (int i = idx; i < N; i++) {
             if (seen.count(nums[i])) continue;
             seen.insert(nums[i]);
 
             swap(nums[idx], nums[i]);
-            search(nums, idx + 1, ans);
+            search(nums, idx + 1, ans); // 这里是idx相关
             swap(nums[idx], nums[i]);
         }
     }
