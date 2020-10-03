68ec2ae3060321354a751ada6b5b2157814bb6be
smilingpoplar <smilingpoplar@gmail.com>
2020-10-03 10:42:10 +0800

    update

--- leetcode/merge-intervals.cpp
+++ leetcode/merge-intervals.cpp
@@ -10,34 +10,20 @@
 
 using namespace std;
 
-struct Interval {
-    int start;
-    int end;
-    Interval() : start(0), end(0) {}
-    Interval(int s, int e) : start(s), end(e) {}
-};
-
 class Solution {
 public:
-    vector<Interval> merge(vector<Interval>& intervals) {
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if (intervals.empty()) return {};
-        // 待合并区间要按起点排，先考虑大区间
-        // 若按终点排，当后面大区间要合并前面多个小区间时，要修改已输出区间，很麻烦
-        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) {
-            return a.start < b.start || (a.start == b.start && a.end > b.end);
-        });
+        sort(begin(intervals), end(intervals));
         
-        vector<Interval> ans;
-        auto last = intervals[0]; // 维护着上个待合并区间
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i].start > last.end) {
-                ans.push_back(last);
-                last = intervals[i];
+        vector<vector<int>> ans;
+        for (auto &interval : intervals) {
+            if (ans.empty() || interval[0] > ans.back()[1]) {
+                ans.push_back(interval);
             } else {
-                last.end = max(last.end, intervals[i].end);
+                ans.back()[1] = max(ans.back()[1], interval[1]);
             }
         }
-        ans.push_back(last);
         return ans;
     }
 };
