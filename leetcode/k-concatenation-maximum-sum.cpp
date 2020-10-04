c0b4f3a0aa1bb11047dc3fc174401f17aa730399
smilingpoplar <smilingpoplar@gmail.com>
2020-10-04 15:36:56 +0800

    update

--- leetcode/k-concatenation-maximum-sum.cpp
+++ leetcode/k-concatenation-maximum-sum.cpp
@@ -12,26 +12,23 @@ using namespace std;
 class Solution {
 public:
     int kConcatenationMaxSum(vector<int>& arr, int k) {
-        // 数组重复k次，最大子段和只有两种可能：
-        // 1) 一个数组：arr内的maxSum
-        // 2) 多个数组：max{arr[..N-1]} + max{(k-2)*arrSum, 0} + max{arr[0..]}
+        // 最大子段和只有三种取值：
+        //  1. 在arr中间  2. 在两数组尾首arr[..N-1]+arr[0..]
+        //  3. 在两数组尾首处插入(k-2)个arr[]
+        long maxendinghere = 0, maxsofar = 0;
+        for (int i = 0; i < min(k, 2); i++) { // 前两种情况
+            for (int a : arr) {
+                maxendinghere = max(maxendinghere + a, 0L);
+                maxsofar = max(maxsofar, maxendinghere);
+            }
+        }
+        
+        long sum = accumulate(begin(arr), end(arr), 0);
         const int MOD = 1e9 + 7;
-        const int N = arr.size();
-        int maxEndHere = 0, maxStartHere = 0;
-        int maxSum = 0, arrSum = 0;
-        for (int i = 0; i < N; i++) {
-            maxEndHere = max(maxEndHere + arr[i], 0);
-            maxStartHere = max(maxStartHere + arr[N-1-i], 0);
-            maxSum = max(maxSum, maxEndHere);
-            arrSum += arr[i];
-        }
-        if (k > 1) {
-            int midArrsSum = max(((k - 2) * (long)arrSum) % MOD, 0l);
-            maxSum = max(maxSum, maxEndHere + midArrsSum + maxStartHere);
-        }
-        return maxSum % MOD;
+        return max(maxsofar, maxsofar + (k - 2) * sum) % MOD;               
     }
 };
+
 int main(int argc, const char * argv[]) {    
     return 0;
 }
