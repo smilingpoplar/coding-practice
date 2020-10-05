f5230a511a9c996a4df63ce9564ff54db380a21d
smilingpoplar <smilingpoplar@gmail.com>
2020-10-06 03:00:44 +0800

    update

--- leetcode/longest-valid-parentheses.cpp
+++ leetcode/longest-valid-parentheses.cpp
@@ -28,41 +28,6 @@ public:
     }
 };
 
-/*
-class Solution {
-public:
-    int longestValidParentheses(string s) {
-        const int N = s.size();
-        // 左括号数lefts、右括号数rights
-        int lefts = 0, rights = 0, ans = 0;
-        // 从左往右数的过程中确保lefts>=rights
-        for (int i = 0; i < N; i++) {
-            if (s[i] == '(') lefts++;
-            else if (s[i] == ')') rights++;
-            
-            if (lefts == rights) { // 已配对
-                ans = max(ans, 2 * lefts);
-            } else if (lefts < rights) {
-                lefts = rights = 0;
-            }
-        }
-        // 从右往左数的过程中确保rights>=lefts
-        lefts = rights = 0;
-        for (int i = N - 1; i >= 0; i--) {
-            if (s[i] == '(') lefts++;
-            else if (s[i] == ')') rights++;
-            
-            if (lefts == rights) { // 已配对
-                ans = max(ans, 2 * lefts);
-            } else if (rights < lefts) {
-                lefts = rights = 0;
-            }
-        }
-        return ans;
-    }
-};
-*/
-
 int main(int argc, const char * argv[]) {
     Solution solution;
     cout << solution.longestValidParentheses("()(())");
