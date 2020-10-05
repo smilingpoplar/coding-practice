36d0f3c67bba3291b98c57ff08fd1f26bd96e576
smilingpoplar <smilingpoplar@gmail.com>
2020-10-05 23:48:15 +0800

    update

--- leetcode/minimum-height-trees.cpp
+++ leetcode/minimum-height-trees.cpp
@@ -13,9 +13,7 @@ class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         // 叶节点到这些“根”的距离最长，从外往内一圈圈删除叶节点，最后剩下的可作为根。
-        // bfs不断删除度为1的叶节点，可能剩下1个节点或2个节点（比如一边高多1）。
-        if (n == 1) return { 0 };
-        
+        // bfs不断删除度为1的叶节点，根据最长路径的奇偶性，可能剩下1个或2个节点。
         vector<vector<int>> adj(n);
         for (auto &edge : edges) {
             adj[edge[0]].push_back(edge[1]);
@@ -25,19 +23,23 @@ public:
         vector<int> degree(n);
         for (int i = 0; i < n; i++) {
             degree[i] = adj[i].size();
-            if (degree[i] == 1) leaves.push_back(i);
+            if (degree[i] == 0 || degree[i] == 1) { // 单个节点的度为0
+                leaves.push_back(i);
+                degree[i] = 0; // 删除叶节点
+            }
         }
         
-        // bfs不断删除度为1的叶节点，直到剩下1或2个节点
-        while (n > 2) {
-            n -= leaves.size();
+        int count = leaves.size();
+        while (count < n) {
             vector<int> newLeaves;
             for (int u : leaves) {
                 for (int v : adj[u]) {
                     if (--degree[v] == 1) newLeaves.push_back(v);
                 }
+                degree[u] = 0; // 删除叶节点
             }
             swap(leaves, newLeaves);
+            count += leaves.size();
         }
         return leaves;
     }
