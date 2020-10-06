1e5569f5da0a6e8400a80c0613c20ea2be319f63
smilingpoplar <smilingpoplar@gmail.com>
2020-10-07 00:38:20 +0800

    update

--- leetcode/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ leetcode/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -13,6 +13,34 @@ class Solution {
 public:
     string getHappyString(int n, int k) {
         if (k > 3 * (1 << (n - 1))) return "";
+        string cur, ans;
+        search(n, k, cur, ans);
+        return ans;
+    }
+
+    void search(const int n, int& k, string& cur, string& ans) {
+        if (k == 0) return;  // 找到k个n长串
+        if (cur.size() == n) { // 找到n长串
+            k--;
+            ans = cur;
+            return;
+        }
+
+        for (char c = 'a'; c <= 'c'; c++) {
+            if (cur.empty() || cur.back() != c) {
+                cur.push_back(c);
+                search(n, k, cur, ans);
+                cur.pop_back();
+            }
+        }
+    }
+};
+
+/*
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        if (k > 3 * (1 << (n - 1))) return "";
         deque<string> q{""};
         while (q[0].size() < n) {
             string s = q[0]; q.pop_front();
@@ -24,6 +52,7 @@ public:
         return q[k - 1];
     }
 };
+*/
 
 int main(int argc, const char * argv[]) {    
     return 0;
