bbfc2458178024a92e63ff845312c76b10125989
smilingpoplar <smilingpoplar@gmail.com>
2021-09-01 17:18:12 +0800

    update

--- leetcode/sum-of-mutated-array-closest-to-target.cpp
+++ leetcode/sum-of-mutated-array-closest-to-target.cpp
@@ -13,7 +13,7 @@ class Solution {
 public:
     int findBestValue(vector<int>& arr, int target) {
         const int N = arr.size();
-        sort(arr.begin(), arr.end());
+        sort(begin(arr), end(arr));
         vector<int> presum(N + 1, 0); // presum[i]表示sum(arr[0..i))
         int mx = INT_MIN;
         for (int i = 0; i < N; i++) {
@@ -21,11 +21,10 @@ public:
             mx = max(mx, arr[i]);
         }
         
-        int l = 0, u = mx + 1;
+        int l = 0, u = mx + 1; // 题目条件
         while (l + 1 < u) {
             int m = l + (u - l) / 2;
-            // 数组之和sum(value)是关于value的递增函数，
-            // sum(value)>=target满足二分搜索的条件形式[0..0 1..1]
+            // 满足二分搜索的条件形式[0..0 1..1]
             if (getSum(m, arr, presum) >= target) {
                 u = m;
             } else {
@@ -40,8 +39,8 @@ public:
     
     int getSum(int value, vector<int>& arr, const vector<int> &presum) {
         // 将>=value的值全变成value
-        int idx = lower_bound(arr.begin(), arr.end(), value) - arr.begin();
-        // [0..idx)为原数，[idx..N)变为value
+        int idx = lower_bound(begin(arr), end(arr), value) - begin(arr);
+        // [0..idx)为原数，[idx..N)变为value，sum(value)是关于value的递增函数
         int sum = presum[idx] + (arr.size() - idx) * value;
         return sum;
     }
