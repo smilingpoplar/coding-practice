52b4cb6991ae03aca5390f357846a56c20f0df88
smilingpoplar <smilingpoplar@gmail.com>
2021-09-01 19:03:19 +0800

    update

--- leetcode/median-of-two-sorted-arrays.cpp
+++ leetcode/median-of-two-sorted-arrays.cpp
@@ -13,33 +13,33 @@ using namespace std;
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-        // 数组a分成a1[0..i-1]、a2[i..M-1]两部分，数组b分成b1[0..j-1]、b2[j..N-1]两部分。
-        // 把a1和b1放一起构成整体的left部分，设left长度k=i+j=(M+N+1)/2，则中位数仅与{a[i-1], a[i], b[j-1], b[j]}四数有关。
-        // 由0<=i<=M，j=k-i => k-M<=j<=k；要使0<=j<=N，需要k-M>=0，k<=N => M<=N；要在较短数组中搜索i
-        // 若使划分i有效，需 max(left)<=min(right)，即 a[i-1]<=b[j] && b[j-1]<=a[i]
-        // 1) M+N为奇数时，max(left)为中位数
-        // 2) M+N为偶数时，[max(left)+min(right)]/2为中位数
+        // 数组a分成a[0..i-1]、a[i..M-1]两部分，数组b分成b[0..j-1]、b[j..N-1]两部分，
+        // 令k=(M+N+1)/2，划分i要满足i+j=k，且a[i-1]<=b[j] && b[j-1]<=a[i]
+        // 1.要在a、b的较短数组中搜索划分i
+        //  由0<=i<=M（i==M时后半为空），j=k-i => k-M<=j<=k；要使0<=j<=N，需要k-M>=0，k<=N => M<=N
+        // 2.最终M+N为奇数时，lMax=max(a[i-1],b[j-1])为中位数
+        // M+N为偶数时，(lMax+rMin)/2为中位数，rMin=min(a[i],b[j])
         const int M = a.size(), N = b.size();
         if (M > N) return findMedianSortedArrays(b, a);
         
-        int k = (M + N + 1) / 2;
+        const int k = (M + N + 1) / 2;
         int l = 0, u = M;
         while (l <= u) {
             int i = l + (u - l) / 2, j = k - i;
-            if (i > 0 && a[i-1] > b[j]) { // 需要a[i-1]<=b[j]，现在i太大了，要在左半找
+            if (i > 0 && j < N && a[i-1] > b[j]) { // 需要a[i-1]<=b[j]，现在i太大了，要在左半找
                 u = i - 1; 
-            } else if (j > 0 && b[j-1] > a[i]) { // 需要b[j-1]<=a[i]，现在i太小了，要在右半找
+            } else if (j > 0 && i < M && b[j-1] > a[i]) { // 需要b[j-1]<=a[i]，现在i太小了，要在右半找
                 l = i + 1;
             } else { // 有效的划分i
-                int leftMax = INT_MIN;
-                if (i > 0) leftMax = max(leftMax, a[i-1]);
-                if (j > 0) leftMax = max(leftMax, b[j-1]);
-                if ((M + N) % 2 == 1) return leftMax;
+                int lMax = INT_MIN;
+                if (i > 0) lMax = max(lMax, a[i-1]);
+                if (j > 0) lMax = max(lMax, b[j-1]);
+                if ((M + N) % 2 == 1) return lMax;
 
-                int rightMin = INT_MAX;
-                if (i < M) rightMin = min(rightMin, a[i]);
-                if (j < N) rightMin = min(rightMin, b[j]);
-                return (leftMax + rightMin) * 0.5;
+                int rMin = INT_MAX;
+                if (i < M) rMin = min(rMin, a[i]);
+                if (j < N) rMin = min(rMin, b[j]);
+                return (lMax + rMin) * 0.5;
             }
         }
         return -1;
