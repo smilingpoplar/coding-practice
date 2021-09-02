ca9d576e6e0cb3ade48cd9cae81e76c4b0b26524
smilingpoplar <smilingpoplar@gmail.com>
2021-09-02 09:29:51 +0800

    update

--- leetcode/patching-array.cpp
+++ leetcode/patching-array.cpp
@@ -15,7 +15,7 @@ public:
         // 设[1..missing)已能由数组数相加获得，尝试扩展上边界missing
         long missing = 1;
         int i = 0, ans = 0;
-        while (missing <= n) {
+        while (missing <= n) { // 最终missing>n，[1..missing)包含[1..n]
             // nums[i]已能由相加获得（<missing）或在数组中（=missing）
             if (i < nums.size() && nums[i] <= missing) {
                 missing += nums[i];
