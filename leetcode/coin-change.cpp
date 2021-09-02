79805bca3d76ce9c861bb76bbabd1287b7989452
smilingpoplar <smilingpoplar@gmail.com>
2021-09-02 16:15:33 +0800

    update

--- leetcode/coin-change.cpp
+++ leetcode/coin-change.cpp
@@ -13,15 +13,15 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         // 完全背包问题：选最少的硬币数使面值总和为amount
-        const int theMax = amount + 1;
-        vector<int> dp(amount + 1, theMax);
+        const int THE_MAX = amount + 1;
+        vector<int> dp(amount + 1, THE_MAX);
         dp[0] = 0;
-        for (int coin : coins) {
-            for (int i = coin; i <= amount; i++) { // 正序遍历各容量
-                dp[i] = min(dp[i], dp[i - coin] + 1);
+        for (int c : coins) {
+            for (int v = c; v <= amount; v++) { // 正序遍历各容量
+                dp[v] = min(dp[v], dp[v-c] + 1);
             }
         }
-        return (dp[amount] != theMax) ? dp[amount] : -1;
+        return (dp[amount] != THE_MAX) ? dp[amount] : -1;
     }
 };
 
