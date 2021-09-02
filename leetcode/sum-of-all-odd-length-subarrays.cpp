a14787b4324d41e600f5e5937bc8f9fc45638737
smilingpoplar <smilingpoplar@gmail.com>
2021-09-03 00:25:45 +0800

    update

--- leetcode/sum-of-all-odd-length-subarrays.cpp
+++ leetcode/sum-of-all-odd-length-subarrays.cpp
@@ -11,16 +11,16 @@ using namespace std;
 
 class Solution {
 public:
-    int sumOddLengthSubarrays(vector<int>& arr) {
-        // 看arr[i]对奇数长子段贡献了多少值
-        // arr[i]左边有子段[[0,i]..i]，共i+1个
-        // 右边有子段[i..[i,N-1]]，共N-i个
-        // 所以包含A[i]的子段共(i+1)*(N-i)个，
-        // 其中奇数长子段有((i+1)*(N-i)+1)/2个
-        const int N = arr.size();
+    int sumOddLengthSubarrays(vector<int>& A) {
+        // 看A[i]对奇数长子段贡献了多少值
+        // 左边包含A[i]的子段A[..i]有i+1个，右边包含A[i]的子段A[i..]有N-i个，包含A[i]的子段共(i+1)*(N-i)个。
+        // 设包含A[i]的奇数子段有odd个，偶数子段有even个，由举例归纳的、应该没错的结论为 odd=even或even+1。
+        // 注：比如举例统计[1 2 3 4 5]中包含2的长度1子段数、长度2子段数、长度3子段数...，
+        // 所以奇数长子段有 odd = ((i+1)*(N-i)+1)/2个
+        const int N = A.size();
         int ans = 0;
         for (int i = 0; i < N; i++) {
-            ans += ((i+1)*(N-i)+1)/2 * arr[i];
+            ans += ((i+1)*(N-i)+1)/2 * A[i];
         }
         return ans;
     }
