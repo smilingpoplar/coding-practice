266a5b1998e64ff67167e1e16f90676dbee0f36c
smilingpoplar <smilingpoplar@gmail.com>
2021-09-02 13:14:49 +0800

    update

--- leetcode/last-stone-weight-ii.cpp
+++ leetcode/last-stone-weight-ii.cpp
@@ -12,13 +12,14 @@ using namespace std;
 class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
-        // 分成和最接近的两个子集
-        // 不妨求 和<=sum/2的子集，01背包
+        // 分成和最接近的两个子集，求 和<=sum/2的最大子集，01背包
+        // 设dp[i][v]表示前i个数的子集和等于v是否可行。
+        // dp[i][v] = dp[i-1][v]/*不取nums[i]*/ || dp[i-1][v-num]/*取nums[i]*/。
+        // 递推式i这维依赖于i-1项，可省掉i这一维。dp[v] = dp[v] || dp[v-num]。v逆序遍历。
         int sum = 0;
         for (int w : stones) 
             sum += w;
         
-        // dp[v]表示能填满v容量
         vector<bool> dp(sum / 2, false);
         dp[0] = true;
         for (int w : stones) {
