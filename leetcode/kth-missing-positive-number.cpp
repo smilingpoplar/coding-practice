0ac29363aed911eff1cd7e64ea47054d539dd5ae
smilingpoplar <smilingpoplar@gmail.com>
2021-09-02 09:13:10 +0800

    update

--- leetcode/kth-missing-positive-number.cpp
+++ leetcode/kth-missing-positive-number.cpp
@@ -12,14 +12,23 @@ using namespace std;
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        // 找第k小的缺失数
-        // 猜一个数x，看<=x的缺失数有多少个
-        // 猜的数不妨从arr[]中选arr[m]，看<=arr[m]的缺失数个数
+        // 题目：找第k小的缺失自然数
+        // 假设arr[i]全部>k，所求就是k；从左往右遇到arr[i]<=k，就递增k
+        for (int num : arr) { // arr[]是递增数组
+            if (num <= k) k++;
+            else break;
+        }
+        return k;
+    }    
+};
 
-        // 数组arr和缺失数组mis拼成自然数数组all，
-        // 值arr[m]是数组all的第arr[m]个元素，又是数组arr的第m+1个元素，
-        // 所以是缺失数组mis的第arr[m]-(m+1)个元素，
-        // 即<=arr[m]的缺失数有count=arr[m]-(m+1)个，...(1)式
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        // 题目：找第k小的缺失自然数
+        // 数组arr和缺失数组mis可拼成自然数序列，
+        // 值arr[m]是自然数序列的第arr[m]个元素，又是数组arr的第m+1个元素，
+        // 所以<=arr[m]的缺失数count(m)=arr[m]-(m+1)
         // 因为arr[m]比m增加得快，count(m)是关于m的递增函数，
         // 条件式enough(m){count(m)>=k}满足二分搜索的条件形式[0..0 1..1]
         int l = -1, u = arr.size();
@@ -31,10 +40,10 @@ public:
                 l = m;
             }
         }
-        // <=arr[u]的缺失数首次有>=k个，从前一个数arr[u-1]开始数，
-        // 根据(1)式<=arr[u-1]的缺失数有arr[u-1]-u个，
+        // <=arr[u]的缺失数首次有>=k个。
+        // <=arr[u-1]的缺失数有count(u-1)=arr[u-1]-u个，
         // 比起需要的k个，还差k-(arr[u-1]-u)个，
-        // arr[u-1]+(k-(arr[u-1]-u)) = u+k
+        // 第k个缺失数为 arr[u-1]+(k-(arr[u-1]-u)) = u+k
         return u + k;
     }
     
@@ -42,6 +51,7 @@ public:
         return arr[m] - (m + 1) >= k;
     }
 };
+
 
 int main(int argc, const char * argv[]) {    
     return 0;
