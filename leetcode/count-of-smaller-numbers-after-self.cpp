700afb3c54ca86a76798e35d9d4fa468913e8f72
smilingpoplar <smilingpoplar@gmail.com>
2021-09-03 12:47:12 +0800

    update

--- leetcode/count-of-smaller-numbers-after-self.cpp
+++ leetcode/count-of-smaller-numbers-after-self.cpp
@@ -12,7 +12,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> countSmaller(vector<int>& nums) {
-        // 在idx数组上作归并排序
+        // 要看数的右边，所以不能移动数字，在索引数组idx上归并排序
         const int N = nums.size();
         vector<int> idx;
         for (int i = 0; i < N; i++) {
@@ -22,15 +22,15 @@ public:
         mergeSort(nums, idx, 0, N - 1, ans);
         return ans;
     }
-    
+
     void mergeSort(vector<int> &nums, vector<int> &idx, int l, int r, vector<int> &ans) {
         if (l >= r) return;
         int mid = l + (r - l) / 2;
         mergeSort(nums, idx, l, mid, ans);
         mergeSort(nums, idx, mid + 1, r, ans);
-        
+
         vector<int> merged(r - l + 1);
-        // 从两数组末端开始比较
+        // 两指针同向遍历，从两数组末端开始比较
         int i = mid, j = r, k = r - l;
         while (i >= l && j > mid) {
             if (nums[idx[i]] > nums[idx[j]]) {
