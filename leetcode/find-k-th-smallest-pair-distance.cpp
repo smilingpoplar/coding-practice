ee852a371481c433ba2d0aef8f2d73b0a80bb30a
smilingpoplar <smilingpoplar@gmail.com>
2021-09-03 14:34:37 +0800

    update

--- leetcode/find-k-th-smallest-pair-distance.cpp
+++ leetcode/find-k-th-smallest-pair-distance.cpp
@@ -12,8 +12,9 @@ using namespace std;
 class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
-        // 猜第k小的距离m，m的值范围[0, max(nums)-min(nums)]
+        // 两数的距离与数的位置无关，先排序数组
         sort(nums.begin(), nums.end());
+        // 猜第k小的距离m，m的值范围[0, max(nums)-min(nums)]
         int l = 0, u = nums.back() - nums[0];
         while (l <= u) {
             int mid = l + (u - l) / 2;
@@ -27,8 +28,8 @@ public:
     }
     
     bool enough(int m, vector<int> &nums, int k) {
-        // <=m的距离个数count(m)是关于m的递增函数
-        // count(m)>=k满足二分搜索的条件形式[0..0 1..1]
+        // count(m){ <=m的距离个数 }是关于m的递增函数，
+        // enough(m){ count(m)>=k }满足二分搜索的条件形式[0..0 1..1]
         int count = 0;
         for (int j = 1; j < nums.size(); j++) {
             // 找 nums[j]-nums[i] <= m，nums[i] >= nums[j]-m
