61be6bc469429bc21c05c31c2e7c89fa457fd36d
smilingpoplar <smilingpoplar@gmail.com>
2021-09-04 18:25:09 +0800

    update

--- leetcode/find-minimum-in-rotated-sorted-array-ii.cpp
+++ leetcode/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -18,9 +18,8 @@ public:
         int lo = 0, hi = N - 1;
         while (lo < hi) { // len([lo..hi])>=2
             int mi = lo + (hi - lo) / 2;
-            // 由于mi向下取整，可能mi==lo，
-            // 若用if (nums[lo] < nums[mi])，当mi==lo时这个if将被绕过
-            // 用mi和hi就没这个问题，mi!=hi
+            // 由于mi向下取整，可能==lo，使判断`if (nums[lo] < nums[mi])`被绕过
+            // 用mi和hi比较就没这个问题，mi一定!=hi
             if (nums[mi] > nums[hi]) { // 右半旋转，且m位置不是min
                 lo = mi + 1;
             } else if (nums[lo] > nums[mi]) { // 左半旋转
