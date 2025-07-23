55e82f4e2eea585db492790f9924929b1b285b16
le <smilingpoplar@gmail.com>
2025-07-23 08:10:44 +0800

    update

--- leetcode/reverse-linked-list-ii.cpp
+++ leetcode/reverse-linked-list-ii.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 struct ListNode {
     int val;
-    ListNode *next;
+    ListNode* next;
     ListNode(int x) : val(x), next(NULL) {}
 };
 
@@ -19,25 +19,27 @@ class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int m, int n) {
         if (!head) return NULL;
-        
+
         ListNode dummy(-1);
         dummy.next = head;
         auto prev = &dummy;
-        for (int i = 1; i < m; i++) prev = prev->next;
+        for (int i = 1; i < m; i++) {
+            prev = prev->next;
+        }
 
-        // 组头是反转后的组尾tail，将[m+1..n]的节点插入prev之后
-        auto tail = prev->next, curr = tail->next;
+        // 组头是反转后的组尾，将[m+1..n]的节点插入prev之后
+        auto newtail = prev->next, curr = newtail->next;
         for (int i = m + 1; i <= n; i++) {
-            tail->next = curr->next;
+            newtail->next = curr->next;
             curr->next = prev->next;
             prev->next = curr;
-            curr = tail->next;
+            curr = newtail->next;
         }
         return dummy.next;
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     ListNode l1(1);
     ListNode l2(2);
     ListNode l3(3);
@@ -47,13 +49,13 @@ int main(int argc, const char * argv[]) {
     l2.next = &l3;
     l3.next = &l4;
     l4.next = &l5;
-    
+
     Solution solution;
     auto head = solution.reverseBetween(&l1, 1, 2);
     while (head) {
         cout << head->val << " ";
         head = head->next;
     }
-    
+
     return 0;
 }
