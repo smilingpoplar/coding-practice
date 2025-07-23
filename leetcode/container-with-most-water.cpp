d19bdae4064ffc80cce406bcdffdda09957de6b6
le <smilingpoplar@gmail.com>
2025-07-23 23:01:45 +0800

    update

--- leetcode/container-with-most-water.cpp
+++ leetcode/container-with-most-water.cpp
@@ -13,23 +13,24 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        // 首尾两指针，向内移动矮的那个
-        // 容器高度由矮的决定，向内移动高的那个则容器高度不变或变小、宽度变小，结果变差
+        // 贪心法：首尾两指针，向内移动矮的那个
         int i = 0, j = (int)height.size() - 1;
         int ans = 0;
         while (i < j) {
             ans = max(ans, min(height[i], height[j]) * (j - i));
-            if (height[i] < height[j]) i++;
-            else j--;
+            if (height[i] < height[j])
+                i++;
+            else
+                j--;
         }
         return ans;
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     vector<int> height = {3, 2, 3};
     Solution solution;
     cout << solution.maxArea(height);
-    
+
     return 0;
 }
