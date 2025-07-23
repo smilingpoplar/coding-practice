672f13765fa628e4764f725355b3e143c78c9b4b
le <smilingpoplar@gmail.com>
2025-07-24 07:23:56 +0800

    update

--- leetcode/restore-ip-addresses.cpp
+++ leetcode/restore-ip-addresses.cpp
@@ -6,8 +6,8 @@
 //
 
 #include <iostream>
-#include <vector>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
@@ -15,6 +15,7 @@ class Solution {
     const int TOTAL_PART_COUNT = 4;
     const int MIN_PART_SIZE = 1;
     const int MAX_PART_SIZE = 3;
+
 public:
     vector<string> restoreIpAddresses(string s) {
         vector<string> ans;
@@ -22,9 +23,9 @@ public:
         search(s, 0, parts, ans);
         return ans;
     }
-    
+
     void search(const string &s, int idx,
-             vector<string> &parts, vector<string> &ans) {
+                vector<string> &parts, vector<string> &ans) {
         const int N = s.size();
         if (parts.size() == TOTAL_PART_COUNT) {
             if (idx == N) {
@@ -37,11 +38,11 @@ public:
             }
             return;
         }
-        
+
         // partSize = (N - idx) / (TOTAL_PART_COUNT - parts.size())
         if (N - idx < (TOTAL_PART_COUNT - parts.size()) * MIN_PART_SIZE) return;
         if (N - idx > (TOTAL_PART_COUNT - parts.size()) * MAX_PART_SIZE) return;
-        
+
         for (int sz = MIN_PART_SIZE; sz <= MAX_PART_SIZE && idx + sz <= N; sz++) {
             // 验证子段是否合法
             if (sz > 1 && s[idx] == '0') continue;
@@ -55,12 +56,12 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     Solution solution;
     auto ans = solution.restoreIpAddresses("010010");
     for (auto ip : ans) {
         cout << ip << endl;
     }
-    
+
     return 0;
 }
