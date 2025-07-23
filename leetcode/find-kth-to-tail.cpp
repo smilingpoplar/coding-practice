7f966ea824457734aa55609406898bbad8abbc3d
le <smilingpoplar@gmail.com>
2025-07-23 11:29:32 +0800

    + sort-in-list

--- leetcode/find-kth-to-tail.cpp
+++ leetcode/find-kth-to-tail.cpp
@@ -15,36 +15,21 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-ListNode* FindKthToTail(ListNode* pHead, int k) {
-    if (k == 0 || !pHead) return nullptr;
-    auto fast = pHead;
-    for (int i = 0; i < k; i++) {
-        if (!fast) return nullptr;
-        fast = fast->next;
+class Solution {
+public:
+    ListNode* FindKthToTail(ListNode* pHead, int k) {
+        if (k == 0 || !pHead) return nullptr;
+        auto fast = pHead;
+        for (int i = 0; i < k; i++) {
+            if (!fast) return nullptr;
+            fast = fast->next;
+        }
+
+        auto slow = pHead;
+        while (fast) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        return slow;
     }
-
-    auto slow = pHead;
-    while (fast) {
-        fast = fast->next;
-        slow = slow->next;
-    }
-    return slow;
-}
-
-int main(int argc, const char* argv[]) {
-    ListNode l1(1);
-    ListNode l2(2);
-    ListNode l3(3);
-    ListNode l4(4);
-    ListNode l5(5);
-    l1.next = &l2;
-    l2.next = &l3;
-    l3.next = &l4;
-    l4.next = &l5;
-    l5.next = &l3;
-
-    Solution solution;
-    cout << solution.hasCycle(&l1);
-
-    return 0;
-}
+};
---
+++ leetcode/sort-in-list.cpp
@@ -0,0 +1,50 @@
+//
+//  sort-in-list
+//  https://leetcode.com/problems/sort-in-list/
+//
+//  Copyright (c) 2015 YangLe. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* sortInList(ListNode* head) {
+        if (!head || !head->next) return head;
+        auto fast = head->next, slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        auto head2 = slow->next;
+        slow->next = nullptr;
+
+        return mergeTwoLists(sortInList(head), sortInList(head2));
+    }
+
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(-1);
+        auto curr = &dummy;
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                curr->next = l1;
+                curr = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                curr = l2;
+                l2 = l2->next;
+            }
+        }
+        curr->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
