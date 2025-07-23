2f539d613d0e2c918a8a86b97889283fb4169444
le <smilingpoplar@gmail.com>
2025-07-23 22:21:08 +0800

    update

--- leetcode/fibonacci-number.cpp
+++ leetcode/fibonacci-number.cpp
@@ -12,10 +12,10 @@ using namespace std;
 
 class Solution {
 public:
-    int fib(int N) {
-        if (N <= 1) return N;
+    int fib(int n) {
+        if (n <= 1) return n;
         int prev1 = 1, prev2 = 0;
-        for (int i = 2; i <= N; i++) {
+        for (int i = 2; i <= n; i++) {
             int curr = prev1 + prev2;
             prev2 = prev1;
             prev1 = curr;
