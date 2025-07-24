658e5a5c1d0f4ff9588a3db0f9933192176efe89
le <smilingpoplar@gmail.com>
2025-07-24 10:38:17 +0800

    update

--- leetcode/longest-increasing-subsequence.cpp
+++ leetcode/longest-increasing-subsequence.cpp
@@ -13,7 +13,9 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        // 设dp[i]表示nums[0..i]的最长递增子序列
+        // 设dp[i]表示以位置i结尾的nums[0..i]的最长递增子序列长，
+        // dp[i] = max{1, dp[j]+1 | 0<=j<i且nums[j]<nums[i] }
+        // 所求为 max{dp[i]}
         const int N = nums.size();
         vector<int> dp(N, 1);
         int ans = 0;
@@ -33,16 +35,19 @@ public:
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> tails; // 各长度LIS的最小尾元素
+        vector<int> tails;  // 保存各长度LIS的最小尾元素，是个递增数组
         for (int num : nums) {
+            // 在tails[]中找第一个>=num的数
             auto it = lower_bound(tails.begin(), tails.end(), num);
-            if (it != tails.end()) *it = num;
-            else tails.push_back(num);
+            if (it != tails.end())
+                *it = num;  // 若找到，则用num替换该项（末尾变小、LIS长度不变）
+            else
+                tails.push_back(num);  // 若找不到，说明所有尾元素都<num，可扩展序列
         }
         return tails.size();
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char* argv[]) {
     return 0;
 }
