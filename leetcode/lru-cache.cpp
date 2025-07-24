91e0d9dbd178e551b6f49caddc8f9c3533424d71
le <smilingpoplar@gmail.com>
2025-07-24 10:54:03 +0800

    update

--- leetcode/lru-cache.cpp
+++ leetcode/lru-cache.cpp
@@ -11,41 +11,46 @@
 
 using namespace std;
 
-class LRUCache{
-    // 把entry{key,value}用链表串起来，
+class LRUCache {
+    // 把 entry{key,value} 用链表串起来，
     // 每次访问把entry移到表头，删除lru时删除表尾
-    struct Entry { int key; int value; };
-    list<Entry> l; // 实际存储
-    unordered_map<int, list<Entry>::iterator> entryPtrs; // key=>entry
+    struct Entry {
+        int key;
+        int value;
+    };
+    list<Entry> l;                                 // 实际存储
+    unordered_map<int, list<Entry>::iterator> mp;  // key=>entry
     int capacity;
+
 private:
     void touch(int key) {
         // toList.splice(toListIterator, fromList, fromListSingleIterator)
-        l.splice(l.begin(), l, entryPtrs[key]);
-        entryPtrs[key] = l.begin();
+        l.splice(l.begin(), l, mp[key]);
+        mp[key] = l.begin();
     }
+
 public:
     LRUCache(int capacity) {
         this->capacity = capacity;
     }
-    
+
     int get(int key) {
-        if (!entryPtrs.count(key)) return -1;
+        if (!mp.count(key)) return -1;
         touch(key);
-        return entryPtrs[key]->value;
+        return mp[key]->value;
     }
-    
+
     void put(int key, int value) {
         if (capacity == 0) return;
-        if (!entryPtrs.count(key)) {
-            if (l.size() == capacity) { // 删除表尾
-                entryPtrs.erase(l.back().key);
+        if (!mp.count(key)) {            // 新增
+            if (l.size() == capacity) {  // 删除表尾
+                mp.erase(l.back().key);
                 l.pop_back();
             }
             l.push_front({key, value});
-            entryPtrs[key] = l.begin();
-        } else {
-            entryPtrs[key]->value = value;
+            mp[key] = l.begin();
+        } else {  // 更新
+            mp[key]->value = value;
             touch(key);
         }
     }
@@ -58,11 +63,11 @@ public:
  * obj.put(key,value);
  */
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     LRUCache cache(2);
     cout << cache.get(5) << endl;
     cache.set(5, 5);
     cout << cache.get(5) << endl;
-    
+
     return 0;
 }
