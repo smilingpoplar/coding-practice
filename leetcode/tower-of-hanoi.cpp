5be4a6339beb386b9dbf864fd830e3bf15c88276
le <smilingpoplar@gmail.com>
2025-07-25 07:33:25 +0800

    + tower-of-hanoi

---
+++ leetcode/tower-of-hanoi.cpp
@@ -0,0 +1,27 @@
+//
+//  tower-of-hanoi
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> getSolution(int n) {
+        vector<string> ans;
+        hanoi(n, "left", "mid", "right", ans);
+        return ans;
+    }
+
+    void hanoi(int n, const string &a, const string &b,
+               const string &c, vector<string> &ans) {
+        if (n == 0) {
+            return;
+        }
+        hanoi(n - 1, a, c, b, ans);
+        ans.push_back("move from " + a + " to " + c);
+        hanoi(n - 1, b, a, c, ans);
+    }
+};
