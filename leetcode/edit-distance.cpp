494841e71ceccfbb8580cfa0e784ecdc3df2636a
le <smilingpoplar@gmail.com>
2025-07-24 09:04:04 +0800

    update

--- leetcode/edit-distance.cpp
+++ leetcode/edit-distance.cpp
@@ -15,18 +15,18 @@ public:
     int minDistance(string word1, string word2) {
         // 设dp[i][j]表示s1[i..]和s2[j..]的最小编辑距离
         // 若s1[i]==s2[j]，dp[i][j]=dp[i+1][j+1]；否则，
-        // dp[i][j] = 1 + min{ dp[i+1][j]/*删除*/, dp[i][j+1]/*添加*/,dp[i+1][j+1]/*替换*/ }
+        // dp[i][j] = 1 + min{ dp[i+1][j]/*删除*/, dp[i][j+1]/*添加*/, dp[i+1][j+1]/*替换*/ }
         const int M = word1.size(), N = word2.size();
         vector<vector<int>> dp(M + 1, vector<int>(N + 1, 0));
         for (int i = M - 1; i >= 0; i--) dp[i][N] = M - i;
         for (int j = N - 1; j >= 0; j--) dp[M][j] = N - j;
-        
+
         for (int i = M - 1; i >= 0; i--) {
             for (int j = N - 1; j >= 0; j--) {
                 if (word1[i] == word2[j]) {
-                    dp[i][j] = dp[i+1][j+1];
+                    dp[i][j] = dp[i + 1][j + 1];
                 } else {
-                    dp[i][j] = 1 + min({dp[i+1][j], dp[i][j+1], dp[i+1][j+1]});
+                    dp[i][j] = 1 + min({dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1]});
                 }
             }
         }
@@ -34,9 +34,9 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     Solution solution;
     cout << solution.minDistance("cat", "ha");
-    
+
     return 0;
 }
--- leetcode/longest-palindromic-substring.cpp
+++ leetcode/longest-palindromic-substring.cpp
@@ -20,7 +20,7 @@ public:
         // 要让dp[j-1]表示旧状态dp[i+1][j-1]，j从右往左遍历
         const int N = s.size();
         vector<bool> dp(N, false);
-        int longest = 0;
+        int maxlen = 0;
         string ans;
         for (int i = N - 1; i >= 0; i--) {
             for (int j = N - 1; j >= i; j--) {
@@ -29,8 +29,8 @@ public:
 
                 if (dp[j]) {
                     int len = j - i + 1;
-                    if (len > longest) {
-                        longest = len;
+                    if (len > maxlen) {
+                        maxlen = len;
                         ans = s.substr(i, len);
                     }
                 }
@@ -45,31 +45,31 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         // 从可能的中心往外扩展找回文
-        int longest = 0, lo = 0;
+        int maxlen = 0, lo = 0;
         for (int i = 0; i < s.size(); i++) {
             int len1 = expand(s, i, i);
             int len2 = expand(s, i, i + 1);
             int len = max(len1, len2);
-            if (len > longest) {
-                longest = len;
+            if (len > maxlen) {
+                maxlen = len;
                 lo = i - (len - 1) / 2;
             }
         }
-        return s.substr(lo, longest);
+        return s.substr(lo, maxlen);
     }
-    
+
     int expand(const string &s, int l, int r) {
         while (l >= 0 && r < s.size() && s[l] == s[r]) {
             l--;
             r++;
         }
-        return r - l - 1; // (l,r)
+        return r - l - 1;  // (l,r)
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     Solution solution;
     cout << solution.longestPalindrome("31232156");
-    
+
     return 0;
 }
