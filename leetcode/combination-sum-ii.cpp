6ae5d0590be0465802d10c5e30510ce0d585ab8d
le <smilingpoplar@gmail.com>
2025-07-24 22:47:30 +0800

    update

--- leetcode/combination-sum-ii.cpp
+++ leetcode/combination-sum-ii.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+    vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> comb;
@@ -21,16 +21,16 @@ public:
     }
 
     void search(const vector<int> candiatates, int idx, int target,
-             vector<int> &comb, vector<vector<int>> &ans) {
+                vector<int> &comb, vector<vector<int>> &ans) {
         if (target < 0) return;
         if (target == 0) {
             ans.push_back(comb);
             return;
         }
-        
+
         for (int i = idx; i < candiatates.size(); i++) {
             // 有重复元素，相同元素只选第一个
-            if (i > idx && candiatates[i] == candiatates[i-1]) continue;
+            if (i > idx && candiatates[i] == candiatates[i - 1]) continue;
             comb.push_back(candiatates[i]);
             search(candiatates, i + 1, target - candiatates[i], comb, ans);
             comb.pop_back();
@@ -38,8 +38,8 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
-    vector<int> nums = {10,1,2,7,6,1,5};
+int main(int argc, const char *argv[]) {
+    vector<int> nums = {10, 1, 2, 7, 6, 1, 5};
     Solution solution;
     auto result = solution.combinationSum2(nums, 8);
     for (const auto &combination : result) {
@@ -48,6 +48,6 @@ int main(int argc, const char * argv[]) {
         }
         cout << endl;
     }
-    
+
     return 0;
 }
