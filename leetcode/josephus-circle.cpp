6365be4c21a15429a0854d464e83d99e6e3ed3b1
le <smilingpoplar@gmail.com>
2025-07-25 18:32:14 +0800

    + josephus-circle

---
+++ leetcode/josephus-circle.cpp
@@ -0,0 +1,20 @@
+//
+//  josephus-circle
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int ysf(int n, int m) {
+        // 例如，(1+m) % len => 1，反过来就是 1 => (1+m) % len
+        // 推广到 x => (x+m) % len
+        int x = 0;  // 涉及到模运算，从0开始
+        for (int i = 2; i <= n; i++) {
+            x = (x + m) % i;
+        }
+        return x + 1;  // 从1开始编号
+    }
+};
