996bed008880c956f8ca23a34cdbd39105233a3b
le <smilingpoplar@gmail.com>
2025-07-25 20:40:44 +0800

    update

--- leetcode/maximum-size-subarray-sum-equals-k.cpp
+++ leetcode/maximum-size-subarray-sum-equals-k.cpp
@@ -13,20 +13,19 @@ using namespace std;
 class Solution {
 public:
     int maxSubArrayLen(vector<int>& nums, int k) {
-        unordered_map<int, int> mp; // sum=>idx，相同sum记录最小的idx
+        unordered_map<int, int> mp = {{0, -1}};  // sum=>firstIdx
         int runningSum = 0;
-        mp[runningSum] = -1;
         int ans = 0;
         for (int i = 0; i < nums.size(); i++) {
             runningSum += nums[i];
             int toFind = runningSum - k;
             if (mp.count(toFind)) ans = max(ans, i - mp[toFind]);
-            if (!mp.count(runningSum)) mp[runningSum] = i; // 相同sum记录最小的idx
+            if (!mp.count(runningSum)) mp[runningSum] = i;  // 相同sum记录最小的idx
         }
         return ans;
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     return 0;
 }
