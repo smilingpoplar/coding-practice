31366e030f173b3ac1731e862c953edc9f2689aa
le <smilingpoplar@gmail.com>
2025-07-25 16:59:32 +0800

    update

--- leetcode/decode-ways.cpp
+++ leetcode/decode-ways.cpp
@@ -19,18 +19,18 @@ public:
         const int N = s.size();
         vector<int> dp(N + 1, 0);
         dp[N] = 1;
-        
+
         for (int i = N - 1; i >= 0; i--) {
-            if (s[i] != '0') 
-                dp[i] += dp[i+1];
-            if (i + 1 < N && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) 
-                dp[i] += dp[i+2];
+            if (s[i] != '0')
+                dp[i] += dp[i + 1];
+            if (i + 1 < N && (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')))
+                dp[i] += dp[i + 2];
         }
         return dp[0];
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     Solution solution;
     cout << solution.numDecodings("26");
 
