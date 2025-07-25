d3c9a67078117f5925da8a5b7f7ebfd041db613e
le <smilingpoplar@gmail.com>
2025-07-25 10:58:01 +0800

    + reorder-array

---
+++ leetcode/reorder-array.cpp
@@ -0,0 +1,24 @@
+//
+//  reorder-array
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> reOrderArray(vector<int>& array) {
+        // 冒泡排序，奇数往前移
+        const int N = array.size();
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = N - 1; j > i; j--) {
+                if (array[j] % 2 == 1 && array[j - 1] % 2 == 0) {  // j处奇数，j-1处偶数
+                    swap(array[j], array[j - 1]);
+                }
+            }
+        }
+        return array;
+    }
+};
