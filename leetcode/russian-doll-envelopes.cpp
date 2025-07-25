57f4d3163a436e844550ff2c8b5d94a6b5e9fc30
le <smilingpoplar@gmail.com>
2025-07-26 07:56:12 +0800

    update

--- leetcode/russian-doll-envelopes.cpp
+++ leetcode/russian-doll-envelopes.cpp
@@ -12,24 +12,27 @@ using namespace std;
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        // 按width排序，width相等时按照height递减排序，然后根据height找最长递增子序列，
-        // 能避免选中[3,4],[3,5]这种width相等的不该选情况。
-        sort(envelopes.begin(), envelopes.end(), 
-             [](vector<int>& a, vector<int>& b) {
-                return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
+        // 长宽同时递增的最长子序列
+        // 按width排序，然后根据height找最长递增子序列
+        // width相等时按照height递减排序，能避免选中[3,4],[3,5]这种情况
+        sort(envelopes.begin(), envelopes.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0] < b[0] || (a[0] == b[0] && a[1] > b[1]);
         });
         // 根据height找LIS，O(nlgn)
-        vector<int> tails; // 各长度LIS的最小尾元素
-        for (auto &e : envelopes) {
+        vector<int> tails;  // 各长度LIS的最小尾元素
+        for (auto& e : envelopes) {
             int height = e[1];
             auto it = lower_bound(tails.begin(), tails.end(), height);
-            if (it != tails.end()) *it = height;
-            else tails.push_back(height);
+            if (it != tails.end()) {
+                *it = height;
+            } else {
+                tails.push_back(height);
+            }
         }
         return tails.size();
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char* argv[]) {
     return 0;
 }
