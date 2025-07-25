8ad3352a83cab38702ae6b85a3aa22dc5fb2f99f
le <smilingpoplar@gmail.com>
2025-07-25 08:42:15 +0800

    update

--- leetcode/recover-binary-search-tree.cpp
+++ leetcode/recover-binary-search-tree.cpp
@@ -17,18 +17,18 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-/*
 class Solution {
 public:
-    void recoverTree(TreeNode* root) {
+    void recoverTree(TreeNode *root) {
         // 中序遍历，O(N)
+        // 找出违反递增关系的最初prev节点、最后node节点
         stack<TreeNode *> stk;
         pushLeft(stk, root);
-        
-        TreeNode *prev = NULL, *m1 = NULL, *m2 = NULL;
+
+        TreeNode *prev = nullptr, *m1 = nullptr, *m2 = nullptr;
         while (!stk.empty()) {
-            // 找出违反递增关系的首尾节点
-            auto node = stk.top(); stk.pop();
+            auto node = stk.top();
+            stk.pop();
             if (prev && node->val <= prev->val) {
                 if (!m1) m1 = prev;
                 m2 = node;
@@ -39,16 +39,16 @@ public:
 
         if (m1 && m2) swap(m1->val, m2->val);
     }
-    
-    void pushLeft(stack<TreeNode*> &stk, TreeNode *node) {
+
+    void pushLeft(stack<TreeNode *> &stk, TreeNode *node) {
         while (node) {
             stk.push(node);
             node = node->left;
         }
     }
 };
-*/
 
+/*
 class Solution {
 public:
     void recoverTree(TreeNode* root) {
@@ -57,7 +57,7 @@ public:
         // morris中序遍历，O(1)空间
         // 用中序遍历前驱节点的右指针pred->right表示左子树是否访问过
         // pred->right==NULL => 左子树未访问，记住要返回到当前节点，进入左子树
-        // pred->right!=NULL => 左子树已访问，清空pred->right，访问当前节点，进入右子树        
+        // pred->right!=NULL => 左子树已访问，清空pred->right，访问当前节点，进入右子树
         auto curr = root;
         TreeNode *pred = NULL; // 中序遍历前驱节点
         while (curr) {
@@ -94,6 +94,7 @@ public:
         if (m1 && m2) swap(m1->val, m2->val);
     }
 };
+*/
 
 void print(TreeNode *root) {
     if (!root) return;
@@ -102,16 +103,16 @@ void print(TreeNode *root) {
     print(root->right);
 }
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     TreeNode t0(3);
     TreeNode t1l(2);
     t0.left = &t1l;
     TreeNode t2r(1);
     t1l.right = &t2r;
-    
+
     Solution solution;
     solution.recoverTree(&t0);
-    print(&t0); // 3 1 2
-    
+    print(&t0);  // 3 1 2
+
     return 0;
 }
