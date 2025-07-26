62e3bf98ef44a3e2a2c94cddfb3c9d5e0e4c8d7b
le <smilingpoplar@gmail.com>
2025-07-26 10:44:58 +0800

    update

--- leetcode/path-sum-iii.cpp
+++ leetcode/path-sum-iii.cpp
@@ -25,13 +25,13 @@ public:
     int pathSum(TreeNode* root, int sum) {
         if (!root) return 0;
         return searchFrom(root, sum)
-            + pathSum(root->left, sum) 
+            + pathSum(root->left, sum)
             + pathSum(root->right, sum);
     }
-    
+
     int searchFrom(TreeNode *root, int sum) {
         if (!root) return 0;
-        return (root->val == sum ? 1 : 0) 
+        return (root->val == sum ? 1 : 0)
             + searchFrom(root->left, sum - root->val)
             + searchFrom(root->right, sum - root->val);
     }
@@ -41,14 +41,14 @@ public:
 class Solution {
 public:
     int pathSum(TreeNode *root, int target) {
-        unordered_map<int, int> presum = {{0,1}}; // sum=>count
+        unordered_map<int, int> presum = {{0, 1}};  // sum=>count
         int runningSum = 0;
         int ans = 0;
         search(root, target, runningSum, presum, ans);
         return ans;
     }
 
-    void search(TreeNode *root, int target, int runningSum, 
+    void search(TreeNode *root, const int target, int runningSum,
                 unordered_map<int, int> &presum, int &ans) {
         if (!root) return;
 
@@ -60,10 +60,10 @@ public:
         presum[runningSum]++;
         search(root->left, target, runningSum, presum, ans);
         search(root->right, target, runningSum, presum, ans);
-        presum[runningSum]--; // 回溯，为其他搜索路径准备
+        presum[runningSum]--;  // 回溯，为其他搜索路径准备
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     return 0;
 }
