a30e501cb767b9e929f56127a2a248eb7f91ac37
le <smilingpoplar@gmail.com>
2025-07-26 14:16:57 +0800

    update

--- leetcode/longest-substring-without-repeating-characters.cpp
+++ leetcode/longest-substring-without-repeating-characters.cpp
@@ -14,9 +14,10 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        const int N = s.size();
         unordered_map<char, int> cnt;
         int ans = 0;
-        for (int hi = 0, lo = 0; hi < s.size(); hi++) {
+        for (int hi = 0, lo = 0; hi < N; hi++) {
             cnt[s[hi]]++;
             while (cnt[s[hi]] > 1) {
                 cnt[s[lo]]--;
@@ -32,10 +33,11 @@ public:
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> mp; // char=>lastIdx
+        const int N = s.size();
+        unordered_map<char, int> mp;  // char=>lastIdx
         int ans = 0;
-        for (int hi = 0, lo = 0; hi < s.size(); hi++) {
-            if (mp.count(s[hi]) && mp[s[hi]] >= lo) { // s[hi]重复出现且在窗口内
+        for (int hi = 0, lo = 0; hi < N; hi++) {
+            if (mp.count(s[hi]) && mp[s[hi]] >= lo) {  // s[hi]重复出现且在窗口内
                 lo = mp[s[hi]] + 1;
             }
             ans = max(ans, hi - lo + 1);
@@ -45,9 +47,9 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     Solution solution;
     cout << solution.lengthOfLongestSubstring("abbaab");
-    
+
     return 0;
 }
