7aa65f46898b153dd656c5d5d5c4cb6c4689ccfd
le <smilingpoplar@gmail.com>
2025-07-26 15:42:50 +0800

    update

--- leetcode/maximum-width-of-binary-tree.cpp
+++ leetcode/maximum-width-of-binary-tree.cpp
@@ -20,23 +20,23 @@ using namespace std;
  */
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* root) {
+    int widthOfBinaryTree(TreeNode *root) {
         int ans = INT_MIN;
-        vector<int> leftMost; // 各层最左节点的id 
+        vector<int> leftMost;  // 各层最左节点的id
         dfs(root, 0, 1, leftMost, ans);
         return ans;
     }
-    
+
     void dfs(TreeNode *root, int depth, int id, vector<int> &leftMost, int &ans) {
         if (!root) return;
         if (depth == leftMost.size()) leftMost.push_back(id);
-        
+
         ans = max(ans, id - leftMost[depth] + 1);
         dfs(root->left, depth + 1, id * 2, leftMost, ans);
         dfs(root->right, depth + 1, id * 2 + 1, leftMost, ans);
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char *argv[]) {
     return 0;
 }
