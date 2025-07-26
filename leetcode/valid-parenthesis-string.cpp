4900ecf51511a7ae6aae0cebfca72dd9b97bb42c
le <smilingpoplar@gmail.com>
2025-07-26 14:30:59 +0800

    update

--- leetcode/valid-parenthesis-string.cpp
+++ leetcode/valid-parenthesis-string.cpp
@@ -12,11 +12,11 @@ using namespace std;
 class Solution {
 public:
     bool checkValidString(string s) {
-        // '*' 可表示 '('、')'、空串，设多出的左括号数leftDiff的范围为[lo, hi]。
+        // 设多出的左括号数leftDiff的范围为[lo, hi]
         // 1. 遇到'('，左括号多1，lo++, hi++；
         // 2. 遇到')'，左括号少1，lo--, hi--；
         // 3. 遇到'*'，它可以是'('、空串、')'，[lo, hi]内的每个值都可-1、+0、+1，整体扩张lo--, hi++。
-        // 综上，遇到'('时lo++、其他lo--；遇到')'时hi--，其他hi++。 
+        // 综上，遇到'('时lo++、其他lo--；遇到')'时hi--，其他hi++。
         int lo = 0, hi = 0;
         for (char c : s) {
             lo += (c == '(') ? 1 : -1;
@@ -31,6 +31,6 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char* argv[]) {
     return 0;
 }
