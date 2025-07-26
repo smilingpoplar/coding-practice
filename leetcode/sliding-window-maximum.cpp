d0947d70cbe520ccfeb8ce278e42b463e715cb3a
le <smilingpoplar@gmail.com>
2025-07-26 22:30:26 +0800

    update

--- leetcode/sliding-window-maximum.cpp
+++ leetcode/sliding-window-maximum.cpp
@@ -6,8 +6,8 @@
 //
 
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
@@ -19,26 +19,26 @@ public:
         vector<int> ans;
         deque<int> dq;
         for (int i = 0; i < nums.size(); i++) {
-            if (!dq.empty() && i - dq[0] + 1 > k) dq.pop_front(); // 控制住窗口长
             while (!dq.empty() && nums[i] > nums[dq.back()]) {
                 dq.pop_back();
             }
             dq.push_back(i);
-            
+            if (i - dq[0] + 1 > k) dq.pop_front();  // 控制住窗口长
+
             if (i - k + 1 >= 0) ans.push_back(nums[dq[0]]);
         }
         return ans;
     }
 };
 
-int main(int argc, const char * argv[]) {
-    vector<int> nums = {1,3,-1,-3,5,3,6,7};
+int main(int argc, const char* argv[]) {
+    vector<int> nums = {1, 3, -1, -3, 5, 3, 6, 7};
     int k = 3;
     Solution solution;
     auto result = solution.maxSlidingWindow(nums, k);
     for (auto num : result) {
         cout << num << " ";
     }
-    
+
     return 0;
 }
