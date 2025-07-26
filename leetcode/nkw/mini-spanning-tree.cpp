0dbdb6aba854fcd50640715013340393a2c0c61d
le <smilingpoplar@gmail.com>
2025-07-26 13:05:38 +0800

    update

--- leetcode/nkw/mini-spanning-tree.cpp
+++ leetcode/nkw/mini-spanning-tree.cpp
@@ -38,3 +38,55 @@ public:
         return uf[x];
     }
 };
+
+/*
+#include <climits>
+#include <queue>
+
+class Solution {
+  public:
+    int miniSpanningTree(int n, int m, vector<vector<int> >& cost) {
+        // Prim算法，图遍历+优先队列
+        vector<int> dist(n + 1, INT_MAX); // 各节点到MST的距离
+        dist[1] = 0; // 节点是1-based编号
+
+        // 邻接表
+        unordered_map<int, unordered_map<int, int>> adj;
+        for (auto &e : cost) {
+            // 多条路径选最小的那条
+            int u = e[0], v = e[1], d = e[2];
+            if (!adj[u].count(v) || d < adj[u][v])
+                adj[u][v] = d;
+            if (!adj[v].count(u) || d < adj[v][u])
+                adj[v][u] = d;
+        }
+        // 优先队列
+        auto cmp = [](const vector<int>& a, const vector<int>& b) { // [dist, from]
+            return a[0] > b[0]; // 最小堆
+        };
+        priority_queue<vector<int>, vector<vector<int>>, decltype(cmp)> pq(cmp);
+        pq.push({dist[1], 1});
+
+        int ans = 0, cnt = 0;
+        vector<int> seen(n + 1, false);
+        while (!pq.empty() && cnt < n) { // MST有n个顶点
+            auto top = pq.top(); pq.pop();
+            int d = top[0], u = top[1]; // MST中的顶点
+            if (seen[u]) continue;
+            seen[u] = true;
+            ans += d;
+            cnt++;
+
+            for (int v = 1; v <= n; v++) {
+                if (seen[v] || !adj[u].count(v)) continue;
+                int newdist = adj[u][v];
+                if (newdist < dist[v]) {
+                    dist[v] = newdist;
+                    pq.push({newdist, v});
+                }
+            }
+        }
+        return ans;
+    }
+};
+*/
