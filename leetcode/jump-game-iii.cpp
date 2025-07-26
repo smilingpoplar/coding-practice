833efc04d690b9f98197d4411a8173c63231a202
le <smilingpoplar@gmail.com>
2025-07-26 21:26:49 +0800

    update

--- leetcode/jump-game-iii.cpp
+++ leetcode/jump-game-iii.cpp
@@ -14,12 +14,12 @@ public:
     bool canReach(vector<int>& arr, int st) {
         if (st < 0 || st >= arr.size() || arr[st] < 0) return false;
         if (arr[st] == 0) return true;
-        arr[st] *= -1; // 标记为已访问
+        arr[st] *= -1;  // 标记为已访问
         int val = abs(arr[st]);
         return canReach(arr, st - val) || canReach(arr, st + val);
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char* argv[]) {
     return 0;
 }
