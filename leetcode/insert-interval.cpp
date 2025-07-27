bfb6f6809be3ea546759bec77ce05034daf1f8c4
le <smilingpoplar@gmail.com>
2025-07-27 09:47:50 +0800

    update

--- leetcode/insert-interval.cpp
+++ leetcode/insert-interval.cpp
@@ -14,14 +14,16 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         // 区间互不重叠、已按起点排序
-        vector<vector<int>> before, after; // 前后的不重叠区间
+        vector<vector<int>> before, after;  // 前后的不重叠区间
         int left = newInterval[0], right = newInterval[1];
-        for (auto& interval : intervals) {
-            if (interval[1] < left) before.push_back(interval);
-            else if (interval[0] > right) after.push_back(interval);
-            else {
-                left = min(left, interval[0]);
-                right = max(right, interval[1]);
+        for (auto& i : intervals) {
+            if (i[1] < left) {
+                before.push_back(i);
+            } else if (i[0] > right) {
+                after.push_back(i);
+            } else {
+                left = min(left, i[0]);
+                right = max(right, i[1]);
             }
         }
 
@@ -31,6 +33,6 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     return 0;
 }
--- leetcode/is-subsequence.cpp
+++ leetcode/is-subsequence.cpp
@@ -14,9 +14,10 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         // 在t中找s
-        int is = 0;
-        for (int it = 0; it < t.size() && is < s.size(); it++) {
+        int is = 0, it = 0;
+        while (it < t.size() && is < s.size()) {
             if (s[is] == t[it]) is++;
+            it++;
         }
         return is == s.size();
     }
@@ -26,14 +27,14 @@ public:
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        // follow up，预处理t，记录个字母出现的位置列表
+        // follow up，预处理t，记录每个字母出现的位置列表
         vector<vector<int>> pos(26);
         for (int i = 0; i < t.size(); i++) {
             pos[t[i] - 'a'].push_back(i);
         }
-        
+
         int lastIdx = -1;
-        for (char c : s) { // 应在各字母的位置列表穿梭前进
+        for (char c : s) {  // 应在各字母的位置列表穿梭前进
             auto &list = pos[c - 'a'];
             auto it = upper_bound(list.begin(), list.end(), lastIdx);
             if (it == list.end()) return false;
@@ -43,6 +44,6 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char *argv[]) {
     return 0;
 }
