d9957ca6ea4b725213b25d2ce77dde9e21fea4d4
le <smilingpoplar@gmail.com>
2025-07-27 11:15:36 +0800

    update

--- leetcode/triangle.cpp
+++ leetcode/triangle.cpp
@@ -12,25 +12,23 @@ using namespace std;
 
 class Solution {
 public:
-    int minimumTotal(vector<vector<int> > &triangle) {
-        // 考查到t[i][j]的路径，可来自上一行的t[i-1][j-1]或t[i-1][j]
+    int minimumTotal(vector<vector<int>> &triangle) {
         // 设dp[i][j]表示从t[0][0]到t[i][j]的最小路径和，则
-        // dp[i][j] = min( dp[i-1][j-1], dp[i-1][j] ) + t[i][j]，0<j<i<N（不含边界j==0和j==i）
-        // 当j==0时，dp[i][j] = dp[i-1][j] + t[i][j]；当j==i时，dp[i][j] = dp[i-1][j-1] + t[i][j]
+        // dp[i][j] = min( dp[i-1][j-1], dp[i-1][j] ) + t[i][j]，0<=j<=i<N
         // 在i这一维上递推式只依赖于i-1项，可省略i这一维，保持i从左往右遍历
         // 要让dp[j-1]表示旧状态dp[i-1][j-1]，j从右往左遍历
         // dp[j] = min(dp[j-1], dp[j]) + t[i][j]
         if (triangle.empty()) return 0;
         const int N = triangle.size();
-        vector<int> dp(N, 0); // 对应i==0
-        dp[0] = triangle[0][0];
+        vector<int> dp(N, 0);
 
+        dp[0] = triangle[0][0];  // i==0
         for (int i = 1; i < N; i++) {
-            dp[i] = dp[i-1] + triangle[i][i]; // j==i
+            dp[i] = dp[i - 1] + triangle[i][i];  // j==i
             for (int j = i - 1; j >= 1; j--) {
-                dp[j] = min(dp[j-1], dp[j]) + triangle[i][j];
+                dp[j] = min(dp[j - 1], dp[j]) + triangle[i][j];
             }
-            dp[0] = dp[0] + triangle[i][0]; // j==0
+            dp[0] = dp[0] + triangle[i][0];  // j==0
         }
 
         int ans = INT_MAX;
@@ -41,12 +39,12 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     vector<vector<int>> triangle = {
         {2},
-        {3,4},
-        {6,5,7},
-        {4,1,8,3},
+        {3, 4},
+        {6, 5, 7},
+        {4, 1, 8, 3},
     };
     Solution solution;
     cout << solution.minimumTotal(triangle);
