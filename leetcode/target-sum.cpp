3493552f3ab702423a1389c2823d1236014370ce
le <smilingpoplar@gmail.com>
2025-07-28 08:50:04 +0800

    update

--- leetcode/target-sum.cpp
+++ leetcode/target-sum.cpp
@@ -12,6 +12,8 @@ using namespace std;
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
+        // 设加正号的子集和为A，加负号的子集和为B，则A-B=S；又A+B=sum，所以
+        // A = (S+sum)/2，B = (sum-S)/2，要满足sum>=S且sum-S为偶数
         int sum = 0;
         for (int num : nums)
             sum += num;
@@ -22,14 +24,14 @@ public:
         vector<int> dp(target + 1, 0);
         dp[0] = 1;
         for (int num : nums) {
-            for (int j = target; j >= num; j--) { // 逆序循环
-                dp[j] += dp[j-num];
+            for (int i = target; i >= num; i--) {  // 逆序循环
+                dp[i] += dp[i - num];
             }
         }
         return dp[target];
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char* argv[]) {
     return 0;
 }
