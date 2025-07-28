c32a65686cf4fdb59d275d6ca886872148ad50fe
le <smilingpoplar@gmail.com>
2025-07-28 08:13:16 +0800

    update

--- leetcode/expression-add-operators.cpp
+++ leetcode/expression-add-operators.cpp
@@ -17,8 +17,8 @@ public:
         search(num, target, 0, "", 0, 0, ans);
         return ans;
     }
-    
-    void search(string num, int target, int idx, string lastExprStr, 
+
+    void search(string num, int target, int idx, string lastExprStr,
                 long lastExprVal, long lastNum, vector<string> &ans) {
         if (idx == num.size()) {
             if (lastExprVal == target) ans.push_back(lastExprStr);
@@ -27,25 +27,25 @@ public:
 
         // [idx..i]是currStr, [i+1..]进递归
         for (int i = idx; i < num.size(); i++) {
-            if (i > idx && num[idx] == '0') continue; // 多位数不能以"0"开头
+            if (i > idx && num[idx] == '0') continue;  // 多位数不能以"0"开头
             auto currStr = num.substr(idx, i - idx + 1);
             long currNum = stol(currStr);
-            if (idx == 0) {
+            if (idx == 0) {  // 不需添加"+-*"
                 search(num, target, i + 1, currStr, currNum, currNum, ans);
             } else {
-                search(num, target, i + 1, lastExprStr + "+" + currStr, 
+                search(num, target, i + 1, lastExprStr + "+" + currStr,
                        lastExprVal + currNum, currNum, ans);
-                search(num, target, i + 1, lastExprStr + "-" + currStr, 
+                search(num, target, i + 1, lastExprStr + "-" + currStr,
                        lastExprVal - currNum, -currNum, ans);
                 // 比如 lastExprVal=1+2+3，现在遇到*4，要lastExprVal-3+3*4
                 // 若再遇到*5，要lastExprVal-(3*4)+(3*4)*5
-                search(num, target, i + 1, lastExprStr + "*" + currStr, 
+                search(num, target, i + 1, lastExprStr + "*" + currStr,
                        lastExprVal - lastNum + lastNum * currNum, lastNum * currNum, ans);
             }
         }
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     return 0;
 }
