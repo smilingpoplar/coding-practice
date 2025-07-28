e876142847a23d2316df4669f1021c721a79b0a9
le <smilingpoplar@gmail.com>
2025-07-28 08:24:30 +0800

    update

--- leetcode/basic-calculator-ii.cpp
+++ leetcode/basic-calculator-ii.cpp
@@ -16,10 +16,12 @@ public:
         // 中缀转后缀，用nums栈和ops栈
         // num先入栈，遇到下一个优先级<=栈顶的op时、先弹出计算再把op入栈。
         unordered_map<char, int> priority = {
-            {'*', 2}, {'/', 2},
-            {'+', 1}, {'-', 1},
+            {'*', 2},
+            {'/', 2},
+            {'+', 1},
+            {'-', 1},
         };
-        
+
         stack<int> nums;
         stack<char> ops;
         for (int i = 0; i < s.size(); i++) {
@@ -27,25 +29,28 @@ public:
             if (c == ' ') continue;
             if (isdigit(c)) {
                 int num = c - '0';
-                while (i + 1 < s.size() && isdigit(s[i+1]))
+                while (i + 1 < s.size() && isdigit(s[i + 1]))
                     num = num * 10 + s[++i] - '0';
                 nums.push(num);
-            } else { // +-*/
+            } else {  // +-*/
                 while (!ops.empty() && priority[c] <= priority[ops.top()]) calc(nums, ops);
                 ops.push(c);
             }
         }
         while (!ops.empty()) calc(nums, ops);
-        
+
         return nums.top();
     }
 
     void calc(stack<int> &nums, stack<char> &ops) {
         if (ops.empty() || nums.size() < 2) return;
-        char op = ops.top(); ops.pop();
-        int val2 = nums.top(); nums.pop();
-        int val1 = nums.top(); nums.pop();
-        
+        char op = ops.top();
+        ops.pop();
+        int val2 = nums.top();
+        nums.pop();
+        int val1 = nums.top();
+        nums.pop();
+
         if (op == '*') {
             nums.push(val1 * val2);
         } else if (op == '/') {
@@ -58,9 +63,9 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     Solution solution;
     cout << solution.calculate(" 3+5 / 2 ");
-    
+
     return 0;
 }
