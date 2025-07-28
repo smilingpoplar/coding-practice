26fe5e5516f5bdfef142e6a4a230fbfea898ec2b
le <smilingpoplar@gmail.com>
2025-07-28 19:09:39 +0800

    update

--- leetcode/inorder-successor-in-bst.cpp
+++ leetcode/inorder-successor-in-bst.cpp
@@ -10,27 +10,25 @@
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        if (!root || !p) return NULL;
+        if (!root || !p) return nullptr;
         // bst的中序遍历是个有序数组，这题相当于要找第一个>p.val的节点
-        // 类似二分搜索，root就是二分搜索中的mid
-        TreeNode *succ = NULL;
+        // 类似二分搜索
+        TreeNode* succ = nullptr;
         while (root) {
-            if (root->val > p->val) { // root作候选，看左边有没更小的
+            if (p->val < root->val) {  // root作候选，看左边有没更小的
                 succ = root;
                 root = root->left;
-            } else { // root值太小，排除root和左子树
+            } else {  // root值太小，排除root和左子树
                 root = root->right;
             }
         }
@@ -38,6 +36,6 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     return 0;
 }
