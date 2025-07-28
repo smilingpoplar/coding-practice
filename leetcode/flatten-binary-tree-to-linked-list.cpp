50c7a3711bae3b679ce50f0d44e8c140519c5ea2
le <smilingpoplar@gmail.com>
2025-07-29 06:12:05 +0800

    udate

--- leetcode/flatten-binary-tree-to-linked-list.cpp
+++ leetcode/flatten-binary-tree-to-linked-list.cpp
@@ -19,30 +19,30 @@ struct TreeNode {
 
 class Solution {
 public:
-    void flatten(TreeNode* root) {
-        // 前序遍历
-        stack<TreeNode *> stk;
-        if (root) stk.push(root);
-        TreeNode *prev = NULL;
-        while (!stk.empty()) {
-            auto node = stk.top(); stk.pop();
-            if (node->right) stk.push(node->right);
-            if (node->left) stk.push(node->left);
-            
-            node->left = node->right = NULL;
-            if (prev) prev->right = node;
-            prev = node;
-        }
+    void flatten(TreeNode *root) {
+        TreeNode *prev = nullptr;
+        preorder(root, prev);
+    }
+
+    void preorder(TreeNode *root, TreeNode *&prev) {
+        if (!root) return;
+        auto left = root->left, right = root->right;
+        root->left = root->right = nullptr;
+        if (prev) prev->right = root;
+        prev = root;
+
+        if (left) preorder(left, prev);
+        if (right) preorder(right, prev);
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char *argv[]) {
     TreeNode t0(1);
     TreeNode t1l(2);
     t0.left = &t1l;
     TreeNode t2r(3);
     t1l.right = &t2r;
-    
+
     Solution solution;
     solution.flatten(&t0);
     auto head = &t0;
@@ -50,6 +50,6 @@ int main(int argc, const char * argv[]) {
         cout << head->val << " ";
         head = head->right;
     }
-    
+
     return 0;
 }
