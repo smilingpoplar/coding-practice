0a7991809eb1b269f5355474deee0e9b6654420f
le <smilingpoplar@gmail.com>
2025-07-29 12:34:39 +0800

    update: zigzag-conversion

--- leetcode/zigzag-conversion.cpp
+++ leetcode/zigzag-conversion.cpp
@@ -12,25 +12,25 @@ using namespace std;
 class Solution {
 public:
     string convert(string s, int numRows) {
-        // 观察numRows不同取值的例子，一个zigzag的字符数是2*numRows-2
         if (numRows == 1) return s;
-        const int zigzag = 2 * numRows - 2;
-        string result;
-        for (int i = 0; i < numRows; i++) {
-            for (int j = i; j < s.size(); j += zigzag) {
-                result += s[j];
-                if (i == 0 || i == numRows - 1) continue;
-                int index = j + zigzag - 2 * i;
-                if (index < s.size()) result += s[index];
+
+        int rowIdx = 0, dir = 1;
+        vector<string> rows(numRows);
+        for (char c : s) {
+            rows[rowIdx].push_back(c);
+
+            if (rowIdx == numRows - 1) {
+                dir = -1;
+            } else if (rowIdx == 0) {
+                dir = 1;
             }
+            rowIdx += dir;
         }
-        return result;
+
+        string ans;
+        for (auto &row : rows) {
+            ans += row;
+        }
+        return ans;
     }
 };
-
-int main(int argc, const char * argv[]) {
-    Solution solution;
-    cout << solution.convert("PAYPALISHIRING", 3);
-    
-    return 0;
-}
