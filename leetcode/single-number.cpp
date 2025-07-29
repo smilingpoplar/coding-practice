f157c2754a51e5fbc1aa78a9c20a62435ac25e05
le <smilingpoplar@gmail.com>
2025-07-29 16:54:59 +0800

    update

--- leetcode/single-number.cpp
+++ leetcode/single-number.cpp
@@ -21,10 +21,30 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {
+/*
+class Solution {
+public:
+    // O(logN)，二分
+    int singleElement(vector<int>& v) {
+        // 第一个 v[mid]!=v[mid^1] 的位置
+        int lo = -1, hi = v.size();
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (v[mid] != v[mid ^ 1]) {
+                hi = mid;
+            } else {
+                lo = mid;
+            }
+        }
+        return v[hi];
+    }
+};
+*/
+
+int main(int argc, const char* argv[]) {
     vector<int> nums = {3, 3, 4, 5, 5};
     Solution solution;
     cout << solution.singleNumber(nums);
-    
+
     return 0;
 }
