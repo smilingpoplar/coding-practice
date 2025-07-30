f4c1788dd9675383b69b2939cfc11a4f5167fdef
le <smilingpoplar@gmail.com>
2025-07-30 11:19:49 +0800

    update

--- leetcode/koko-eating-bananas.cpp
+++ leetcode/koko-eating-bananas.cpp
@@ -26,9 +26,8 @@ public:
     }
 
     bool guess(int K, vector<int>& piles, int H) {
-        // 吃完piles[i]需要(piles[i]+K-1)/K小时
-        // sum{ (piles[i]+K-1)/K }是K的递减函数，
-        // sum<=H 符合二分搜索[0 0 .. 0 1 1 ..]的条件
+        // 吃完piles[i]需要(piles[i]+K-1)/K小时，sum{ (piles[i]+K-1)/K }是K的递减函数，
+        // sum<=H 符合二分搜索[0 0 .. 0 1 1 ..]的条件，找最小的K对应找第一个1
         int sum = 0;
         for (int pile : piles) {
             sum += (pile + K - 1) / K;
