7d2d4a53ab0ef9011971d1617723da25ef3a2465
le <smilingpoplar@gmail.com>
2025-07-30 08:40:40 +0800

    update

--- leetcode/reorganize-string.cpp
+++ leetcode/reorganize-string.cpp
@@ -13,16 +13,16 @@ class Solution {
 public:
     string reorganizeString(string S) {
         // https://leetcode.com/problems/rearrange-string-k-distance-apart/ 的特例
-        // 可行需 最多的字符数 <= 其他所有字符隔开的空槽
+        // 可行需 最多的字符数 <= 由其他所有字符隔开的空槽
         // 即 maxCnt <= N-maxCnt+1，2*maxCnt <= N+1
         const int N = S.size();
         int maxCnt = INT_MIN;
         unordered_map<char, int> cnt;
         for (char c : S) {
             cnt[c]++;
-            if (cnt[c] > maxCnt) maxCnt = cnt[c];
+            maxCnt = max(maxCnt, cnt[c]);
         }
-        if (2 * maxCnt > N + 1) return ""; // 此判断之后一定可行
+        if (2 * maxCnt > N + 1) return "";  // 此判断之后一定可行
 
         auto cmp = [&cnt](char a, char b) { return cnt[a] < cnt[b]; };
         priority_queue<char, vector<char>, decltype(cmp)> pq(cmp);
@@ -31,13 +31,15 @@ public:
         string ans;
         queue<char> freezed;
         while (!pq.empty()) {
-            char c = pq.top(); pq.pop();
+            char c = pq.top();
+            pq.pop();
             ans += c;
             cnt[c]--;
-            
+
             freezed.push(c);
-            if (freezed.size() >= 2) { // 相同字母至少距离2
-                int released = freezed.front(); freezed.pop();
+            if (freezed.size() >= 2) {  // 相同字母至少距离2
+                int released = freezed.front();
+                freezed.pop();
                 if (cnt[released] > 0) pq.push(released);
             }
         }
@@ -45,6 +47,6 @@ public:
     }
 };
 
-int main(int argc, const char * argv[]) {    
+int main(int argc, const char *argv[]) {
     return 0;
 }
