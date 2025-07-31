3d27004dd410778861849543f627efde34481da6
le <smilingpoplar@gmail.com>
2025-07-31 10:20:08 +0800

    update

--- leetcode/interval-list-intersections.cpp
+++ leetcode/interval-list-intersections.cpp
@@ -12,24 +12,27 @@ using namespace std;
 
 class Solution {
 public:
-    vector<Interval> intervalIntersection(vector<Interval>& A, vector<Interval>& B) {
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& A, vector<vector<int>>& B) {
         // 归并排序
         const int M = A.size(), N = B.size();
         int i = 0, j = 0;
-        vector<Interval> ans;
+        vector<vector<int>> ans;
         while (i < M && j < N) {
             // 如果A[i]和B[j]相交，[lo,hi]是相交区间
-            int lo = max(A[i].start, B[j].start);
-            int hi = min(A[i].end, B[j].end);
+            int lo = max(A[i][0], B[j][0]);
+            int hi = min(A[i][1], B[j][1]);
             if (lo <= hi) ans.push_back({lo, hi});
-            
-            if (A[i].end < B[j].end) i++;
-            else j++;
+
+            if (A[i][1] < B[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
         }
         return ans;
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     return 0;
 }
