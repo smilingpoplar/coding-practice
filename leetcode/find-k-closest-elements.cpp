ba12a12b5dc675a07b942534556ca15467247c11
le <smilingpoplar@gmail.com>
2025-07-31 18:21:21 +0800

    update: find-k-closest-elements

--- leetcode/find-k-closest-elements.cpp
+++ leetcode/find-k-closest-elements.cpp
@@ -13,6 +13,27 @@ using namespace std;
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        // 设要找的子段为 arr[i..i+k-1]，对位置i做二分搜索
+        // 比较 arr[i] 和 子段后的arr[i+k]：
+        // 若 x - arr[i] > arr[i+k] - x，右移子段起始i
+        int lo = 0, hi = (int)arr.size() - k;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (x - arr[mid] > arr[mid + k] - x) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+
+        return vector<int>(begin(arr) + lo, begin(arr) + lo + k);
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         // 双指针法
         int i = 0, j = (int)arr.size() - 1;
         while (j - i >= k) {  // 循环结束后 j-i+1 == k
@@ -23,13 +44,10 @@ public:
             }
         }
 
-        vector<int> ans;
-        for (int l = i; l <= j; l++) {
-            ans.push_back(arr[l]);
-        }
-        return ans;
+        return vector<int>(begin(arr) + i, begin(arr) + i + k);
     }
 };
+*/
 
 int main(int argc, const char* argv[]) {
     return 0;
