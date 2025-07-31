00fd25cd835013203dc6b2cb64272ed87573f0aa
le <smilingpoplar@gmail.com>
2025-07-31 19:15:51 +0800

    update

--- leetcode/one-edit-distance.cpp
+++ leetcode/one-edit-distance.cpp
@@ -13,23 +13,22 @@ using namespace std;
 class Solution {
 public:
     bool isOneEditDistance(string s, string t) {
-        const int M = s.size();
-        const int N = t.size();
+        const int M = s.size(), N = t.size();
         if (abs(M - N) > 1) return false;
 
         int maxI = min(M, N);
         for (int i = 0; i < maxI; i++) {
             if (s[i] == t[i]) continue;
             // 单字母替换、添加或删除
-            if (M == N) return s.substr(i+1) == t.substr(i+1);
-            if (M < N) return s.substr(i) == t.substr(i+1);
-            return s.substr(i+1) == t.substr(i);
+            if (M == N) return s.substr(i + 1) == t.substr(i + 1);
+            if (M < N) return s.substr(i) == t.substr(i + 1);
+            return s.substr(i + 1) == t.substr(i);
         }
-        // 所有前面都相同，需某串多一个字母
+        // 所有前面都相同，需某串多一个字符
         return M != N;
     }
 };
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]) {
     return 0;
 }
